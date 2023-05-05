f5914b0fb7754fc0f0f1c97069af6c069810f725
Aliaksei Kryvasheyeu <aliaksei_kryvasheyeu@epam.com>
2023-05-05 09:16:02 +0200

    corrects user comp

--- components/Chatbar/User.tsx
+++ components/Chatbar/User.tsx
@@ -1,8 +1,11 @@
 import { IconLogin, IconLogout, IconUser } from '@tabler/icons-react';
 import { signIn, signOut, useSession } from 'next-auth/react';
-import { FC } from 'react';
+import { FC, useCallback } from 'react';
 
+import { useTranslation } from 'next-i18next';
 import Image from 'next/image';
+
+import { SidebarButton } from '../Sidebar/SidebarButton';
 
 const UserIcon: FC<{ image?: string | null; title?: string | null }> = ({
   image,
@@ -25,26 +28,27 @@ const UserIcon: FC<{ image?: string | null; title?: string | null }> = ({
 
 export const User: FC = () => {
   const { data: session } = useSession();
+  const { t } = useTranslation('sidebar');
+  const onClick = useCallback(() => {
+    session
+      ? signOut({ redirect: false })
+      : signIn('azure-ad', { redirect: true });
+  }, [session]);
 
-  console.log(session?.user);
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center gap-2 w-full">
       {session && (
-        <>
+        <div className="flex-1 min-w-0 flex items-center gap-2">
           <UserIcon image={session?.user?.image} title={session?.user?.name} />
           {session?.user?.name ?? ''}
-        </>
+        </div>
       )}
-      <button
-        type="button"
-        onClick={() =>
-          session
-            ? signOut({ redirect: false })
-            : signIn('azure-ad', { redirect: true })
-        }
-      >
-        {session ? <IconLogout /> : <IconLogin />}
-      </button>
+      <SidebarButton
+        onClick={onClick}
+        className={session ? 'w-12' : ''}
+        icon={session ? <IconLogout /> : <IconLogin />}
+        text={session ? '' : t('Sign In')}
+      />
     </div>
   );
 };
--- components/Sidebar/SidebarButton.tsx
+++ components/Sidebar/SidebarButton.tsx
@@ -1,15 +1,26 @@
 import { FC } from 'react';
 
+import classNames from 'classnames';
+
 interface Props {
   text: string;
   icon: JSX.Element;
   onClick: () => void;
+  className?: string;
 }
 
-export const SidebarButton: FC<Props> = ({ text, icon, onClick }) => {
+export const SidebarButton: FC<Props> = ({
+  text,
+  icon,
+  onClick,
+  className,
+}) => {
   return (
     <button
-      className="flex w-full cursor-pointer select-none items-center gap-3 rounded-md py-3 px-3 text-[14px] leading-3 text-white transition-colors duration-200 hover:bg-gray-500/10"
+      className={classNames(
+        'flex w-full cursor-pointer select-none items-center gap-3 rounded-md py-3 px-3 text-[14px] leading-3 text-white transition-colors duration-200 hover:bg-gray-500/10',
+        className,
+      )}
       onClick={onClick}
     >
       <div>{icon}</div>
