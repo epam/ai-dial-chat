66ac94352fe65bf3347125ab47f4f9d29d382e80
Denys Kolomiitsev <denys_kolomiitsev@epam.com>
2023-06-01 17:17:48 +0200

    changed chat loader

--- components/Chat/ChatLoader.tsx
+++ components/Chat/ChatLoader.tsx
@@ -1,7 +1,7 @@
 import { IconRobot } from '@tabler/icons-react';
 import { FC } from 'react';
 
-interface Props { }
+interface Props {}
 
 export const ChatLoader: FC<Props> = () => {
   return (
@@ -10,10 +10,11 @@ export const ChatLoader: FC<Props> = () => {
       style={{ overflowWrap: 'anywhere' }}
     >
       <div className="m-auto flex gap-4 p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
-        <div className="min-w-[40px] items-end">
+        <div className="animate-bounce min-w-[40px] items-end">
           <IconRobot size={30} />
         </div>
-        <span className="animate-pulse cursor-default mt-1">▍</span>
+
+        <span className="animate-ping cursor-default mt-1">▍</span>
       </div>
     </div>
   );
