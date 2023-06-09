6997b63c0e458fe865499881b13719cf8153036a
Aliaksei Kryvasheyeu <aliaksei_kryvasheyeu@epam.com>
2023-06-09 10:39:43 +0200

    do not throw exception when models are not available

--- pages/api/models.ts
+++ pages/api/models.ts
@@ -48,38 +48,34 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         ...getHeaders(session),
       },
     });
+    const models: OpenAIModel[] = [];
 
-    if (response.status === 401) {
-      // return new Response(response.body, {
-      //   status: 500,
-      //   headers: response.headers,
-      // });
-
-      return res.status(500).send(await response.text());
-    } else if (response.status !== 200) {
+    if (response.status !== 200) {
       console.error(
         `OpenAI API returned an error ${
           response.status
         }: ${await response.text()}`,
       );
-      throw new Error('OpenAI API returned an error');
-    }
+      // throw new Error('OpenAI API returned an error');
+    } else {
+      const json = await response.json();
 
-    const json = await response.json();
-
-    const models: OpenAIModel[] = json.data
-      .map((model: any) => {
-        const model_name = OPENAI_API_TYPE === 'azure' ? model.model : model.id;
-        for (const [key, value] of Object.entries(OpenAIModelID)) {
-          if (value === model_name) {
-            return {
-              id: model.id,
-              name: OpenAIModels[value].name,
-            };
+      const openAIModels = json.data
+        .map((model: any) => {
+          const model_name =
+            OPENAI_API_TYPE === 'azure' ? model.model : model.id;
+          for (const [key, value] of Object.entries(OpenAIModelID)) {
+            if (value === model_name) {
+              return {
+                id: model.id,
+                name: OpenAIModels[value].name,
+              };
+            }
           }
-        }
-      })
-      .filter(Boolean);
+        })
+        .filter(Boolean);
+      models.push(...openAIModels);
+    }
 
     if (process.env.GOOGLE_AI_TOKEN) {
       models.push({
