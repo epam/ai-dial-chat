7d8254bbc1b1d03a0882a64cf11b1bd77ffe8e1a
Denys Kolomiitsev <denys_kolomiitsev@epam.com>
2023-06-13 16:48:59 +0200

    added i18n

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -413,18 +413,26 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to {APP_NAME}
+            {t('Welcome to {{name}}', { appName: APP_NAME })}
           </div>
           <div className="text-center text-lg text-black dark:text-white">
-            <div className="mb-8">{`${APP_NAME} is an open source clone of OpenAI's ChatGPT UI.`}</div>
+            <div className="mb-8">
+              {t(`{{name}} is an open source clone of OpenAI's ChatGPT UI.`, {
+                appName: APP_NAME,
+              })}
+            </div>
             <div className="mb-2 font-bold">
-              Important: {APP_NAME} is 100% unaffiliated with OpenAI.
+              {t('Important: {{name}} is 100% unaffiliated with OpenAI.', {
+                appName: APP_NAME,
+              })}
             </div>
           </div>
           <div className="text-center text-gray-500 dark:text-gray-400">
             <div className="mb-2">
-              {APP_NAME} allows you to plug in your API key to use this UI with
-              their API.
+              {t(
+                '{{name}} allows you to plug in your API key to use this UI with their API.',
+                { appName: APP_NAME },
+              )}
             </div>
             <div className="mb-2">
               It is <span className="italic">only</span> used to communicate
