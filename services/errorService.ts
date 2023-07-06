12a9b1a9068edf8435356faf23e471a97c4efa06
Nikita Butsko <nikita_butsko@epam.com>
2023-07-06 12:29:45 +0200

    fix: fix tests

--- services/errorService.ts
+++ services/errorService.ts
@@ -29,6 +29,27 @@ const useErrorService = () => {
       },
       [t],
     ),
+    getAddonsError: useMemo(
+      () => (error: any) => {
+        return !error
+          ? null
+          : ({
+              title: t('Error fetching addons.'),
+              code: error.status || 'unknown',
+              messageLines: error.statusText
+                ? [error.statusText]
+                : [
+                    t(
+                      'Make sure your OpenAI API key is set in the bottom left of the sidebar.',
+                    ),
+                    t(
+                      'If you completed this step, OpenAI may be experiencing issues.',
+                    ),
+                  ],
+            } as ErrorMessage);
+      },
+      [t],
+    ),
   };
 };
 
