e6879303b2ed66ce95d7a5b3a4d9ab71ed8b3ab9
Nikita Butsko <nikita_butsko@epam.com>
2023-07-06 13:25:26 +0200

    fix: remove consolelog

--- pages/api/models.ts
+++ pages/api/models.ts
@@ -98,7 +98,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     });
 
     for (const entity of [...models, ...applications, ...assistants]) {
-      console.log(entity)
       entities.push({
         id: entity.id,
         name: OpenAIEntityModels[entity.id as OpenAIEntityModelID]?.name || entity.id,
