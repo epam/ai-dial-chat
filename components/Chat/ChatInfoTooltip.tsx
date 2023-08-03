4b56d7ba92ee6c8b48b089b5686476815089bdf1
Mikita Butsko <mikita_butsko@epam.com>
2023-08-03 18:13:17 +0200

    fix: remove unused stuff

--- components/Chat/ChatInfoTooltip.tsx
+++ components/Chat/ChatInfoTooltip.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { ModelIconMappingType } from '@/types/icons';
