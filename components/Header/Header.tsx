25be440b6b830066afb6fc356962df3b9ddabfe4
Mikita Butsko <mikita_butsko@epam.com>
2023-08-17 16:26:38 +0200

    fix: fix centering logo  on tablet

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -80,13 +80,13 @@ const Header = () => {
       <CreateNewChatMobile />
       <div className="flex grow justify-between">
         <span
-          className="min-w-[110px] grow bg-left bg-no-repeat dark:hidden md:ml-5 md:grow-0"
+          className="min-w-[110px] grow bg-center bg-no-repeat dark:hidden md:ml-5 md:grow-0 lg:bg-left"
           style={{
             backgroundImage: `var(--app-logo)`,
           }}
         ></span>
         <span
-          className="hidden min-w-[110px] grow bg-left bg-no-repeat dark:block md:ml-5 md:grow-0"
+          className="hidden min-w-[110px] grow bg-center bg-no-repeat dark:block md:ml-5 md:grow-0 lg:bg-left"
           style={{
             backgroundImage: `var(--app-logo-dark)`,
           }}
