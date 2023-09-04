20c699b400b2d257c0a6a999343f0701eb9f809a
Mikita Butsko <mikita_butsko@epam.com>
2023-09-04 18:22:04 +0200

    fix: fix global case

--- utils/app/auth/nextauth-client.ts
+++ utils/app/auth/nextauth-client.ts
@@ -1,12 +1,11 @@
 import { Client } from 'openid-client';
 
 class NextClient {
-  private static _client: Client | null = null;
   public static set client(clientLocal: Client | null) {
-    NextClient._client = clientLocal;
+    (globalThis as unknown as any)._client = clientLocal;
   }
   public static get client(): Client | null {
-    return NextClient._client;
+    return (globalThis as unknown as any)._client;
   }
 }
 
