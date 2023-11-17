daf39a0727b08769dad64d81c2c16ddc9daf650d
Aliaksei Kryvasheyeu <aliaksei_kryvasheyeu@epam.com>
2023-11-17 09:59:45 +0100

    chore: changes pino conf

--- src/utils/server/logger.ts
+++ src/utils/server/logger.ts
@@ -1,10 +1,4 @@
 import pino from 'pino';
+import pretty from 'pino-pretty';
 
-export const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-    },
-  },
-});
+export const logger = pino(pretty({colorize: true}));
