4f00272992de8d0994ef08ad530305aff7a4b949
Ilya Bondar <ilya_bondar@epam.com>
2023-11-22 20:22:50 +0100

    redesign like #88

--- src/components/Chat/NotAllowedModel.tsx
+++ src/components/Chat/NotAllowedModel.tsx
@@ -1,13 +1,18 @@
 import { useTranslation } from 'next-i18next';
 
 import { EntityType } from '@/src/types/common';
+import { IconExclamationCircle } from '@tabler/icons-react';
+import { ChatInputFooter } from './ChatInput/ChatInputFooter';
 
 export const NotAllowedModel = ({ type = EntityType.Model }) => {
   const { t } = useTranslation('chat');
 
   return (
-    <div className="absolute bottom-0 flex w-full items-center justify-center rounded bg-red-200 px-10 py-8 text-center text-base text-red-800 dark:bg-red-900 dark:text-red-400">
-      <span>{t('chat.error.incorrect-selected', { context: type })}</span>
+    <div className="absolute bottom-0 flex w-full flex-col items-center justify-center">
+      <div className="flex w-full items-center gap-2 rounded bg-red-200 p-4 text-base text-red-800 dark:bg-red-900 dark:text-red-400 lg:max-w-3xl">
+        <IconExclamationCircle size={24}/><span> {t('chat.error.incorrect-selected', { context: type })}</span>
+      </div>
+      <ChatInputFooter />
     </div>
   );
 };
