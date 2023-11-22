c93b3b6e7f716335ad38835b4796b647ed35fa2b
Ilya Bondar <ilya_bondar@epam.com>
2023-11-22 20:25:19 +0100

    lint fixes

--- src/components/Chat/NotAllowedModel.tsx
+++ src/components/Chat/NotAllowedModel.tsx
@@ -1,7 +1,9 @@
+import { IconExclamationCircle } from '@tabler/icons-react';
+
 import { useTranslation } from 'next-i18next';
 
 import { EntityType } from '@/src/types/common';
-import { IconExclamationCircle } from '@tabler/icons-react';
+
 import { ChatInputFooter } from './ChatInput/ChatInputFooter';
 
 export const NotAllowedModel = ({ type = EntityType.Model }) => {
@@ -10,7 +12,8 @@ export const NotAllowedModel = ({ type = EntityType.Model }) => {
   return (
     <div className="absolute bottom-0 flex w-full flex-col items-center justify-center">
       <div className="flex w-full items-center gap-2 rounded bg-red-200 p-4 text-base text-red-800 dark:bg-red-900 dark:text-red-400 lg:max-w-3xl">
-        <IconExclamationCircle size={24}/><span> {t('chat.error.incorrect-selected', { context: type })}</span>
+        <IconExclamationCircle size={24} />
+        <span> {t('chat.error.incorrect-selected', { context: type })}</span>
       </div>
       <ChatInputFooter />
     </div>
