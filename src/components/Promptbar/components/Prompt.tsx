928d73ba2b701258eda4b9d4f43f46842437899d
Ilya Bondar <ilya_bondar@epam.com>
2023-11-27 17:23:07 +0100

    fix: fix displaying the prompt context menu (#238) (issue #235)

--- src/components/Promptbar/components/Prompt.tsx
+++ src/components/Promptbar/components/Prompt.tsx
@@ -270,8 +270,8 @@ export const PromptComponent = ({ item: prompt, level }: Props) => {
             ref={refs.setFloating}
             {...getFloatingProps()}
             className={classNames(
-              'absolute right-3 z-50 flex justify-end xl:invisible xl:group-hover:visible',
-              isContextMenu ? 'visible' : 'invisible',
+              'absolute right-3 z-50 flex justify-end group-hover:visible',
+              isSelected || isContextMenu ? 'visible' : 'invisible',
             )}
             onClick={stopBubbling}
           >
