5d258fe9e77f38aaea3cb754cfff0824552a992e
Mikita Butsko <mikita_butsko@epam.com>
2023-12-04 19:03:53 +0100

    fix: change content type to application/x-ndjson

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -144,7 +144,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       jobTitle: token?.jobTitle as string,
     });
     res.setHeader('Transfer-Encoding', 'chunked');
-    res.setHeader('Content-Type', 'application/octet-stream');
+    res.setHeader('Content-Type', 'application/x-ndjson');
 
     const reader = stream.getReader();
     const processStream = async () => {
