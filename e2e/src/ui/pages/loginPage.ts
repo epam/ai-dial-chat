68e92b8766fb01e65bd4c938861d0e30eff2f524
Irina_Kartun <irina.kartun@gmail.com>
2023-12-15 18:19:04 +0100

    e2e/update_login_method: added auth0 login for e2e tests

--- e2e/src/ui/pages/loginPage.ts
+++ e2e/src/ui/pages/loginPage.ts
@@ -3,16 +3,21 @@ import { LoginSelectors } from '../selectors';
 import { BaseElement } from '../webElements';
 import { BasePage } from './basePage';
 
+import { Tags } from '@/e2e/src/ui/domData';
 import * as process from 'process';
 
 export class LoginPage extends BasePage {
   private tokenInput = new BaseElement(this.page, LoginSelectors.token);
+  public ssoSignInButton = new BaseElement(
+    this.page,
+    `${Tags.button}.${Tags.button}`,
+  );
   private signInButton = new BaseElement(
     this.page,
     LoginSelectors.signIn,
   ).getElementLocatorByText(ExpectedConstants.signInButtonTitle);
 
-  async loginToChatBot() {
+  async loginToChatBotWithToken() {
     const token = process.env.PREVIEW_TEST_TOKEN
       ? process.env.PREVIEW_TEST_TOKEN!.split(',')[0]
       : process.env.AUTH_TEST_TOKEN!.split(',')[0];
