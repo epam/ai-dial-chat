6ed2a3aa26badacfd32649dc9ac6aa2b7341d0ff
irinakartun <irina_kartun@epam.com>
2023-12-22 13:43:49 +0300

    e2e: fixed signin locator to fit all providers (#413)
    Co-authored-by: Irina_Kartun <irina.kartun@gmail.com>

--- e2e/src/ui/pages/loginPage.ts
+++ e2e/src/ui/pages/loginPage.ts
@@ -1,11 +1,8 @@
 import { BaseElement } from '../webElements';
 import { BasePage } from './basePage';
 
-import { Tags } from '@/e2e/src/ui/domData';
+import { LoginSelectors } from '@/e2e/src/ui/selectors';
 
 export class LoginPage extends BasePage {
-  public ssoSignInButton = new BaseElement(
-    this.page,
-    `${Tags.button}.${Tags.button}`,
-  );
+  public ssoSignInButton = new BaseElement(this.page, LoginSelectors.ssoSignIn);
 }
