109f015b356dc8a059a50f9d6531c52ac03b1a61
Aliaksandr Kezik <a.kezzk@gmail.com>
2023-12-29 13:43:01 +0100

    number of visible stages as a constant

--- src/components/Chat/MessageStages.tsx
+++ src/components/Chat/MessageStages.tsx
@@ -9,10 +9,15 @@ export interface Props {
   stages: Stage[];
 }
 
+const NUMBER_OF_VISIBLE_STAGES = 3;
+
 export const MessageStages = ({ stages }: Props) => {
   const [showAll, setShowAll] = useState(false);
 
-  const displayedStages = stages.slice(0, showAll ? stages.length : 3);
+  const displayedStages = stages.slice(
+    0,
+    showAll ? stages.length : NUMBER_OF_VISIBLE_STAGES,
+  );
 
   return (
     <div className="flex flex-col gap-1">
@@ -20,7 +25,7 @@ export const MessageStages = ({ stages }: Props) => {
         <MessageStage key={stage.index} stage={stage} />
       ))}
 
-      {stages.length > 3 && (
+      {stages.length > NUMBER_OF_VISIBLE_STAGES && (
         <button onClick={() => setShowAll(!showAll)} className="mx-auto mt-2">
           {showAll ? (
             <IconChevronsUp size={24} className="text-secondary" />
