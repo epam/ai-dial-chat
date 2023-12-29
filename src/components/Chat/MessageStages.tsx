b4404dfe1135e171db686bca14e46c6f02ef1c3d
Aliaksandr Kezik <a.kezzk@gmail.com>
2023-12-29 13:30:46 +0100

    feat: need to group stages if there are a lot of them (Issue #429)

--- src/components/Chat/MessageStages.tsx
+++ src/components/Chat/MessageStages.tsx
@@ -1,3 +1,6 @@
+import { IconChevronsDown, IconChevronsUp } from '@tabler/icons-react';
+import { useState } from 'react';
+
 import { Stage } from '@/src/types/chat';
 
 import { MessageStage } from './MessageStage';
@@ -7,11 +10,25 @@ export interface Props {
 }
 
 export const MessageStages = ({ stages }: Props) => {
+  const [showAll, setShowAll] = useState(false);
+
+  const displayedStages = stages.slice(0, showAll ? stages.length : 3);
+
   return (
     <div className="flex flex-col gap-1">
-      {stages.map((stage) => (
+      {displayedStages.map((stage) => (
         <MessageStage key={stage.index} stage={stage} />
       ))}
+
+      {stages.length > 3 && (
+        <button onClick={() => setShowAll(!showAll)} className="mx-auto mt-2">
+          {showAll ? (
+            <IconChevronsUp size={24} className="text-secondary" />
+          ) : (
+            <IconChevronsDown size={24} className="text-secondary" />
+          )}
+        </button>
+      )}
     </div>
   );
 };
