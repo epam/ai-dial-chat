ba213fbc7ec625cf442afd8cb57f2295042f5443
Aliaksandr Kezik <a.kezzk@gmail.com>
2023-12-29 19:20:53 +0100

    fix: icon is not changed to default if changed conversation entity to entity with default icon (Issue #450)

--- src/components/Chatbar/components/ModelIcon.tsx
+++ src/components/Chatbar/components/ModelIcon.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import { memo } from 'react';
 import SVG from 'react-inlinesvg';
 
@@ -34,17 +33,18 @@ const ModelIconTemplate = memo(
       >
         <SVG
           src={entity?.iconUrl || ''}
+          className={classNames(!entity?.iconUrl && 'hidden')}
           width={size}
           height={size}
           description={entity?.name || entityId}
-        >
-          <SVG
-            src={fallbackUrl}
-            width={size}
-            height={size}
-            description={entity?.name || entityId}
-          />
-        </SVG>
+        />
+        <SVG
+          src={fallbackUrl}
+          className={classNames(entity?.iconUrl && 'hidden')}
+          width={size}
+          height={size}
+          description={entity?.name || entityId}
+        />
       </span>
     );
   },
