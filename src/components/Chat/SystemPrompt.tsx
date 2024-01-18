7024c73bad508c4baf188b23453630427bbfaa43
Aliaksandr Kezik <a.kezzk@gmail.com>
2024-01-18 13:28:41 +0100

    resolve warnings

--- src/components/Chat/SystemPrompt.tsx
+++ src/components/Chat/SystemPrompt.tsx
@@ -1,4 +1,11 @@
-import { FC, KeyboardEvent, useCallback, useEffect, useRef } from 'react';
+import {
+  ChangeEvent,
+  FC,
+  KeyboardEvent,
+  useCallback,
+  useEffect,
+  useRef,
+} from 'react';
 
 import { useTranslation } from 'next-i18next';
 
@@ -55,7 +62,7 @@ export const SystemPrompt: FC<Props> = ({
   } = usePromptSelection(maxLength);
 
   const handleChange = useCallback(
-    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    (e: ChangeEvent<HTMLTextAreaElement>) => {
       const value = e.target.value;
 
       if (value.length > maxLength && value.length >= content.length) {
@@ -69,6 +76,7 @@ export const SystemPrompt: FC<Props> = ({
       onChangePrompt(value);
     },
     [
+      content.length,
       maxLength,
       onChangePrompt,
       setContent,
