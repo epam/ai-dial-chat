032d717b05209c56541c0cd7b7f1a89ba7e5cae0
Alexander <98586297+Alexander-Kezik@users.noreply.github.com>
2024-01-19 17:03:23 +0100

    fix: fix issues with elements highlight in attachments functionality (Issue #533) (#576)

--- src/components/Files/FileManagerModal.tsx
+++ src/components/Files/FileManagerModal.tsx
@@ -333,6 +333,7 @@ export const FileManagerModal = ({
                               onFileUpload={handleUploadFile}
                               onRenameFolder={handleRenameFolder}
                               onItemEvent={handleItemCallback}
+                              withBorderHighlight={false}
                             />
                           </div>
                         );
