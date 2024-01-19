1f223aa2d0bee33edb8adb01059b9e80dad40dcf
Mikita Butsko <mikita_butsko@epam.com>
2024-01-19 15:11:49 +0100

    fix: remove echo timeout

--- startup.sh
+++ startup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-KEEP_ALIVE_TIMEOUT="${KEEP_ALIVE_TIMEOUT:-5000}"
-
-echo KEEP_ALIVE_TIMEOUT
+# With alb 
+KEEP_ALIVE_TIMEOUT="${KEEP_ALIVE_TIMEOUT:-61000}"
 
 exec npm start -- --keepAliveTimeout $KEEP_ALIVE_TIMEOUT
