7f03d14fcad117892215278268745423869486f1
Mikita Butsko <mikita_butsko@epam.com>
2024-02-02 15:07:11 +0100

    fix: fix pino pretty module

--- apps/chat/src/utils/server/logger.ts
+++ apps/chat/src/utils/server/logger.ts
@@ -1,9 +1,13 @@
 import pino from 'pino';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import pretty from 'pino-pretty';
 
-const stream = pretty({
-  colorize: true,
-  messageFormat: '{msg} [trace_id={trace_id}, span_id={span_id}]',
+export const logger = pino({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      messageFormat: '{msg} [trace_id={trace_id}, span_id={span_id}]',
+    },
+  },
 });
-
-export const logger = pino(stream);
