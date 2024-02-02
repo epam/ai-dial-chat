5f8a2483531db141df5c116a363a93b4aa67fb74
Mikita Butsko <mikita_butsko@epam.com>
2024-02-02 14:47:23 +0100

    fix: fix pino pretty

--- apps/chat/src/utils/server/logger.ts
+++ apps/chat/src/utils/server/logger.ts
@@ -1,11 +1,9 @@
 import pino from 'pino';
+import pretty from 'pino-pretty';
 
-export const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      messageFormat: '{msg} [trace_id={trace_id}, span_id={span_id}]',
-    },
-  },
+const stream = pretty({
+  colorize: true,
+  messageFormat: '{msg} [trace_id={trace_id}, span_id={span_id}]',
 });
+
+export const logger = pino(stream);
