9ea533f3c2c8cb643a4f7e2ab866ed4e86fb0d8d
Ilya Bondar <ilya_bondar@epam.com>
2024-02-06 22:16:27 +0100

    Update NotFoundEntity.tsx

--- apps/chat/src/components/Common/NotFoundEntity.tsx
+++ apps/chat/src/components/Common/NotFoundEntity.tsx
@@ -2,19 +2,31 @@ import { IconReload } from '@tabler/icons-react';
 
 import { useTranslation } from 'next-i18next';
 
-import { Translation } from '@/src/types/translation';
 import classNames from 'classnames';
 
+import { Translation } from '@/src/types/translation';
+
 interface Props {
-  className?: string;
   entity: string;
+  containerClassName?: string;
+  dataQa?: string;
 }
 
-export const NotFoundEntity = ({ entity }: Props) => {
+export const NotFoundEntity = ({
+  entity,
+  containerClassName,
+  dataQa = 'not-found',
+}: Props) => {
   const { t } = useTranslation(Translation.Common);
 
   return (
-    <div className={classNames("flex h-full flex-col items-center justify-center", className)}>
+    <div
+      className={classNames(
+        'flex flex-col items-center justify-center',
+        containerClassName || 'h-full',
+      )}
+      data-qa={dataQa}
+    >
       <IconReload stroke={1} className="text-secondary" size={60} />
       <p className="mt-3 text-center text-sm">{t(`${entity} not found.`)}</p>
       <p className="mt-1 text-center text-sm">{t('Please reload the page.')}</p>
