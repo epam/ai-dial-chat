d1f1f710461fea6256903e93840e32020f7835fd
Aliaksandr Kezik <a.kezzk@gmail.com>
2024-02-08 09:44:28 +0100

    add spinner to system prompt

--- apps/chat/src/components/Chat/SystemPrompt.tsx
+++ apps/chat/src/components/Chat/SystemPrompt.tsx
@@ -9,6 +9,8 @@ import {
 
 import { useTranslation } from 'next-i18next';
 
+import classNames from 'classnames';
+
 import { usePromptSelection } from '@/src/hooks/usePromptSelection';
 
 import { getPromptLimitDescription } from '@/src/utils/app/modals';
@@ -19,6 +21,7 @@ import { Translation } from '@/src/types/translation';
 import { DEFAULT_SYSTEM_PROMPT } from '@/src/constants/default-settings';
 
 import { ConfirmDialog } from '@/src/components/Common/ConfirmDialog';
+import { Spinner } from '@/src/components/Common/Spinner';
 
 import { PromptDialog } from './ChatInput/PromptDialog';
 import { PromptList } from './ChatInput/PromptList';
@@ -158,18 +161,28 @@ export const SystemPrompt: FC<Props> = ({
   return (
     <div className="flex flex-col">
       <label className="mb-4 text-left">{t('System prompt')}</label>
-      <textarea
-        ref={textareaRef}
-        className="w-full resize-none overflow-y-auto rounded border border-primary bg-transparent px-4 py-3 outline-none placeholder:text-secondary focus-within:border-accent-primary"
-        placeholder={t('Type a text or «/» to use a prompt...') || ''}
-        style={{ maxHeight: `${MAX_HEIGHT}px` }}
-        value={content}
-        rows={1}
-        disabled={isPromptLoading}
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-        data-qa="system-prompt"
-      />
+      <div className="relative flex flex-col">
+        <textarea
+          ref={textareaRef}
+          className="w-full resize-none overflow-y-auto rounded border border-primary bg-transparent px-4 py-3 outline-none placeholder:text-secondary focus-within:border-accent-primary"
+          placeholder={t('Type a text or «/» to use a prompt...') || ''}
+          style={{ maxHeight: `${MAX_HEIGHT}px` }}
+          value={content}
+          rows={1}
+          disabled={isPromptLoading}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          data-qa="system-prompt"
+        />
+        <span
+          className={classNames(
+            'absolute bottom-2 right-3 rounded bg-layer-2 p-[3px]',
+            !isPromptLoading && 'hidden',
+          )}
+        >
+          <Spinner size={24} />
+        </span>
+      </div>
 
       {showPromptList && filteredPrompts.length > 0 && (
         <div>
