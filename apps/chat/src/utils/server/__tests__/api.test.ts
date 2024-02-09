508d7a330883a8fb45344480ed29abbd8bd2f719
Ilya Bondar <ilya_bondar@epam.com>
2024-02-09 19:08:18 +0100

    added modelId encoding

---
+++ apps/chat/src/utils/server/__tests__/api.test.ts
@@ -0,0 +1,10 @@
+import { decodeModelId, encodeModelId } from '../api';
+
+describe('decodeModelId and encodeModelId', () => {
+  it.each(['gpt_4', 'gpt__4', 'gpt%5F%5F4', `gpt${encodeURI('%5F%5F')}4`])(
+    'decodeModelId(encodeModelId(%s))',
+    (path: string) => {
+      expect(decodeModelId(encodeModelId(path))).toBe(path);
+    },
+  );
+});
--- apps/chat/src/utils/server/api.ts
+++ apps/chat/src/utils/server/api.ts
@@ -54,9 +54,22 @@ export const getEntityUrlFromSlugs = (
 };
 
 const pathKeySeparator = '__';
+const encodedKeySeparator = '%5F%5F';
 
 export const combineApiKey = (...args: (string | number)[]) =>
   args.join(pathKeySeparator);
+
+export const encodeModelId = (modelId: string) =>
+  modelId
+    .split(pathKeySeparator)
+    .map((i) => encodeURI(i))
+    .join(encodedKeySeparator);
+
+export const decodeModelId = (modelKey: string) =>
+  modelKey
+    .split(encodedKeySeparator)
+    .map((i) => decodeURI(i))
+    .join(pathKeySeparator);
 
 enum PseudoModel {
   Replay = 'replay',
@@ -79,7 +92,7 @@ export const getConversationApiKey = (
     return conversation.name;
   }
   return combineApiKey(
-    getModelApiIdFromConversation(conversation as Conversation),
+    encodeModelId(getModelApiIdFromConversation(conversation as Conversation)),
     conversation.name,
   );
 };
@@ -91,7 +104,7 @@ export const parseConversationApiKey = (apiKey: string): ConversationInfo => {
   const [modelId, name] =
     parts.length < 2
       ? [EMPTY_MODEL_ID, apiKey] // receive without postfix with model i.e. {name}
-      : [parts[0], parts.slice(1).join(pathKeySeparator)]; // receive correct format {modelId}__{name}
+      : [decodeModelId(parts[0]), parts.slice(1).join(pathKeySeparator)]; // receive correct format {modelId}__{name}
 
   return {
     id: name,
