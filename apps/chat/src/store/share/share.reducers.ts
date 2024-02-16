e79385c549abe82447eaeed1fb2026fde058b890
Mikita Butsko <mikita_butsko@epam.com>
2024-02-16 11:22:04 +0100

    fix: fix share modal name display

--- apps/chat/src/store/share/share.reducers.ts
+++ apps/chat/src/store/share/share.reducers.ts
@@ -1,6 +1,8 @@
 import { PayloadAction, createSelector, createSlice } from '@reduxjs/toolkit';
 
 import { getFileName } from '@/src/utils/app/file';
+import { splitEntityId } from '@/src/utils/app/folders';
+import { parseConversationApiKey } from '@/src/utils/server/api';
 
 import { ConversationInfo } from '@/src/types/chat';
 import {
@@ -49,7 +51,12 @@ export const shareSlice = createSlice({
     ) => {
       state.invitationId = undefined;
       state.shareModalState = ModalState.LOADING;
-      state.shareResourceName = getFileName(payload.resourceId);
+
+      const name = splitEntityId(payload.resourceId).name;
+      state.shareResourceName =
+        payload.resourceType === BackendResourceType.CONVERSATION
+          ? parseConversationApiKey(splitEntityId(payload.resourceId).name).name
+          : name;
     },
     sharePrompt: (
       state,
