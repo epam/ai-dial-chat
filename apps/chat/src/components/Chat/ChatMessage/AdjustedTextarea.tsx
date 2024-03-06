153e94e5e3f25731b591194b388c25d752e330f9
Ilya Bondar <ilya_bondar@epam.com>
2024-03-07 00:36:51 +0100

    add maxHeight support and reuse in similar places

--- apps/chat/src/components/Chat/ChatMessage/AdjustedTextarea.tsx
+++ apps/chat/src/components/Chat/ChatMessage/AdjustedTextarea.tsx
@@ -5,20 +5,27 @@ import classNames from 'classnames';
 type Props = React.DetailedHTMLProps<
   React.TextareaHTMLAttributes<HTMLTextAreaElement>,
   HTMLTextAreaElement
->;
+> & {
+  maxHeight?: number;
+};
 
 export const AdjustedTextarea = React.forwardRef((props: Props, ref) => {
   const hiddenTextareaRef = useRef<HTMLTextAreaElement>(null);
   const mainTextareaRef = useRef<HTMLTextAreaElement>(null);
-  useImperativeHandle(ref, () => mainTextareaRef.current);
+  useImperativeHandle(ref, () => mainTextareaRef.current); // proxy ref
 
-  const { value, className } = props;
+  const { value, className, maxHeight = 100000 } = props;
 
   useEffect(() => {
     if (hiddenTextareaRef.current && mainTextareaRef.current) {
-      mainTextareaRef.current.style.height = `${hiddenTextareaRef.current.scrollHeight}px`;
+      hiddenTextareaRef.current.style.width = `${mainTextareaRef.current.clientWidth}px`; // adapt width (parent paddings don't work for absolute element)
+      const scrollHeight = hiddenTextareaRef.current.scrollHeight;
+      mainTextareaRef.current.style.height = `${scrollHeight}px`; // set height as scrollHeight of hidden element
+      mainTextareaRef.current.style.overflowY = `${
+        scrollHeight > maxHeight ? 'auto' : 'hidden' // show vertical scroll if max height was achieved
+      }`;
     }
-  }, [value]);
+  }, [maxHeight, value]);
 
   return (
     <>
@@ -28,11 +35,16 @@ export const AdjustedTextarea = React.forwardRef((props: Props, ref) => {
         name="hidden"
         className={classNames(
           className,
-          'absolute left-[-10000px] top-[-10000px]',
+          'absolute left-[-10000px] top-[-10000px]', // hidden, but exists
         )}
         data-qa="hidden-textarea"
       />
-      <textarea {...props} ref={mainTextareaRef} name="main" />
+      <textarea
+        {...props}
+        ref={mainTextareaRef}
+        name="main"
+        style={{ maxHeight: `${maxHeight}px` }}
+      />
     </>
   );
 });
