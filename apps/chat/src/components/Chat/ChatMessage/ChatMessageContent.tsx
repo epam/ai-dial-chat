0b6bc27a4951a848a3dd46e8229d35aa7417619d
Alexander <98586297+Alexander-Kezik@users.noreply.github.com>
2024-03-06 15:43:07 +0100

    fix(chat): add \r to message splitting while editing (Issue #916) (#947)

--- apps/chat/src/components/Chat/ChatMessage/ChatMessageContent.tsx
+++ apps/chat/src/components/Chat/ChatMessage/ChatMessageContent.tsx
@@ -218,7 +218,7 @@ export const ChatMessageContent = ({
 
   useEffect(() => {
     if (textareaRef.current) {
-      textareaRef.current.rows = messageContent.split('\n').length;
+      textareaRef.current.rows = messageContent.split(/\r\n|\r|\n/).length;
     }
   }, [isEditing, messageContent]);
 
