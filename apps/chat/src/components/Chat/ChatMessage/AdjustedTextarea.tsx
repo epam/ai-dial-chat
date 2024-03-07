cf3ced2afb86a47dd0885a31f419382c727fc612
Aliaksandr Kezik <a.kezzk@gmail.com>
2024-03-07 15:35:13 +0100

    fix AdjustedTextarea

--- apps/chat/src/components/Chat/ChatMessage/AdjustedTextarea.tsx
+++ apps/chat/src/components/Chat/ChatMessage/AdjustedTextarea.tsx
@@ -9,45 +9,48 @@ type Props = React.DetailedHTMLProps<
   maxHeight?: number;
 };
 
-export const AdjustedTextarea = React.forwardRef(
-  ({ value, maxHeight = 10000, className, ...props }: Props, ref) => {
-    const hiddenTextareaRef = useRef<HTMLTextAreaElement>(null);
-    const mainTextareaRef = useRef<HTMLTextAreaElement>(null);
-    useImperativeHandle(ref, () => mainTextareaRef.current); // proxy ref
+export const AdjustedTextarea = React.forwardRef((props: Props, ref) => {
+  const hiddenTextareaRef = useRef<HTMLTextAreaElement>(null);
+  const mainTextareaRef = useRef<HTMLTextAreaElement>(null);
+  useImperativeHandle(ref, () => mainTextareaRef.current); // proxy ref
 
-    useEffect(() => {
-      if (hiddenTextareaRef.current && mainTextareaRef.current) {
-        hiddenTextareaRef.current.style.width = `${mainTextareaRef.current.clientWidth}px`; // adapt width (parent paddings don't work for absolute element)
-        const scrollHeight = hiddenTextareaRef.current.scrollHeight;
-        mainTextareaRef.current.style.height = `${scrollHeight}px`; // set height as scrollHeight of hidden element
-        mainTextareaRef.current.style.overflowY = `${
-          scrollHeight > maxHeight ? 'auto' : 'hidden' // show vertical scroll if max height was achieved
-        }`;
-      }
-    }, [maxHeight, value]);
+  const { value, maxHeight = 10000, className, ...restProps } = props;
 
-    return (
-      <>
-        <textarea
-          {...props}
-          ref={hiddenTextareaRef}
-          name="hidden"
-          className={classNames(
-            className,
-            'invisible absolute', // hidden, but exists
-          )}
-          data-qa="hidden-textarea"
-        />
-        <textarea
-          data-qa="chat-textarea"
-          {...props}
-          ref={mainTextareaRef}
-          name="main"
-          style={{ maxHeight: `${maxHeight}px` }}
-        />
-      </>
-    );
-  },
-);
+  useEffect(() => {
+    if (hiddenTextareaRef.current && mainTextareaRef.current) {
+      hiddenTextareaRef.current.style.width = `${mainTextareaRef.current.clientWidth}px`; // adapt width (parent paddings don't work for absolute element)
+      const scrollHeight = hiddenTextareaRef.current.scrollHeight;
+      mainTextareaRef.current.style.height = `${scrollHeight}px`; // set height as scrollHeight of hidden element
+      mainTextareaRef.current.style.overflowY = `${
+        scrollHeight > maxHeight ? 'auto' : 'hidden' // show vertical scroll if max height was achieved
+      }`;
+    }
+  }, [maxHeight, value]);
+
+  return (
+    <>
+      <textarea
+        {...restProps}
+        ref={hiddenTextareaRef}
+        name="hidden"
+        className={classNames(
+          className,
+          'invisible absolute', // hidden, but exists
+        )}
+        data-qa="hidden-textarea"
+        value={value}
+      />
+      <textarea
+        data-qa="chat-textarea"
+        className={className}
+        value={value}
+        {...restProps}
+        ref={mainTextareaRef}
+        name="main"
+        style={{ maxHeight: `${maxHeight}px` }}
+      />
+    </>
+  );
+});
 
 AdjustedTextarea.displayName = 'AdjustedTextarea';
