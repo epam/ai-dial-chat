32f1c0d306a49464dfa76a9b4e53e51c7977c076
Aliaksandr Kezik <a.kezzk@gmail.com>
2024-04-23 11:36:45 +0200

    revert

--- apps/chat/src/utils/app/file.ts
+++ apps/chat/src/utils/app/file.ts
@@ -123,7 +123,7 @@ export const getFilesWithInvalidFileType = (
     ? []
     : files.filter((file) => !isAllowedMimeType(allowedFileTypes, file.type));
 };
-export const notAllowedSymbols = ':;,=/{}%&\\\x00-\x1F';
+export const notAllowedSymbols = ':;,=/{}%&\\';
 export const notAllowedSymbolsRegex = new RegExp(
   `[${escapeRegExp(notAllowedSymbols)}]|(\r\n|\n|\r|\t)|[\x00-\x1F]`,
   'gm',
