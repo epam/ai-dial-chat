c19e7da678a3af6095e01346f0360f1c59b59a20
Ilya Bondar <ilya_bondar@epam.com>
2024-04-23 23:02:51 +0200

    fix e2e-tests

--- apps/chat-e2e/src/tests/chatApi/arithmeticRequest.test.ts
+++ apps/chat-e2e/src/tests/chatApi/arithmeticRequest.test.ts
@@ -28,7 +28,6 @@ const modelsForArithmeticRequest: {
   { modelId: ModelIds.AI21_JUMBO, isSysPromptAllowed: true },
   { modelId: ModelIds.ANTHROPIC_CLAUDE_V2, isSysPromptAllowed: true },
   { modelId: ModelIds.ANTHROPIC_CLAUDE_V21, isSysPromptAllowed: true },
-  { modelId: ModelIds.ANTHROPIC_CLAUDE_V3, isSysPromptAllowed: true },
   {
     modelId: ModelIds.ANTHROPIC_CLAUDE_INSTANT_V1,
     isSysPromptAllowed: true,
