e66ec8372a37381142e2137116f13df8fc89c73d
Derikyan <armen.derikyan@gmail.com>
2024-05-15 22:03:03 +0400

    fix(chat):Use prettier for foratting (Issue #1191)

--- apps/chat/src/components/Common/ReplaceConfirmationModal/ReplaceConfirmationModal.tsx
+++ apps/chat/src/components/Common/ReplaceConfirmationModal/ReplaceConfirmationModal.tsx
@@ -302,7 +302,7 @@ export const ReplaceConfirmationModal = ({ isOpen }: Props) => {
       dismissProps={{ outsidePressEvent: 'mousedown' }}
     >
       <div className="flex h-full flex-col justify-between gap-2 sm:gap-4">
-        <div className="flex h-[90%] flex-col gap-4 px-3 py-4 md:p-6 pb-0">
+        <div className="flex h-[90%] flex-col gap-4 px-3 py-4 pb-0 md:p-6">
           <div className="flex h-fit flex-col gap-2">
             <h2 className="text-base font-semibold">
               {t('Some items failed to import due to duplicate names')}
