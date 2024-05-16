b61b6d9c7973259b95582cbc3196a965499a8d78
Mikhail Hahalushka <Mikhail_Hahalushka@epam.com>
2024-05-16 14:27:38 +0400

    get rid of !important

--- apps/chat/src/components/Chat/ModelList.tsx
+++ apps/chat/src/components/Chat/ModelList.tsx
@@ -111,7 +111,7 @@ const ModelGroup = ({
                 : currentEntity.name}
             </span>
             <ModelVersionSelect
-              className="absolute right-3 !h-auto"
+              className="absolute right-3 h-max"
               entities={entities}
               onSelect={onSelect}
               currentEntity={currentEntity}
