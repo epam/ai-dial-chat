8dd439701ccd7bfc39553062653c9b6b4e3c99ee
Ilya Bondar <ilya_bondar@epam.com>
2024-05-28 14:19:56 +0200

    fix for playback

--- apps/chat/src/store/share/share.epics.ts
+++ apps/chat/src/store/share/share.epics.ts
@@ -164,7 +164,11 @@ const shareConversationFolderEpic: AppEpic = (action$) =>
         map((res) => res.filter(Boolean) as Conversation[]),
         switchMap((conversations: Conversation[]) => {
           const internalResourcesIds = conversations
-            .flatMap((res) => getInternalResourcesUrls(res.messages))
+            .flatMap((res) =>
+              getInternalResourcesUrls(
+                res.playback?.messagesStack || res.messages,
+              ),
+            )
             .map((url) => ({ url }));
 
           return ShareService.share({
