b6eb32790c2c8d0c47edede83f93bea07c69f412
Maksim Nartov <nartovm1994@gmail.com>
2024-06-14 01:37:13 +0200

    It works but it should be checked

--- apps/chat-e2e/src/tests/promptFolderNamesakesTest.test.ts
+++ apps/chat-e2e/src/tests/promptFolderNamesakesTest.test.ts
@@ -66,7 +66,7 @@ dialTest(
   },
 );
 
-dialTest.only(
+dialTest(
   'Prompt folder: error message appears if to drag prompt folder IN to another folder where folder with the same name exists',
   async ({
     dialHomePage,
@@ -121,7 +121,7 @@ dialTest.only(
         await promptBar.dragAndDropEntityToFolder(
           folderPrompts.getFolderByName(duplicatedFolderName, 2),
           folderPrompts.getFolderByName(
-            ExpectedConstants.newFolderWithIndexTitle(1)
+            ExpectedConstants.newFolderWithIndexTitle(1),
           ),
         );
 
@@ -137,6 +137,177 @@ dialTest.only(
           .toBe(
             ExpectedConstants.duplicatedFolderNameErrorMessage(
               duplicatedFolderName,
+            ),
+          );
+      },
+    );
+  },
+);
+
+dialTest(
+  'Prompt folder: error message appears if to drag prompt folder OUT to another folder where the same name exists',
+  async ({
+    dialHomePage,
+    promptBar,
+    folderPrompts,
+    errorToast,
+    setTestIds,
+  }) => {
+    setTestIds('EPMRTC-2971');
+    const duplicatedFolderName = 'New folder 2';
+
+    await dialTest.step('Create 3 folders', async () => {
+      await dialHomePage.openHomePage();
+      await dialHomePage.waitForPageLoaded();
+      for (let i = 1; i <= 3; i++) {
+        await promptBar.createNewFolder();
+        await expect
+          .soft(
+            folderPrompts.getFolderByName(
+              ExpectedConstants.newFolderWithIndexTitle(i),
+            ),
+            ExpectedMessages.folderIsVisible,
+          )
+          .toBeVisible();
+      }
+    });
+
+    await dialTest.step('Create nested structure', async () => {
+      for (let i = 3; i >= 2; i--) {
+        await promptBar.dragAndDropEntityToFolder(
+          folderPrompts.getFolderByName(
+            ExpectedConstants.newFolderWithIndexTitle(i),
+          ),
+          folderPrompts.getFolderByName(
+            ExpectedConstants.newFolderWithIndexTitle(i - 1),
+          ),
+        );
+      }
+    });
+
+    await dialTest.step('Expand folders', async () => {
+      await folderPrompts.expandFolder(
+        ExpectedConstants.newPromptFolderWithIndexTitle(2),
+      );
+    });
+
+    await dialTest.step('Rename folder 3', async () => {
+      await folderPrompts.openFolderDropdownMenu(
+        ExpectedConstants.newFolderWithIndexTitle(3),
+      );
+      await folderPrompts.editFolderNameWithTick(
+        ExpectedConstants.newFolderWithIndexTitle(2),
+      );
+    });
+
+    await dialTest.step(
+      'Drag renamed folder to the level where namesake exists',
+      async () => {
+        await promptBar.dragAndDropEntityToFolder(
+          folderPrompts.getFolderByName(
+            ExpectedConstants.newFolderWithIndexTitle(2),
+            2,
+          ),
+          folderPrompts.getFolderByName(
+            ExpectedConstants.newFolderWithIndexTitle(1),
+          ),
+        );
+
+        await expect
+          .soft(
+            errorToast.getElementLocator(),
+            ExpectedMessages.errorToastIsShown,
+          )
+          .toBeVisible();
+        const errorMessage = await errorToast.getElementContent();
+        expect
+          .soft(errorMessage, ExpectedMessages.notAllowedNameErrorShown)
+          .toBe(
+            ExpectedConstants.duplicatedFolderNameErrorMessage(
+              duplicatedFolderName,
+            ),
+          );
+      },
+    );
+
+    await dialTest.step(
+      'Drag "New folder 2" located in "New folder 1" to the root',
+      async () => {
+        await promptBar.dragFolderToRoot(
+          folderPrompts.getFolderByName(duplicatedFolderName),
+        );
+      },
+    );
+  },
+);
+
+dialTest.only(
+  'Prompt folder: error message appears if to drag prompt folder out to root where the same name exists',
+  async ({
+    dialHomePage,
+    promptBar,
+    folderPrompts,
+    errorToast,
+    setTestIds,
+  }) => {
+    setTestIds('EPMRTC-2972')
+    
+    await dialTest.step('Create 3 folders', async () => {
+      await dialHomePage.openHomePage();
+      await dialHomePage.waitForPageLoaded();
+      for (let i = 1; i <= 2; i++) {
+        await promptBar.createNewFolder();
+        await expect
+          .soft(
+            folderPrompts.getFolderByName(
+              ExpectedConstants.newFolderWithIndexTitle(i),
+            ),
+            ExpectedMessages.folderIsVisible,
+          )
+          .toBeVisible();
+      }
+    });
+
+    await dialTest.step('Create nested structure', async () => {
+        await promptBar.dragAndDropEntityToFolder(
+          folderPrompts.getFolderByName(
+            ExpectedConstants.newFolderWithIndexTitle(2),
+          ),
+          folderPrompts.getFolderByName(
+            ExpectedConstants.newFolderWithIndexTitle(1),
+          ),
+        );
+    });
+
+    await dialTest.step('Create new Folder', async () => {
+        await promptBar.createNewFolder();
+        await expect
+          .soft(
+            folderPrompts.getFolderByName(
+              ExpectedConstants.newFolderWithIndexTitle(2),
+            ),
+            ExpectedMessages.folderIsVisible,
+          )
+          .toBeVisible();
+    });
+
+
+    await dialTest.step(
+      'Drag & drop "New folder 2"  located in "New folder 1" to the root',
+      async () => {
+        await promptBar.dragFolderToRoot(
+          folderPrompts.getFolderByName('New folder 2'),
+        );
+
+        await expect
+          .soft(errorToast.getElementLocator(), ExpectedMessages.errorToastIsShown)
+          .toBeVisible();
+        const errorMessage = await errorToast.getElementContent();
+        expect
+          .soft(errorMessage, ExpectedMessages.notAllowedNameErrorShown)
+          .toBe(
+            ExpectedConstants.duplicatedFolderRootNameErrorMessage(
+              'New folder 2',
             ),
           );
       },
