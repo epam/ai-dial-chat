2e7ba2dab988ba5cd4d60707d0b600f0d3a72e69
Maksim Nartov <nartovm1994@gmail.com>
2024-06-13 20:50:36 +0200

    EPMRTC-2970

--- apps/chat-e2e/src/tests/promptFolderNamesakesTest.test.ts
+++ apps/chat-e2e/src/tests/promptFolderNamesakesTest.test.ts
@@ -6,7 +6,7 @@ import {
 } from '@/src/testData';
 import { expect } from '@playwright/test';
 
-dialTest.only(
+dialTest(
   'Prompt folder: error message appears if to rename prompt folder to already existed name in the root',
   async ({
     dialHomePage,
@@ -58,6 +58,84 @@ dialTest.only(
           .soft(errorMessage, ExpectedMessages.notAllowedNameErrorShown)
           .toBe(
             ExpectedConstants.duplicatedFolderRootNameErrorMessage(
+              duplicatedFolderName,
+            ),
+          );
+      },
+    );
+  },
+);
+
+dialTest.only(
+  'Prompt folder: error message appears if to drag prompt folder IN to another folder where folder with the same name exists',
+  async ({
+    dialHomePage,
+    promptBar,
+    folderPrompts,
+    errorToast,
+    setTestIds,
+  }) => {
+    setTestIds('EPMRTC-2970');
+    const duplicatedFolderName = ExpectedConstants.newFolderWithIndexTitle(2);
+
+    await dialTest.step(
+      'Create 2 folders and move one into another. Create one more',
+      async () => {
+        await dialHomePage.openHomePage();
+        await dialHomePage.waitForPageLoaded();
+        for (let i = 1; i <= 2; i++) {
+          await promptBar.createNewFolder();
+          await expect
+            .soft(
+              folderPrompts.getFolderByName(
+                ExpectedConstants.newFolderWithIndexTitle(i),
+              ),
+              ExpectedMessages.folderIsVisible,
+            )
+            .toBeVisible();
+        }
+        await promptBar.dragAndDropEntityToFolder(
+          folderPrompts.getFolderByName(
+            ExpectedConstants.newFolderWithIndexTitle(2),
+          ),
+          folderPrompts.getFolderByName(
+            ExpectedConstants.newFolderWithIndexTitle(1),
+          ),
+        );
+      },
+    );
+
+    await dialTest.step('Create 1 more folder', async () => {
+      await promptBar.createNewFolder();
+      await expect
+        .soft(
+          folderPrompts.getFolderByName(duplicatedFolderName),
+          ExpectedMessages.folderIsVisible,
+        )
+        .toBeVisible();
+    });
+
+    await dialTest.step(
+      'Drag & drop "New folder 2" to "New folder 1"',
+      async () => {
+        await promptBar.dragAndDropEntityToFolder(
+          folderPrompts.getFolderByName(duplicatedFolderName, 2),
+          folderPrompts.getFolderByName(
+            ExpectedConstants.newFolderWithIndexTitle(1)
+          ),
+        );
+
+        await expect
+          .soft(
+            errorToast.getElementLocator(),
+            ExpectedMessages.errorToastIsShown,
+          )
+          .toBeVisible();
+        const errorMessage = await errorToast.getElementContent();
+        expect
+          .soft(errorMessage, ExpectedMessages.notAllowedNameErrorShown)
+          .toBe(
+            ExpectedConstants.duplicatedFolderNameErrorMessage(
               duplicatedFolderName,
             ),
           );
