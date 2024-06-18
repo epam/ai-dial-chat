c01a4bf8b92db0ee5930ad1f14d961e67f37ccdb
Alexandr Ogarkov <Alexandr.Ogarkov-ext@pernod-ricard.com>
2024-06-18 10:09:50 +0200

    Fix table td bg after upstream update

--- apps/chat/src/components/Markdown/ChatMDComponent.tsx
+++ apps/chat/src/components/Markdown/ChatMDComponent.tsx
@@ -83,7 +83,7 @@ export const getMDComponents = (
     },
     td({ children }) {
       return (
-        <td className="break-words border border-tertiary bg-layer-3 px-3 py-1 text-sm">
+        <td className="break-words border border-tertiary bg-layer-2 px-3 py-1 text-sm">
           {children}
         </td>
       );
