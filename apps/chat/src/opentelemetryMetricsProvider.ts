4634881fab13593c9658f1fb259f4f35d247736d
Denys Kolomiitsev <denys_kolomiitsev@epam.com>
2024-06-19 15:48:20 +0200

    fix: update telemetry tracing and metrics

--- apps/chat/src/opentelemetryMetricsProvider.ts
+++ apps/chat/src/opentelemetryMetricsProvider.ts
@@ -1,4 +1,5 @@
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc';
+import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
 import {
   MeterProvider,
   PeriodicExportingMetricReader,
@@ -7,17 +8,27 @@ import {
 const collectorOptions = {
   // url is optional and can be omitted - default is http://localhost:4317
   // Unix domain sockets are also supported: 'unix:///path/to/socket.sock'
-  // url: process.env.OTEL_EXPORTER_OTLP_METRICS_ENDPOINT,
+  url: process.env.OTEL_EXPORTER_OTLP_METRICS_ENDPOINT,
 };
+const metricExporterPrometheus = new PrometheusExporter({
+  port: 9464,
+  endpoint: '/metrics',
+});
 
-const metricExporter = new OTLPMetricExporter(collectorOptions);
-const meterProvider = new MeterProvider({});
+const metricExporterGRPC = new OTLPMetricExporter(collectorOptions);
+const periodicMetricReaderGRPC = new PeriodicExportingMetricReader({
+  exporter: metricExporterGRPC,
+});
 
-meterProvider.addMetricReader(
-  new PeriodicExportingMetricReader({
-    exporter: metricExporter,
-  }),
-);
+const meterProvider = new MeterProvider({
+  readers: [periodicMetricReaderGRPC, metricExporterPrometheus],
+});
+
+// meterProvider.addMetricReader(
+//   new PeriodicExportingMetricReader({
+//     exporter: metricExporter,
+//   }),
+// );
 
 ['SIGINT', 'SIGTERM'].forEach((signal) => {
   process.on(signal, () => meterProvider.shutdown().catch(console.error));
