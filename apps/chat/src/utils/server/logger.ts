a456831e6948149fd29669fc47aedd584ccd2500
Denys Kolomiitsev <denys_kolomiitsev@epam.com>
2024-06-28 12:55:54 +0200

    fix: fix import for the pino transport

--- apps/chat/src/utils/server/logger.ts
+++ apps/chat/src/utils/server/logger.ts
@@ -1,5 +1,7 @@
 import pino from 'pino';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import transport from 'pino-opentelemetry-transport';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import pretty from 'pino-pretty';
 
 export const logger = pino({
