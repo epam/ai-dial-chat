d2c99185559b504aba5ae73d4b6ea3b6145bbc15
Alexandr Ogarkov <Alexandr.Ogarkov-ext@pernod-ricard.com>
2024-07-09 12:25:46 +0200

    PGPT-233: Fix the color for the button for Tour Guide

--- apps/chat/src/components/TourGuide/components/WelcomeTooltip.tsx
+++ apps/chat/src/components/TourGuide/components/WelcomeTooltip.tsx
@@ -30,7 +30,7 @@ const WelcomeTooltip: FC<TooltipRenderProps> = ({
 
       <div className="my-[30px] flex items-center justify-center">
         <LogoIcon />
-        <div className="ml-[10px] font-weave text-[30px] font-bold text-primary-bg-light">
+        <div className="ml-[10px] font-weave text-3xl font-bold text-primary-bg-light">
           PR GPT
         </div>
       </div>
@@ -46,7 +46,7 @@ const WelcomeTooltip: FC<TooltipRenderProps> = ({
       <button
         id={TooltipId.next}
         {...primaryProps}
-        className="my-[10px] mt-[27px] block rounded-[40px] bg-layer-3 px-[16px] py-[6px] text-primary-bg-dark"
+        className="bg-pr-primary-550 hover:bg-pr-primary-650 text-pr-grey-white my-[10px] mt-[27px] block rounded-[40px] px-[16px] py-[6px]"
       >
         {t('Start a quick tour')}
       </button>
@@ -54,7 +54,7 @@ const WelcomeTooltip: FC<TooltipRenderProps> = ({
       <button
         id={TooltipId.close}
         onClick={closeProps.onClick}
-        className="block text-pr-primary-700"
+        className="text-pr-primary-550 hover:text-pr-primary-650 block"
       >
         {t('Skip the tour')}
       </button>
