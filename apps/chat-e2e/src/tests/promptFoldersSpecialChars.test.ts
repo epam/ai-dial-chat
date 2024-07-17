9237817d17dcfe23c1a5db8d269dca38e6f97b9c
Maksim Nartov <nartovm1994@gmail.com>
2024-07-17 19:05:58 +0200

    first attempt

---
+++ apps/chat-e2e/src/tests/promptFoldersSpecialChars.test.ts
@@ -0,0 +1,60 @@
+import dialTest from '@/src/core/dialFixtures';
+import {
+  ExpectedConstants,
+  ExpectedMessages,
+  MenuOptions,
+} from '@/src/testData';
+import { expect } from '@playwright/test';
+
+dialTest.only(
+  'Prompt folder: Error message appears if there is a dot is at the end of folder name',
+  async ({
+    dialHomePage,
+    promptBar,
+    folderPrompts,
+    folderDropdownMenu,
+    errorToast,
+    setTestIds,
+  }) => {
+    setTestIds('EPMRTC-2975');
+    const folderName = ExpectedConstants.newFolderWithIndexTitle(1);
+    const newNameWithEndDot = `${folderName}.`;
+
+    await dialTest.step('Create prompt folder', async () => {
+      await dialHomePage.openHomePage();
+      await dialHomePage.waitForPageLoaded();
+      
+      await promptBar.createNewFolder();
+      await expect
+        .soft(
+          folderPrompts.getFolderByName(folderName),
+          ExpectedMessages.folderIsVisible,
+        )
+        .toBeVisible();
+    });
+
+    await dialTest.step(
+      'Rename it to have a dot at the end of the name',
+      async () => {
+        await folderPrompts.openFolderDropdownMenu(folderName);
+        await folderDropdownMenu.selectMenuOption(MenuOptions.rename);
+        await folderPrompts.editFolderName(newNameWithEndDot);
+      },
+    );
+
+    await dialTest.step('Click on confirmation button', async () => {
+      await folderPrompts.getEditFolderInputActions().clickTickButton();
+
+      const errorMessage = await errorToast.getElementContent();
+      expect
+        .soft(errorMessage, ExpectedMessages.notAllowedNameErrorShown)
+        .toBe(ExpectedConstants.nameWithDotErrorMessage);
+      await expect
+        .soft(
+          folderPrompts.getEditFolderInput().getElementLocator(),
+          ExpectedMessages.folderEditModeIsActive,
+        )
+        .toBeVisible();
+    });
+  },
+);
