4d1bedeabcc4fdfefbf6b2d5a087711fd584e7ed
Maksim Nartov <nartovm1994@gmail.com>
2024-07-17 21:17:02 +0200

    More

--- apps/chat-e2e/src/tests/promptFoldersSpecialChars.test.ts
+++ apps/chat-e2e/src/tests/promptFoldersSpecialChars.test.ts
@@ -5,11 +5,13 @@ import {
   MenuOptions,
 } from '@/src/testData';
 import { expect } from '@playwright/test';
+import { keys } from '@/src/ui/keyboard'; 
 
 dialTest.only(
   'Prompt folder: Error message appears if there is a dot is at the end of folder name.\n' +
   'Prompt folder: allowed special characters.\n' +
-  'Prompt folder: restricted special characters are not entered',
+  'Prompt folder: restricted special characters are not entered.\n' +
+  'Prompt folder: restricted special characters are removed if to copy-paste',
   async ({
     dialHomePage,
     promptBar,
@@ -17,11 +19,15 @@ dialTest.only(
     folderDropdownMenu,
     errorToast,
     setTestIds,
+    page,
+    sendMessage,
   }) => {
-    setTestIds('EPMRTC-2975', 'EPMRTC-2976', 'EPMRTC-2977');
+    setTestIds('EPMRTC-2975', 'EPMRTC-2976', 'EPMRTC-2977', 'EPMRTC-2978');
     const folderName = ExpectedConstants.newFolderWithIndexTitle(1);
     const newNameWithEndDot = `${folderName}.`;
     const newNameWithSpecialChars = `${folderName} ${ExpectedConstants.allowedSpecialChars}`;
+    const nameWithRestrictedChars = `Folder=,:;{}\\/%&"_name`;
+    const expectedFolderName = 'Folder_name';
 
     await dialTest.step('Create prompt folder', async () => {
       await dialHomePage.openHomePage();
@@ -84,6 +90,38 @@ dialTest.only(
           )
           .toBe('');
       }
+    });
+
+    await dialTest.step('Copy and paste restricted special characters', async () => {
+
+      // await page.evaluate(
+      //   (text) => navigator.clipboard.writeText(text),
+      //   nameWithRestrictedChars,
+      // );
+
+      await page.evaluate(
+        (text) => navigator.clipboard.writeText(text),
+        nameWithRestrictedChars,
+      );
+      await folderPrompts.getEditFolderInput().editInput.click();
+
+      await page.keyboard.press(keys.ctrlPlusV);
+
+      await folderPrompts.getEditFolderInputActions().clickTickButton();
+
+      await expect
+        .soft(
+          folderPrompts.getFolderByName(expectedFolderName),
+          ExpectedMessages.folderIsVisible,
+        )
+        .toBeVisible();
+
+      await expect
+        .soft(
+          errorToast.getElementLocator(),
+          ExpectedMessages.noErrorToastIsShown,
+        )
+        .toBeHidden();
     });
   },
 );
