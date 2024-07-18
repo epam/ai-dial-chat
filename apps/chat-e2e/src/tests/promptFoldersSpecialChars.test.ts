ffdadaf352d433e62a5e21059265ed3cc21c0bac
Maksim Nartov <nartovm1994@gmail.com>
2024-07-18 12:50:08 +0200

    waiting for the toast to disappear + format

--- apps/chat-e2e/src/tests/promptFoldersSpecialChars.test.ts
+++ apps/chat-e2e/src/tests/promptFoldersSpecialChars.test.ts
@@ -4,18 +4,18 @@ import {
   ExpectedMessages,
   MenuOptions,
 } from '@/src/testData';
-import { expect } from '@playwright/test';
 import { keys } from '@/src/ui/keyboard';
+import { expect } from '@playwright/test';
 
 dialTest.only(
   'Prompt folder: Error message appears if there is a dot is at the end of folder name.\n' +
-  'Prompt folder: allowed special characters.\n' +
-  'Prompt folder: restricted special characters are not entered.\n' +
-  'Prompt folder: restricted special characters are removed if to copy-paste.\n' +
-  'Prompt folder: spaces in the middle of folder name stay.\n' +
-  'Prompt folder: name can not be blank or with spaces only.\n' +
-  'Prompt folder: spaces at the beginning or end of folder name are removed.\n' +
-  'Prompt folder: smiles, hieroglyph, specific letters in name',
+    'Prompt folder: allowed special characters.\n' +
+    'Prompt folder: restricted special characters are not entered.\n' +
+    'Prompt folder: restricted special characters are removed if to copy-paste.\n' +
+    'Prompt folder: spaces in the middle of folder name stay.\n' +
+    'Prompt folder: name can not be blank or with spaces only.\n' +
+    'Prompt folder: spaces at the beginning or end of folder name are removed.\n' +
+    'Prompt folder: smiles, hieroglyph, specific letters in name',
   async ({
     dialHomePage,
     promptBar,
@@ -29,7 +29,14 @@ dialTest.only(
     prompts,
   }) => {
     setTestIds(
-      'EPMRTC-2975', 'EPMRTC-2976', 'EPMRTC-2977', 'EPMRTC-2978', 'EPMRTC-2979', 'EPMRTC-2980', 'EPMRTC-2981', 'EPMRTC-2982'
+      'EPMRTC-2975',
+      'EPMRTC-2976',
+      'EPMRTC-2977',
+      'EPMRTC-2978',
+      'EPMRTC-2979',
+      'EPMRTC-2980',
+      'EPMRTC-2981',
+      'EPMRTC-2982',
     );
     const folderName = ExpectedConstants.newFolderWithIndexTitle(1);
     const newNameWithEndDot = `${folderName}.`;
@@ -81,6 +88,9 @@ dialTest.only(
         .toBeVisible();
     });
 
+    // Wait for the toast message to disappear
+    await errorToast.getElementLocator().waitFor({ state: 'hidden' });
+
     await dialTest.step('Rename it to contain special characters', async () => {
       await folderPrompts.editFolderNameWithTick(newNameWithSpecialChars);
       await expect
@@ -91,112 +101,129 @@ dialTest.only(
         .toBeVisible();
     });
 
-    await dialTest.step('Try to type restricted special characters', async () => {
-      await folderPrompts.openFolderDropdownMenu(newNameWithSpecialChars);
-      await folderDropdownMenu.selectMenuOption(MenuOptions.rename);
-      for (const char of ExpectedConstants.restrictedNameChars.split('')) {
-        await folderPrompts.editFolderName(char);
+    await dialTest.step(
+      'Try to type restricted special characters',
+      async () => {
+        await folderPrompts.openFolderDropdownMenu(newNameWithSpecialChars);
+        await folderDropdownMenu.selectMenuOption(MenuOptions.rename);
+        for (const char of ExpectedConstants.restrictedNameChars.split('')) {
+          await folderPrompts.editFolderName(char);
+          expect
+            .soft(
+              await folderPrompts.getEditFolderInput().getEditInputValue(),
+              ExpectedMessages.charactersAreNotDisplayed,
+            )
+            .toBe('');
+        }
+      },
+    );
+
+    await dialTest.step(
+      'Copy and paste restricted special characters',
+      async () => {
+        await page.evaluate(
+          (text) => navigator.clipboard.writeText(text),
+          nameWithRestrictedChars,
+        );
+        await folderPrompts.getEditFolderInput().editInput.click();
+        await page.keyboard.press(keys.ctrlPlusV);
+        await folderPrompts.getEditFolderInputActions().clickTickButton();
+        await expect
+          .soft(
+            folderPrompts.getFolderByName(expectedFolderName),
+            ExpectedMessages.folderIsVisible,
+          )
+          .toBeVisible();
+        await expect
+          .soft(
+            errorToast.getElementLocator(),
+            ExpectedMessages.noErrorToastIsShown,
+          )
+          .toBeHidden();
+      },
+    );
+
+    await dialTest.step(
+      'Rename folder to have spaces in the middle',
+      async () => {
+        await folderPrompts.openFolderDropdownMenu(expectedFolderName);
+        await folderDropdownMenu.selectMenuOption(MenuOptions.rename);
+        await folderPrompts.editFolderName(newNameWithSpaces);
+        // Assert that the input field displays the name with spaces
         expect
           .soft(
             await folderPrompts.getEditFolderInput().getEditInputValue(),
-            ExpectedMessages.charactersAreNotDisplayed,
+            ExpectedMessages.folderNameUpdated,
           )
-          .toBe('');
-      }
-    });
+          .toBe(newNameWithSpaces);
+        await folderPrompts.getEditFolderInputActions().clickTickButton();
+        // Assert that the folder is renamed with spaces on the panel
+        await expect
+          .soft(
+            folderPrompts.getFolderByName(newNameWithSpaces),
+            ExpectedMessages.folderNameUpdated,
+          )
+          .toBeVisible();
+      },
+    );
 
-    await dialTest.step('Copy and paste restricted special characters', async () => {
-      await page.evaluate(
-        (text) => navigator.clipboard.writeText(text),
-        nameWithRestrictedChars,
-      );
-      await folderPrompts.getEditFolderInput().editInput.click();
-      await page.keyboard.press(keys.ctrlPlusV);
-      await folderPrompts.getEditFolderInputActions().clickTickButton();
-      await expect
-        .soft(
-          folderPrompts.getFolderByName(expectedFolderName),
-          ExpectedMessages.folderIsVisible,
-        )
-        .toBeVisible();
-      await expect
-        .soft(
-          errorToast.getElementLocator(),
-          ExpectedMessages.noErrorToastIsShown,
-        )
-        .toBeHidden();
-    });
+    await dialTest.step(
+      'Prompt folder: name can not be blank or with spaces only',
+      async () => {
+        for (const name of ['', '   ']) {
+          await dialTest.step(`Try to rename folder to "${name}"`, async () => {
+            await folderPrompts.openFolderDropdownMenu(newNameWithSpaces);
+            await folderDropdownMenu.selectMenuOption(MenuOptions.rename);
+            await folderPrompts.editFolderName(name);
+            await folderPrompts.getEditFolderInputActions().clickTickButton();
+            await expect
+              .soft(
+                folderPrompts.getFolderByName(newNameWithSpaces),
+                ExpectedMessages.folderNameNotUpdated,
+              )
+              .toBeVisible();
+          });
+        }
+      },
+    );
 
-    await dialTest.step('Rename folder to have spaces in the middle', async () => {
-      await folderPrompts.openFolderDropdownMenu(expectedFolderName);
-      await folderDropdownMenu.selectMenuOption(MenuOptions.rename);
-      await folderPrompts.editFolderName(newNameWithSpaces);
-      // Assert that the input field displays the name with spaces
-      expect
-        .soft(
-          await folderPrompts.getEditFolderInput().getEditInputValue(),
-          ExpectedMessages.folderNameUpdated,
-        )
-        .toBe(newNameWithSpaces);
-      await folderPrompts.getEditFolderInputActions().clickTickButton();
-      // Assert that the folder is renamed with spaces on the panel
-      await expect
-        .soft(
-          folderPrompts.getFolderByName(newNameWithSpaces),
-          ExpectedMessages.folderNameUpdated,
-        )
-        .toBeVisible();
-    });
+    await dialTest.step(
+      'Rename folder to have spaces at the beginning and at the end',
+      async () => {
+        await folderPrompts.openFolderDropdownMenu(newNameWithSpaces);
+        await folderDropdownMenu.selectMenuOption(MenuOptions.rename);
+        await folderPrompts.editFolderName(nameWithSpacesBeforeAndAfter);
+        // Assert that the input field displays the name with spaces
+        expect
+          .soft(
+            await folderPrompts.getEditFolderInput().getEditInputValue(),
+            ExpectedMessages.folderNameUpdated,
+          )
+          .toBe(nameWithSpacesBeforeAndAfter);
+        await folderPrompts.getEditFolderInputActions().clickTickButton();
+        // Assert that the folder is renamed with spaces on the panel
+        await expect
+          .soft(
+            folderPrompts.getFolderByName(expectedName),
+            ExpectedMessages.folderNameUpdated,
+          )
+          .toBeVisible();
+      },
+    );
 
-    await dialTest.step('Prompt folder: name can not be blank or with spaces only', async () => {
-      for (const name of ['', '   ']) {
-        await dialTest.step(`Try to rename folder to "${name}"`, async () => {
-          await folderPrompts.openFolderDropdownMenu(newNameWithSpaces);
-          await folderDropdownMenu.selectMenuOption(MenuOptions.rename);
-          await folderPrompts.editFolderName(name);
-          await folderPrompts.getEditFolderInputActions().clickTickButton();
-          await expect
-            .soft(
-              folderPrompts.getFolderByName(newNameWithSpaces),
-              ExpectedMessages.folderNameNotUpdated,
-            )
-            .toBeVisible();
-        });
-      }
-    });
-
-    await dialTest.step('Rename folder to have spaces at the beginning and at the end', async () => {
-      await folderPrompts.openFolderDropdownMenu(newNameWithSpaces);
-      await folderDropdownMenu.selectMenuOption(MenuOptions.rename);
-      await folderPrompts.editFolderName(nameWithSpacesBeforeAndAfter);
-      // Assert that the input field displays the name with spaces
-      expect
-        .soft(
-          await folderPrompts.getEditFolderInput().getEditInputValue(),
-          ExpectedMessages.folderNameUpdated,
-        )
-        .toBe(nameWithSpacesBeforeAndAfter);
-      await folderPrompts.getEditFolderInputActions().clickTickButton();
-      // Assert that the folder is renamed with spaces on the panel
-      await expect
-        .soft(
-          folderPrompts.getFolderByName(expectedName),
-          ExpectedMessages.folderNameUpdated,
-        )
-        .toBeVisible();
-    });
-
-    await dialTest.step('Rename folder to contain emojis and hieroglyphs', async () => {
-      await folderPrompts.openFolderDropdownMenu(expectedName);
-      await folderDropdownMenu.selectMenuOption(MenuOptions.rename);
-      await folderPrompts.editFolderNameWithTick(newNameWithEmojis);
-      await expect
-        .soft(
-          folderPrompts.getFolderByName(newNameWithEmojis),
-          ExpectedMessages.folderNameUpdated,
-        )
-        .toBeVisible();
-    });
-
+    await dialTest.step(
+      'Rename folder to contain emojis and hieroglyphs',
+      async () => {
+        await folderPrompts.openFolderDropdownMenu(expectedName);
+        await folderDropdownMenu.selectMenuOption(MenuOptions.rename);
+        await folderPrompts.editFolderNameWithTick(newNameWithEmojis);
+        await expect
+          .soft(
+            folderPrompts.getFolderByName(newNameWithEmojis),
+            ExpectedMessages.folderNameUpdated,
+          )
+          .toBeVisible();
+      },
+    );
   },
 );
