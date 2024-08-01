1f527bf4062dc74054dbc3a167d330d5f3a2bbd4
Maksim Nartov <nartovm1994@gmail.com>
2024-08-01 13:25:03 +0200

    EPMRTC-601

--- apps/chat-e2e/src/tests/ChatSwitch.test.ts
+++ apps/chat-e2e/src/tests/ChatSwitch.test.ts
@@ -1,14 +1,15 @@
 import dialTest from '@/src/core/dialFixtures';
-import { API, ExpectedConstants, ExpectedMessages, MenuOptions } from '@/src/testData';
-import { expect } from '@playwright/test';
-import { Conversation } from "@/chat/types/chat";
-import { responseThrottlingTimeout } from "@/src/ui/pages";
-import { Styles } from "@/src/ui/domData";
-import { Cursors } from "@/src/ui/domData";
+import {API, ExpectedConstants, ExpectedMessages, MenuOptions} from '@/src/testData';
+import {expect} from '@playwright/test';
+import {Conversation} from "@/chat/types/chat";
+import {responseThrottlingTimeout} from "@/src/ui/pages";
+import {Styles} from "@/src/ui/domData";
+import {Cursors} from "@/src/ui/domData";
 
 dialTest.only(
   'Another chat is not available while AI is generating a response.\n' +
-  'Chat menu is not available while AI is generating a response',
+  'Chat menu is not available while AI is generating a response.\n' +
+  'Switching to another chat is not available while AI is replaying a chat',
   async ({
            dialHomePage,
            conversations,
@@ -17,10 +18,11 @@ dialTest.only(
            setTestIds,
            localStorageManager,
            chat,
+           chatMessages,
            chatBar,
            sendMessage,
          }) => {
-    setTestIds('EPMRTC-598', 'EPMRTC-599', 'EPMRTC-600');
+    setTestIds('EPMRTC-598', 'EPMRTC-599', 'EPMRTC-600', 'EPMRTC-601');
     const request =
       'give me a sci-fi story with a main topic of your choice. 200 tokens minimum';
     let firstConversation: Conversation;
@@ -90,6 +92,34 @@ dialTest.only(
             ExpectedMessages.conversationIsSelected,
           )
           .toBeVisible();
+      },
+    );
+
+    await dialTest.step(
+      'Select "Replay" option from the first conversation menu',
+      async () => {
+        await chatMessages.waitForResponseReceived();
+
+        await conversations.openEntityDropdownMenu(firstConversation.name);
+        await conversations.selectEntityMenuOption(MenuOptions.replay, {
+          triggeredHttpMethod: 'POST',
+        });
+        await chat.startReplay();
+        // await chat.replay.waitForState();
+      },
+    );
+
+    await dialTest.step(
+      'Verify conversation name cursor is "not-allowed" during the chat replay',
+      async () => {
+        await conversations.getEntityByName(secondConversation.name).hover();
+        let style = await conversations.getConversationName(secondConversation.name).getComputedStyleProperty(Styles.cursor);
+        expect
+          .soft(
+            style[0],
+            ExpectedMessages.sendButtonCursorIsNotAllowed,
+          )
+          .toBe(Cursors.notAllowed);
       },
     );
   },
