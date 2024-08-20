610dfeaf7e6e62701d34e7bb874c48f6daf48987
Sergey Shakhmatov <serge.shakhmatov@gmail.com>
2024-08-20 13:51:13 +0200

    refactor: update useMergedMessages

--- apps/chat/src/components/Chat/ChatView/hooks/useMergedMessages.ts
+++ apps/chat/src/components/Chat/ChatView/hooks/useMergedMessages.ts
@@ -28,41 +28,51 @@ export function useMergedMessages({
   >({});
 
   useEffect(() => {
-    if (selectedConversations.length > 0) {
-      const newMergedMessages: MergedMessages[] = [];
-      const firstConversationMessages =
-        selectedConversations[0].messages.filter((m) => m.role !== Role.System);
-
-      for (let i = 0; i < firstConversationMessages.length; i++) {
-        newMergedMessages.push(
-          selectedConversations.map((conv) => [
-            conv,
-            conv.messages?.filter((m) => m.role !== Role.System)[i] || {
-              role: Role.Assistant,
-              content: '',
-            },
-            i,
-          ]),
-        );
-      }
-      setMergedMessages(newMergedMessages);
-
+    if (selectedConversations.length === 0) {
       onMessagesChange({
-        isNew: true,
-        areConversationsEmpty: selectedConversations.every(
-          (conv) => !conv.messages.find((m) => m.role !== Role.Assistant),
-        ),
+        isNew: false,
+        areConversationsEmpty: true,
+        hasNewSelection: prevSelectedIds.length > 0,
       });
-    }
-  }, [selectedConversations, onMessagesChange]);
+      setPrevSelectedIds([]);
 
-  useEffect(() => {
-    if (
-      !selectedConversations.some((conv) => prevSelectedIds.includes(conv.id))
-    ) {
-      onMessagesChange({ hasNewSelection: true });
+      return;
+    }
+
+    const firstConversationMessages = selectedConversations[0].messages.filter(
+      (m) => m.role !== Role.System,
+    );
+
+    const newMergedMessages: MergedMessages[] = firstConversationMessages.map(
+      (_, i) =>
+        selectedConversations.map((conv) => [
+          conv,
+          conv.messages?.filter((m) => m.role !== Role.System)[i] || {
+            role: Role.Assistant,
+            content: '',
+          },
+          i,
+        ]),
+    );
+    setMergedMessages(newMergedMessages);
+
+    const areConversationsEmpty = selectedConversations.every(
+      (conv) => !conv.messages.some((m) => m.role !== Role.Assistant),
+    );
+
+    const hasNewSelection = !selectedConversations.some((conv) =>
+      prevSelectedIds.includes(conv.id),
+    );
+
+    if (hasNewSelection) {
       setPrevSelectedIds(selectedConversations.map((conv) => conv.id));
     }
+
+    onMessagesChange({
+      isNew: true,
+      areConversationsEmpty,
+      hasNewSelection,
+    });
   }, [selectedConversations, onMessagesChange, prevSelectedIds]);
 
   useEffect(() => {
