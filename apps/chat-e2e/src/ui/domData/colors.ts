13a144f170fcf617b21dcce58a9594a2d32d85ae
Maksim Nartov <nartovm1994@gmail.com>
2024-08-27 08:57:15 +0200

    ignores

--- apps/chat-e2e/src/ui/domData/colors.ts
+++ apps/chat-e2e/src/ui/domData/colors.ts
@@ -7,6 +7,7 @@ export enum Colors {
   controlsBackgroundAccent = 'rgb(92, 141, 234)',
   controlsBackgroundAccentPrimary = 'rgb(39, 100, 217)',
   controlsBackgroundDisable = 'rgb(127, 135, 146)',
+  // eslint-disable-next-line @typescript-eslint/no-duplicate-enum-values
   textSecondary = 'rgb(169, 114, 255)',
   defaultBackground = 'rgb(0, 0, 0, 0)',
   textPermanent = 'rgb(252, 252, 252)',
@@ -14,13 +15,16 @@ export enum Colors {
   backgroundLayer4Dark = 'rgb(51, 57, 66)',
   backgroundLayer4Light = 'rgb(221, 225, 230)',
   backgroundLayer3Dark = 'rgb(34, 41, 50)',
+  // eslint-disable-next-line @typescript-eslint/no-duplicate-enum-values
   backgroundLayer3Light = 'rgb(252, 252, 252)',
+  // eslint-disable-next-line @typescript-eslint/no-duplicate-enum-values
   backgroundAccentSecondaryAlphaDark = 'rgba(55, 186, 188, 0.15)',
   backgroundAccentSecondaryLight = 'rgb(0, 157, 159)',
   backgroundAccentSecondaryAlphaLight = 'rgba(0, 157, 159, 0.1)',
   backgroundAccentTertiaryAlphaDark = 'rgba(169, 114, 255, 0.17)',
   backgroundAccentTertiaryAlphaLight = 'rgba(132, 62, 243, 0.1)',
   textAccentTertiaryLight = 'rgb(132, 62, 243)',
+  // eslint-disable-next-line @typescript-eslint/no-duplicate-enum-values
   controlsTextDisable = 'rgb(51, 57, 66)',
 }
 
