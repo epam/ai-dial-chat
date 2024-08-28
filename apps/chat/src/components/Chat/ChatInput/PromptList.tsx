15c502e2602b31d74b9fe71939c7546213405c31
Aliaksandr Kezik <a.kezzk@gmail.com>
2024-08-28 14:10:05 +0200

    fix tests

--- apps/chat/src/components/Chat/ChatInput/PromptList.tsx
+++ apps/chat/src/components/Chat/ChatInput/PromptList.tsx
@@ -26,7 +26,7 @@ const PromptListItem: FC<ListItemProps> = ({
   return (
     <li
       className={classNames(
-        'flex cursor-pointer justify-between px-3 py-2',
+        'flex cursor-pointer justify-between truncate px-3 py-2',
         index === activePromptIndex && 'bg-accent-primary-alpha',
       )}
       onClick={(e) => {
