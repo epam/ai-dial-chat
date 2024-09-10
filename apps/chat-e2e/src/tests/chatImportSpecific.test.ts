ab50eb2b17aa21d8f73265a9bb530d6711a5906f
Maksim Nartov <nartovm1994@gmail.com>
2024-09-10 20:19:24 +0200

    EPMRTC-3082, EPMRTC-3083

--- apps/chat-e2e/src/tests/chatImportSpecific.test.ts
+++ apps/chat-e2e/src/tests/chatImportSpecific.test.ts
@@ -10,8 +10,9 @@ import { FileUtil } from '@/src/utils';
 import { expect } from '@playwright/test';
 import {FolderInterface} from "@/chat/types/folder";
 import {Conversation} from "@/chat/types/chat";
+import {Prompt} from "@/chat/types/prompt";
 
-dialTest.only(
+dialTest(
   'Import: dots at the end of Chat Folder and Chat name are removed while import',
   async ({
            dialHomePage,
@@ -114,6 +115,110 @@ dialTest.only(
           .soft(
             folderConversations.getFolderEntity(folderToImport, conversationToImportWithDots),
             ExpectedMessages.conversationIsNotVisible,
+          )
+          .toBeHidden();
+      },
+    );
+  },
+);
+
+dialTest.only(
+  'Import: dots at the end of Prompt Folder and Prompt name are removed while import',
+  async ({
+           dialHomePage,
+           promptData,
+           dataInjector,
+           localStorageManager,
+           confirmationDialog,
+           folderPrompts,
+           promptBar,
+           setTestIds,
+         }) => {
+    setTestIds('EPMRTC-3082', 'EPMRTC-3083');
+    const folderToExport = 'folderToExport';
+    const folderToImport = 'folderToImport';
+    const promptToExport = 'promptToExport';
+    const promptToImport = 'promptToImport';
+    const folderToImportWithDots = 'folderToImport...';
+    const promptToImportWithDots = 'promptToImport...';
+    let downloadedDataPath: string;
+
+    await dialTest.step('Prepare prompt inside folder', async () => {
+      const promptInFolder =
+        promptData.prepareDefaultPromptInFolder(promptToExport, folderToExport);
+      await dataInjector.createPrompts(
+        promptInFolder.prompts,
+        promptInFolder.folders,
+      );
+      await localStorageManager.setPromptCollapsedSection(
+        CollapsedSections.Organization,
+      );
+    });
+
+    await dialTest.step('Export prompt', async () => {
+      await dialHomePage.openHomePage();
+      await dialHomePage.waitForPageLoaded();
+      await folderPrompts.expandFolder(folderToExport);
+      await folderPrompts.openFolderEntityDropdownMenu(
+        folderToExport,
+        promptToExport,
+      );
+      const downloadedData = await dialHomePage.downloadData(
+        () =>
+          folderPrompts
+            .getDropdownMenu()
+            .selectMenuOption(MenuOptions.export),
+        `test.json`,
+      );
+      downloadedDataPath = downloadedData.path;
+    });
+
+    await dialTest.step('Update exported json', async () => {
+      const exportedData = FileUtil.readFileData(downloadedDataPath);
+      exportedData.folders.map((f: FolderInterface) => {
+        f.id = f.id.replace(folderToExport, folderToImportWithDots);
+        f.name = f.name.replace(folderToExport, folderToImportWithDots);
+      });
+      exportedData.prompts.map((p: Prompt) => {
+        p.id = p.id.replace(folderToExport, folderToImportWithDots);
+        p.name = p.name.replace(promptToExport, promptToImportWithDots);
+        p.folderId = p.folderId.replace(folderToExport, folderToImportWithDots);
+      });
+      downloadedDataPath = FileUtil.writeDataToFile(exportedData);
+    });
+
+    await dialTest.step(
+      'Import json and check prompt folder and prompt names',
+      async () => {
+        await promptBar.deleteAllEntities();
+        await confirmationDialog.confirm({ triggeredHttpMethod: 'DELETE' });
+        await dialHomePage.importFile(
+          { path: downloadedDataPath },
+          () => promptBar.importButton.click(),
+        );
+        // await folderPrompts.expandFolder(folderToImport);
+        await expect
+          .soft(
+            folderPrompts.getFolderByName(folderToImport),
+            ExpectedMessages.folderIsVisible,
+          )
+          .toBeVisible();
+        await expect
+          .soft(
+            folderPrompts.getFolderByName(folderToImportWithDots),
+            ExpectedMessages.folderIsNotVisible,
+          )
+          .toBeHidden();
+        await expect
+          .soft(
+            folderPrompts.getFolderEntity(folderToImport, promptToImport),
+            ExpectedMessages.promptIsVisible,
+          )
+          .toBeVisible();
+        await expect
+          .soft(
+            folderPrompts.getFolderEntity(folderToImport, promptToImportWithDots),
+            ExpectedMessages.promptIsNotVisible,
           )
           .toBeHidden();
       },
