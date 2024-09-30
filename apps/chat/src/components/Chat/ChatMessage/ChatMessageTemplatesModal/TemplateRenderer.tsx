ede5d57f832fef8c30b77b5482e271309878b401
Ilya Bondar <ilya_bondar@epam.com>
2024-09-30 18:27:29 +0200

    fix key-error

--- apps/chat/src/components/Chat/ChatMessage/ChatMessageTemplatesModal/TemplateRenderer.tsx
+++ apps/chat/src/components/Chat/ChatMessage/ChatMessageTemplatesModal/TemplateRenderer.tsx
@@ -1,4 +1,5 @@
 import { PROMPT_VARIABLE_REGEX } from '@/src/constants/folders';
+import { Fragment } from 'react';
 
 interface Props {
   template?: string;
@@ -13,11 +14,11 @@ export const TemplateRenderer = ({ template }: Props) => {
   let index = 0;
   while ((match = PROMPT_VARIABLE_REGEX.exec(template)) !== null) {
     if (match.index > index) {
-      resultNodes.push(template.slice(index, match.index));
+      resultNodes.push(<Fragment key={index}>{template.slice(index, match.index)}</Fragment>);
     }
-    resultNodes.push(<span className="text-accent-tertiary">{match[0]}</span>);
+    resultNodes.push(<span key={match.index} className="text-accent-tertiary">{match[0]}</span>);
     index = match.index + match[0].length;
   }
-  resultNodes.push(template.slice(index));
+  resultNodes.push(<Fragment key={index}>{template.slice(index)}</Fragment>);
   return <>{resultNodes}</>;
 };
