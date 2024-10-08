f5aa8c6cae9cd19acaf68f822d78926b73885148
Aliaksandr Kezik <a.kezzk@gmail.com>
2024-10-08 15:20:50 +0200

    update dep array

--- apps/chat/src/components/Chat/ModelList.tsx
+++ apps/chat/src/components/Chat/ModelList.tsx
@@ -401,7 +401,13 @@ export const ModelList = ({
             entity.id === selectedModelId,
         ),
     ).slice(0, displayCountLimit ?? Number.MAX_SAFE_INTEGER);
-  }, [allEntities, displayCountLimit, entities, installedModelIds]);
+  }, [
+    allEntities,
+    displayCountLimit,
+    entities,
+    installedModelIds,
+    selectedModelId,
+  ]);
 
   return (
     <div className="flex flex-col gap-3 text-xs" data-qa="talk-to-group">
