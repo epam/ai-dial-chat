9c71898e3c19e51d5baa1b6ca7ac12116bb98114
Ilya Bondar <ilya_bondar@epam.com>
2024-10-22 18:26:30 +0200

    fix(chat): remove alt-text fro icons (Issue #2272)

--- apps/chat/src/components/Chatbar/ModelIcon.tsx
+++ apps/chat/src/components/Chatbar/ModelIcon.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+
 /* eslint-disable @next/next/no-img-element */
 import { memo, useCallback, useRef } from 'react';
 
@@ -36,7 +38,6 @@ const ModelIconTemplate = memo(
       entity?.type === EntityType.Addon
         ? getThemeIconUrl('default-addon')
         : getThemeIconUrl('default-model');
-    const description = entity ? getOpenAIEntityFullName(entity) : entityId;
 
     const getIconUrl = (entity: DialAIEntity | undefined) => {
       if (!entity?.iconUrl) return fallbackUrl;
@@ -72,7 +73,6 @@ const ModelIconTemplate = memo(
           width={size}
           height={size}
           onError={handleError}
-          alt={description}
           ref={ref}
         />
       </span>
