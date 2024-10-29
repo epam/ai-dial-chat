dff05e4ce4b7f93b872b959b664c18a1f6541523
Ilya Bondar <ilya_bondar@epam.com>
2024-10-29 18:22:50 +0100

    fix slash in the end

--- apps/chat/src/utils/app/application.ts
+++ apps/chat/src/utils/app/application.ts
@@ -63,15 +63,11 @@ export const convertApplicationToApi = (
   };
 
   if (applicationData.function) {
-    const sourceFolderWithSlash =
-      ApiUtils.encodeApiUrl(applicationData.function.sourceFolder) +
-      (applicationData.function.sourceFolder.endsWith('/') ? '' : '/');
-
     return {
       ...commonData,
       function: {
         runtime: 'python3.11',
-        source_folder: sourceFolderWithSlash,
+        source_folder: `${ApiUtils.encodeApiUrl(applicationData.function.sourceFolder)}/`,
         mapping: applicationData.function.mapping,
         ...(applicationData.function.env && {
           env: applicationData.function.env,
