2c750c1b16c1b86c8f314e04a3824a57f88cf4f5
Aliaksandr Kezik <a.kezzk@gmail.com>
2024-10-31 12:19:47 +0100

    refactor + add folders

--- apps/chat/src/components/Common/ApplicationWizard/CodeAppView/SourceFilesEditor.tsx
+++ apps/chat/src/components/Common/ApplicationWizard/CodeAppView/SourceFilesEditor.tsx
@@ -7,11 +7,13 @@ import { useTranslation } from 'next-i18next';
 
 import classNames from 'classnames';
 
+import { getChildAndCurrentFoldersIdsById } from '@/src/utils/app/folders';
 import {
   getFileRootId,
   getIdWithoutRootPathSegments,
 } from '@/src/utils/app/id';
 
+import { FeatureType } from '@/src/types/common';
 import { DialFile } from '@/src/types/files';
 import { Translation } from '@/src/types/translation';
 
@@ -20,12 +22,225 @@ import { useAppDispatch, useAppSelector } from '@/src/store/hooks';
 import { UISelectors } from '@/src/store/ui/ui.reducers';
 
 import { SelectFolderModal } from '@/src/components/Files/SelectFolderModal';
+import Folder from '@/src/components/Folder/Folder';
 
 import { FieldErrorMessage } from '../../Forms/FieldErrorMessage';
 import Loader from '../../Loader';
 import { FilesRow } from '../../ReplaceConfirmationModal/Components';
 import Tooltip from '../../Tooltip';
 import { FormData } from '../form';
+
+import { UploadStatus } from '@epam/ai-dial-shared';
+
+interface CodeEditorFile {
+  file: DialFile;
+  isHighlighted: boolean;
+  level?: number;
+  onSelectFile: (file: DialFile) => void;
+}
+
+const CodeEditorFile = ({
+  file,
+  onSelectFile,
+  isHighlighted,
+  level = 0,
+}: CodeEditorFile) => {
+  return (
+    <button type="button" onClick={() => onSelectFile(file)} className="w-full">
+      <FilesRow
+        item={file}
+        level={level}
+        featureContainerClassNames="!w-full"
+        itemComponentClassNames={classNames(
+          '!h-[30px] rounded border-l-2 pr-3',
+          isHighlighted
+            ? 'border-accent-primary bg-accent-primary-alpha'
+            : 'border-transparent',
+        )}
+      />
+    </button>
+  );
+};
+
+interface CodeEditorProps {
+  sourcesFolderId: string | undefined;
+  setValue: UseFormSetValue<FormData>;
+}
+
+const CodeEditor = ({ sourcesFolderId, setValue }: CodeEditorProps) => {
+  const dispatch = useAppDispatch();
+
+  const theme = useAppSelector(UISelectors.selectThemeState);
+  const uploadedContent = useAppSelector(FilesSelectors.selectFileContent);
+  const loadingFolderIds = useAppSelector(
+    FilesSelectors.selectLoadingFolderIds,
+  );
+  const isUploadingContent = useAppSelector(
+    FilesSelectors.selectIsFileContentLoading,
+  );
+  const files = useAppSelector(FilesSelectors.selectFiles);
+  const folders = useAppSelector(FilesSelectors.selectFolders);
+
+  const [fileContent, setFileContent] = useState<string>();
+  const [openedFoldersIds, setOpenedFoldersIds] = useState<string[]>([]);
+  const [selectedFile, setSelectedFile] = useState<DialFile>();
+
+  const { rootFiles, rootFolders } = useMemo(() => {
+    if (sourcesFolderId) {
+      return {
+        rootFiles: files.filter((file) => file.folderId === sourcesFolderId),
+        rootFolders: folders.filter(
+          (folder) => folder.folderId === sourcesFolderId,
+        ),
+      };
+    }
+
+    return {
+      rootFiles: [],
+      rootFolders: [],
+    };
+  }, [files, folders, sourcesFolderId]);
+  const rootFileNames = useMemo(
+    () => rootFiles.map((f) => f.name),
+    [rootFiles],
+  );
+
+  useEffect(() => {
+    if (rootFiles.length && !selectedFile) {
+      const appFile = rootFiles.find((file) => file.name === 'app.py');
+      if (appFile) {
+        setSelectedFile(appFile);
+      } else {
+        setSelectedFile(rootFiles[0]);
+      }
+    }
+  }, [rootFiles, selectedFile]);
+
+  useEffect(() => {
+    if (selectedFile) {
+      dispatch(FilesActions.getFileTextContent({ id: selectedFile.id }));
+    }
+  }, [dispatch, selectedFile]);
+
+  useEffect(() => {
+    if (uploadedContent) {
+      setFileContent(uploadedContent);
+    }
+  }, [uploadedContent]);
+
+  useEffect(() => {
+    if (sourcesFolderId) {
+      setValue('sourceFiles', rootFileNames, { shouldValidate: true });
+    }
+  }, [rootFileNames, setValue, sourcesFolderId]);
+
+  if (!sourcesFolderId) {
+    return null;
+  }
+
+  return (
+    <div className="grid w-full max-w-full grid-cols-[minmax(0,1fr)_2fr] gap-1">
+      <div className="flex flex-col gap-0.5 rounded border border-tertiary bg-layer-3 p-3">
+        {rootFolders.map((folder) => {
+          return (
+            <Folder
+              key={folder.id}
+              searchTerm={''}
+              currentFolder={folder}
+              allFolders={folders}
+              isInitialRenameEnabled
+              loadingFolderIds={loadingFolderIds}
+              openedFoldersIds={openedFoldersIds}
+              allItems={files}
+              itemComponent={(props) => (
+                <CodeEditorFile
+                  level={props.level}
+                  file={props.item as DialFile}
+                  onSelectFile={setSelectedFile}
+                  isHighlighted={selectedFile?.id === props.item.id}
+                />
+              )}
+              onClickFolder={(folderId) => {
+                if (openedFoldersIds.includes(folderId)) {
+                  const childFoldersIds = getChildAndCurrentFoldersIdsById(
+                    folderId,
+                    folders,
+                  );
+                  setOpenedFoldersIds(
+                    openedFoldersIds.filter(
+                      (id) => !childFoldersIds.includes(id),
+                    ),
+                  );
+                } else {
+                  setOpenedFoldersIds(openedFoldersIds.concat(folderId));
+                  const folder = folders.find((f) => f.id === folderId);
+                  if (folder?.status !== UploadStatus.LOADED) {
+                    dispatch(
+                      FilesActions.getFilesWithFolders({ id: folderId }),
+                    );
+                  }
+                }
+              }}
+              withBorderHighlight={false}
+              featureType={FeatureType.File}
+            />
+          );
+        })}
+        {rootFiles.map((file) => (
+          <CodeEditorFile
+            key={file.id}
+            file={file}
+            onSelectFile={setSelectedFile}
+            isHighlighted={selectedFile?.id === file.id}
+          />
+        ))}
+      </div>
+      <div className="h-[400px] w-full rounded border border-tertiary bg-layer-3 p-3">
+        {isUploadingContent ? (
+          <Loader />
+        ) : (
+          <Editor
+            options={{
+              minimap: {
+                enabled: false,
+              },
+              padding: {
+                top: 12,
+                bottom: 12,
+              },
+              scrollBeyondLastLine: false,
+              scrollbar: {
+                alwaysConsumeMouseWheel: false,
+              },
+            }}
+            value={fileContent}
+            language="python"
+            onChange={setFileContent}
+            theme={theme === 'dark' ? 'vs-dark' : 'vs'}
+            onMount={(editor) => {
+              editor.onDidBlurEditorWidget(() => {
+                const value = editor.getValue();
+
+                if (selectedFile && value) {
+                  dispatch(
+                    FilesActions.updateFileContent({
+                      relativePath:
+                        selectedFile.relativePath ??
+                        getIdWithoutRootPathSegments(selectedFile.id),
+                      fileName: selectedFile.name,
+                      content: value,
+                      contentType: selectedFile.contentType,
+                    }),
+                  );
+                }
+              });
+            }}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
 
 interface SourceFilesEditorProps {
   value?: string;
@@ -44,30 +259,7 @@ const _SourceFilesEditor: FC<SourceFilesEditorProps> = ({
 
   const dispatch = useAppDispatch();
 
-  const theme = useAppSelector(UISelectors.selectThemeState);
-  const files = useAppSelector(FilesSelectors.selectFiles);
-  const uploadedContent = useAppSelector(FilesSelectors.selectFileContent);
-  const isUploadingContent = useAppSelector(
-    FilesSelectors.selectIsFileContentLoading,
-  );
-
-  const [selectedFile, setSelectedFile] = useState<DialFile>();
-  const [fileContent, setFileContent] = useState<string | undefined>(
-    uploadedContent,
-  );
   const [isFolderModalOpen, setIsFolderModalOpen] = useState(false);
-
-  const folderFiles = useMemo(() => {
-    if (value) {
-      return files.filter((file) => file.id.startsWith(`${value}/`));
-    }
-    return [];
-  }, [files, value]);
-
-  const folderFileNames = useMemo(
-    () => folderFiles.map((f) => f.name),
-    [folderFiles],
-  );
 
   const handleToggleFileManager = useCallback(() => {
     setIsFolderModalOpen((p) => !p);
@@ -83,44 +275,12 @@ const _SourceFilesEditor: FC<SourceFilesEditorProps> = ({
     [onChange],
   );
 
-  const handleSelectFile = (file: DialFile) => {
-    setSelectedFile(file);
-  };
-
-  useEffect(() => {
-    if (folderFiles.length && !selectedFile) {
-      const appFile = folderFiles.find((file) => file.name === 'app.py');
-      if (appFile) {
-        setSelectedFile(appFile);
-      } else {
-        setSelectedFile(folderFiles[0]);
-      }
-    }
-  }, [folderFiles, selectedFile]);
-
   useEffect(() => {
     if (value) {
       dispatch(FilesActions.getFilesWithFolders({ id: value }));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  useEffect(() => {
-    if (selectedFile) {
-      dispatch(FilesActions.getFileTextContent({ id: selectedFile.id }));
-    }
-  }, [dispatch, selectedFile]);
-
-  useEffect(() => {
-    if (uploadedContent) {
-      setFileContent(uploadedContent);
-    }
-  }, [uploadedContent]);
-  useEffect(() => {
-    if (value) {
-      setValue('sourceFiles', folderFileNames, { shouldValidate: true });
-    }
-  }, [folderFileNames, setValue, value]);
 
   return (
     <>
@@ -165,74 +325,7 @@ const _SourceFilesEditor: FC<SourceFilesEditorProps> = ({
 
       <FieldErrorMessage error={error} className="mt-1" />
 
-      {value && (
-        <div className="grid w-full grid-cols-[1fr_2fr] gap-1">
-          <div className="flex w-full flex-col gap-0.5 rounded border border-tertiary bg-layer-3 p-3">
-            {folderFiles.map((file) => (
-              <button
-                key={file.id}
-                type="button"
-                onClick={() => handleSelectFile(file)}
-                className="block w-full"
-              >
-                <FilesRow
-                  item={file}
-                  featureContainerClassNames="!w-full"
-                  itemComponentClassNames={classNames(
-                    '!h-[30px] w-full rounded',
-                    selectedFile?.id === file.id
-                      ? 'border-l-2 border-accent-primary bg-accent-primary-alpha'
-                      : 'border-l-2 border-transparent',
-                  )}
-                />
-              </button>
-            ))}
-          </div>
-          <div className="h-[400px] w-full rounded border border-tertiary bg-layer-3 p-3">
-            {isUploadingContent ? (
-              <Loader />
-            ) : (
-              <Editor
-                options={{
-                  minimap: {
-                    enabled: false,
-                  },
-                  padding: {
-                    top: 12,
-                    bottom: 12,
-                  },
-                  scrollBeyondLastLine: false,
-                  scrollbar: {
-                    alwaysConsumeMouseWheel: false,
-                  },
-                }}
-                value={fileContent}
-                language="python"
-                onChange={setFileContent}
-                theme={theme === 'dark' ? 'vs-dark' : 'vs'}
-                onMount={(editor) => {
-                  editor.onDidBlurEditorWidget(() => {
-                    const value = editor.getValue();
-
-                    if (selectedFile && value) {
-                      dispatch(
-                        FilesActions.updateFileContent({
-                          relativePath:
-                            selectedFile.relativePath ??
-                            getIdWithoutRootPathSegments(selectedFile.id),
-                          fileName: selectedFile.name,
-                          content: value,
-                          contentType: selectedFile.contentType,
-                        }),
-                      );
-                    }
-                  });
-                }}
-              />
-            )}
-          </div>
-        </div>
-      )}
+      <CodeEditor sourcesFolderId={value} setValue={setValue} />
 
       <SelectFolderModal
         isOpen={isFolderModalOpen}
