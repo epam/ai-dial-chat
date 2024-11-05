e9d18b8c720e8b6b126634f9e376c2182ded8067
Aliaksandr Kezik <a.kezzk@gmail.com>
2024-11-05 18:19:59 +0100

    array -> set && optimized conditions

--- apps/chat/src/components/Common/ApplicationWizard/CodeAppView/CodeEditor.tsx
+++ apps/chat/src/components/Common/ApplicationWizard/CodeAppView/CodeEditor.tsx
@@ -216,25 +216,19 @@ export const CodeEditor = ({ sourcesFolderId, setValue }: Props) => {
 
   useEffect(() => {
     if (!selectedFile) {
-      const uploadedFiles = folderFiles.filter((file) => !file.status);
-      const isNothingLoading = folderFiles.every(
-        (file) => file.status !== UploadStatus.LOADING,
+      const uploadedFiles = folderFiles.filter(
+        (file) => !file.status && !deletingFilesIds.has(file.id),
       );
 
-      if (folderFiles.length && uploadedFiles.length && isNothingLoading) {
+      if (uploadedFiles.length) {
         const appFile = rootFiles.find(
-          (file) =>
-            file.name === CODEAPPS_REQUIRED_FILES.APP &&
-            !deletingFilesIds.includes(file.id) &&
-            file.status !== UploadStatus.FAILED,
+          (file) => file.name === CODEAPPS_REQUIRED_FILES.APP && !file.status,
         );
 
         if (appFile) {
           setSelectedFile(appFile);
         } else {
-          setSelectedFile(
-            uploadedFiles.find((file) => !deletingFilesIds.includes(file.id)),
-          );
+          setSelectedFile(uploadedFiles[0]);
         }
       } else {
         setSelectedFile(undefined);
