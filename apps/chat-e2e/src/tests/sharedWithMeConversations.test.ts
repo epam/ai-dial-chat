68d94de8ce62ec348b01a99e51b8e652525b0e5c
Ilya Bondar <ilya_bondar@epam.com>
2024-11-18 17:35:45 +0100

    format:fix

--- apps/chat-e2e/src/tests/sharedWithMeConversations.test.ts
+++ apps/chat-e2e/src/tests/sharedWithMeConversations.test.ts
@@ -2,7 +2,6 @@ import { Conversation } from '@/chat/types/chat';
 import { FolderInterface } from '@/chat/types/folder';
 import { DialAIEntityModel } from '@/chat/types/models';
 import { ShareByLinkResponseModel } from '@/chat/types/share';
-
 import config from '@/config/chat.playwright.config';
 import dialTest from '@/src/core/dialFixtures';
 import dialSharedWithMeTest from '@/src/core/dialSharedWithMeFixtures';
