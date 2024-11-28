1f6346f7e3cc635588362e990124a43a684a692f
Irina_Kartun <irina.kartun@gmail.com>
2024-11-28 14:14:23 +0100

    fix/2651-hide-empty-conversation: to revert

--- apps/chat-e2e/config/chat.playwright.config.ts
+++ apps/chat-e2e/config/chat.playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   ],
   outputDir: `../${ResultFolder.testResults}`,
   timeout: 60000,
-  retries: 1,
+  retries: 0,
   maxFailures: 0,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
