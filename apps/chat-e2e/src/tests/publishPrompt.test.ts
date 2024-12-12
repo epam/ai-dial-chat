67cffda37a7ae9b96f622c4d30393319ccba043c
Maksim Nartov <nartovm1994@gmail.com>
2024-12-12 19:50:20 +0100

    EPMRTC-3599, EPMRTC-3600

--- apps/chat-e2e/src/tests/publishPrompt.test.ts
+++ apps/chat-e2e/src/tests/publishPrompt.test.ts
@@ -14,10 +14,11 @@ import { GeneratorUtil } from '@/src/utils';
 
 const publicationsToUnpublish: Publication[] = [];
 
-dialAdminTest.only(
+dialAdminTest(
   'Publish single prompt: select folder in Organization path\n' +
     'Publish prompt: create folder in Organization path\n' +
-    'Publish single prompt: rename folder in Organization',
+    'Publish single prompt: rename folder in Organization\n' +
+    'Publish prompt:add, rename and delete options for new folder in Change path',
   async ({
     dialHomePage,
     promptData,
@@ -41,11 +42,16 @@ dialAdminTest.only(
     organizationFolderPrompts,
     confirmationDialog,
     folderDropdownMenu,
-           errorToastAssertion,
-           errorToast,
+    errorToastAssertion,
+    errorToast,
   }) => {
     dialAdminTest.slow();
-    setTestIds('EPMRTC-3305', 'EPMRTC-3595', 'EPMRTC-3313');
+    setTestIds(
+      'EPMRTC-3305',
+      'EPMRTC-3595',
+      'EPMRTC-3313',
+      'EPMRTC-3596',
+      );
     let prompt1: Prompt;
     let prompt2: Prompt;
     const folderName = GeneratorUtil.randomString(10);
@@ -99,11 +105,14 @@ dialAdminTest.only(
         await dialHomePage.waitForPageLoaded();
         await prompts.openEntityDropdownMenu(prompt1.name);
         await promptDropdownMenu.selectMenuOption(MenuOptions.publish);
-        await baseAssertion.assertElementState(publishingRequestModal, 'visible');
+        await baseAssertion.assertElementState(
+          publishingRequestModal,
+          'visible',
+        );
         await publishingRequestModal
           .getChangePublishToPath()
           .changeButton.click();
-      }
+      },
     );
 
     await dialTest.step(
@@ -124,59 +133,16 @@ dialAdminTest.only(
         await dialHomePage.waitForPageLoaded();
         await prompts.openEntityDropdownMenu(prompt1.name);
         await promptDropdownMenu.selectMenuOption(MenuOptions.publish);
-        await baseAssertion.assertElementState(publishingRequestModal, 'visible');
-        await publishingRequestModal
-          .getChangePublishToPath()
-          .changeButton.click();
-      }
-    );
-
-    await dialTest.step(
-      'User creates new folder structure : Folder1->Folder1.1->Folder1.1.1-Folder1.1.1.1',
-      async () => {
-        await selectFolderModal.newFolderButton.click();
-        await selectFolders.editFolderNameWithEnter(`${folderName} 1`);
-        for (let i = 1; i < 4; i++) {
-          await selectFolders.openFolderDropdownMenu(`${folderName} ${i}`);
-          await folderDropdownMenu.selectMenuOption(MenuOptions.addNewFolder);
-          await selectFolders.editFolderNameWithEnter(`${folderName} ${i+1}`);
-        }
-        await selectFolders.openFolderDropdownMenu(`${folderName} 4`);
-        await folderDropdownMenu.selectMenuOption(MenuOptions.addNewFolder);
-        // Assertions
-        await errorToastAssertion.assertToastIsVisible();
-        await errorToastAssertion.assertToastMessage(ExpectedConstants.tooManyNestedFolders, ExpectedMessages.tooManyNestedFolders);
-        // Bug that closing the toast leads to the closing the modal
-        await errorToast.closeToast();
-        await publishingRequestModal
-          .getChangePublishToPath()
-          .changeButton.click();
-        await selectFolders
-          .getFolderByName(`${folderName} 1`)
-          .waitFor({ state: 'visible' });
-        await selectFolders
-          .getFolderByName(folderName)
-          .waitFor({ state: 'visible' });
-        await selectFolders.openFolderDropdownMenu(`${folderName} 1`);
-        await folderDropdownMenu.selectMenuOption(MenuOptions.delete);
-        await confirmationDialog.confirm();
-      },
-    );
-
-    await dialTest.step(
-      'User reloads the page and reopens the modal in order to workaround the 2803 issue',
-      async () => {
-        await dialHomePage.reloadPage();
-        await dialHomePage.waitForPageLoaded();
-        await prompts.openEntityDropdownMenu(prompt1.name);
-        await promptDropdownMenu.selectMenuOption(MenuOptions.publish);
-        await baseAssertion.assertElementState(publishingRequestModal, 'visible');
+        await baseAssertion.assertElementState(
+          publishingRequestModal,
+          'visible',
+        );
         await publishingRequestModal
           .getChangePublishToPath()
           .changeButton.click();
         await selectFolderModal.newFolderButton.click();
         await selectFolders.editFolderNameWithEnter(folderName);
-      }
+      },
     );
 
     await dialTest.step('User selects renamed folder', async () => {
@@ -460,6 +426,211 @@ dialAdminTest.only(
     );
   },
 );
+
+
+dialAdminTest.only(
+  'Publish prompt: add new folder inside nested folder structure with depth 4\n' +
+  'Publish prompt into nested folder structure inside Organization section',
+  async ({
+           dialHomePage,
+           promptData,
+           dataInjector,
+           prompts,
+           promptDropdownMenu,
+           publishingRequestModal,
+           selectFolderModal,
+           adminDialHomePage,
+           adminApproveRequiredPromptsAssertion,
+           adminApproveRequiredPrompts,
+           adminPublishingApprovalModal,
+           adminPublishingApprovalModalAssertion,
+           setTestIds,
+           baseAssertion,
+           selectFolders,
+           publishedPromptPreviewModal,
+           adminPromptToApproveAssertion,
+           adminPublishedPromptPreviewModalAssertion,
+           promptBarOrganizationFolderAssertion,
+           organizationFolderPrompts,
+           folderDropdownMenu,
+           errorToastAssertion,
+           errorToast,
+         }) => {
+    dialAdminTest.slow();
+    setTestIds(
+      'EPMRTC-3599',
+      'EPMRTC-3600'
+    );
+    let prompt1: Prompt;
+    let folderName = GeneratorUtil.randomString(10);
+    let publicationPath = `${PublishPath.Organization}/${folderName} 1/${folderName} 2/${folderName} 3/${folderName} 4`;
+    const requestName1 = GeneratorUtil.randomPublicationRequestName();
+    let publishApiModels: {
+      request: PublicationRequestModel;
+      response: Publication;
+    };
+
+    await dialTest.step('Prepare a new prompt', async () => {
+      prompt1 = promptData.prepareDefaultPrompt();
+      await dataInjector.createPrompts([prompt1]);
+    });
+
+    await dialTest.step('Publish a single prompt', async () => {
+      await dialHomePage.openHomePage();
+      await dialHomePage.waitForPageLoaded();
+      // await promptBar.createNewPrompt();
+      await prompts.openEntityDropdownMenu(prompt1.name);
+      await promptDropdownMenu.selectMenuOption(MenuOptions.publish);
+      await baseAssertion.assertElementState(publishingRequestModal, 'visible');
+    });
+
+    await dialTest.step(
+      'User clicks on "Change path, creates new folder structure : Folder1->Folder1.1->Folder1.1.1-Folder1.1.1.1',
+      async () => {
+        await publishingRequestModal
+          .getChangePublishToPath()
+          .changeButton.click();
+        await selectFolderModal.newFolderButton.click();
+        await selectFolders.editFolderNameWithEnter(`${folderName} 1`);
+        for (let i = 1; i < 4; i++) {
+          await selectFolders.openFolderDropdownMenu(`${folderName} ${i}`);
+          await folderDropdownMenu.selectMenuOption(MenuOptions.addNewFolder);
+          await selectFolders.editFolderNameWithEnter(`${folderName} ${i + 1}`);
+        }
+        await selectFolders.openFolderDropdownMenu(`${folderName} 4`);
+        await folderDropdownMenu.selectMenuOption(MenuOptions.addNewFolder);
+        // Assertions
+        await errorToastAssertion.assertToastIsVisible();
+        await errorToastAssertion.assertToastMessage(
+          ExpectedConstants.tooManyNestedFolders,
+          ExpectedMessages.tooManyNestedFolders,
+        );
+        // Bug that closing the toast leads to the closing the modal
+        await errorToast.closeToast();
+        await publishingRequestModal
+          .getChangePublishToPath()
+          .changeButton.click();
+        await selectFolders
+          .getFolderByName(folderName)
+          .waitFor({ state: 'visible' });
+        for (let i = 1; i < 4; i++) {
+          await selectFolders
+            .getFolderByName(`${folderName} ${i}`)
+            .waitFor({ state: 'visible' });
+        }
+        folderName = `${folderName} 4`
+      },
+    );
+
+    await dialTest.step('User selects nested folder', async () => {
+      await selectFolderModal.selectFolder(folderName);
+      await selectFolderModal.clickSelectFolderButton({
+        triggeredApiHost: API.publicationRulesList,
+      });
+    });
+
+    await dialTest.step(
+      'Set publication request name, check prompt to publish and send request',
+      async () => {
+        await publishingRequestModal.requestName.fillInInput(requestName1);
+        await baseAssertion.assertElementText(
+          publishingRequestModal.getChangePublishToPath().path,
+          publicationPath,
+        );
+        publishApiModels =
+          await publishingRequestModal.sendPublicationRequest();
+        publicationsToUnpublish.push(publishApiModels.response);
+      },
+    );
+
+    await dialAdminTest.step(
+      'Login as admin and verify conversation publishing request is displayed under "Approve required" section',
+      async () => {
+        await adminDialHomePage.openHomePage();
+        await adminDialHomePage.waitForPageLoaded();
+        await adminApproveRequiredPromptsAssertion.assertFolderState(
+          { name: requestName1 },
+          'visible',
+        );
+      },
+    );
+
+    await dialAdminTest.step(
+      'Expand request folder and verify "Publication approval" modal is displayed',
+      async () => {
+        await adminApproveRequiredPrompts.expandApproveRequiredFolder(
+          requestName1,
+        );
+        await adminApproveRequiredPromptsAssertion.assertFolderEntityState(
+          { name: requestName1 },
+          { name: prompt1.name },
+          'visible',
+        );
+        await adminPublishingApprovalModalAssertion.assertElementState(
+          adminPublishingApprovalModal,
+          'visible',
+        );
+        await adminPromptToApproveAssertion.assertEntityState(
+          { name: prompt1.name },
+          'visible',
+        );
+        await adminPromptToApproveAssertion.assertEntityColor(
+          { name: prompt1.name },
+          Colors.textPrimary,
+        );
+        await adminPromptToApproveAssertion.assertEntityVersion(
+          { name: prompt1.name },
+          ExpectedConstants.defaultAppVersion,
+        );
+        await adminPromptToApproveAssertion.assertEntityVersionColor(
+          { name: prompt1.name },
+          Colors.textPrimary,
+        );
+      },
+    );
+
+    await dialAdminTest.step(
+      'Click on "Go to a review" button and verify conversation details are displayed',
+      async () => {
+        await adminPublishingApprovalModal.goToEntityReview({
+          isHttpMethodTriggered: false,
+        });
+        await adminPublishedPromptPreviewModalAssertion.assertPromptPreviewModalState(
+          'visible',
+        );
+        await adminPublishedPromptPreviewModalAssertion.assertPromptPreviewModalTitle(
+          prompt1.name,
+        );
+        await adminPublishedPromptPreviewModalAssertion.assertPromptName(
+          prompt1.name,
+        );
+        await adminPublishedPromptPreviewModalAssertion.assertPromptContent(
+          prompt1.content!,
+        );
+        await publishedPromptPreviewModal.backToPublicationButton.click();
+        await adminPublishingApprovalModal.approveRequest();
+      },
+    );
+
+    await dialTest.step(
+      'by user1 reload page and check prompt in Organization section inside folder1',
+      async () => {
+        await dialHomePage.reloadPage();
+        await dialHomePage.waitForPageLoaded();
+        await promptBarOrganizationFolderAssertion.assertFolderState(
+          { name: folderName },
+          'visible',
+        );
+        await organizationFolderPrompts.expandFolder(folderName);
+        await promptBarOrganizationFolderAssertion.assertFolderEntityState(
+          { name: folderName },
+          { name: prompt1.name },
+          'visible',
+        );
+      },
+    );
+  },
+)
 
 dialTest.afterAll(
   async ({ publicationApiHelper, adminPublicationApiHelper }) => {
