c7c69d67bb031ec11fc780e823741ccdff89f4b4
Aliaksandr Kezik <a.kezzk@gmail.com>
2024-12-23 13:09:13 +0100

    refactor with ref and Promise

--- apps/chat/src/hooks/useTokenizer.ts
+++ apps/chat/src/hooks/useTokenizer.ts
@@ -1,32 +1,33 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 import { DialAIEntityModel } from '../types/models';
 
 import { Tiktoken, get_encoding } from 'tiktoken';
 
 export const useTokenizer = (tokenizer: DialAIEntityModel['tokenizer']) => {
-  const [encoding, setEncoding] = useState<Tiktoken>();
+  const encodingRef = useRef<Tiktoken | null>(null);
 
   useEffect(() => {
-    // use an event loop macro task to avoid blocking rendering
-    const timeoutId = setTimeout(() => {
-      if (tokenizer?.encoding) {
-        setEncoding(get_encoding(tokenizer.encoding));
+    // use microtask to not block the thread and isMounted variable to prevent task execution if component unmounted
+    let isMounted = true;
+    Promise.resolve().then(() => {
+      if (isMounted && tokenizer?.encoding) {
+        encodingRef.current = get_encoding(tokenizer.encoding);
       }
-    }, 0);
+    });
 
     return () => {
-      clearTimeout(timeoutId);
-      encoding?.free();
+      isMounted = false;
     };
-  }, [encoding, tokenizer]);
+  }, [tokenizer]);
 
-  const getTokensLength = useCallback(
-    (str: string) => {
-      return encoding?.encode(str).length ?? new Blob([str]).size;
-    },
-    [encoding],
-  );
+  useEffect(() => {
+    return () => encodingRef.current?.free();
+  }, []);
+
+  const getTokensLength = useCallback((str: string) => {
+    return encodingRef.current?.encode(str).length ?? new Blob([str]).size;
+  }, []);
 
   return {
     getTokensLength,
