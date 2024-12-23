d64484450f39757c7825e92b795f3d362860c867
Aliaksandr Kezik <a.kezzk@gmail.com>
2024-12-23 11:57:36 +0100

    fix(chat): fix settings modal opening delay (Issue #2863)

--- apps/chat/src/hooks/useTokenizer.ts
+++ apps/chat/src/hooks/useTokenizer.ts
@@ -8,13 +8,19 @@ export const useTokenizer = (tokenizer: DialAIEntityModel['tokenizer']) => {
   const [encoding, setEncoding] = useState<Tiktoken | undefined>(undefined);
 
   useEffect(() => {
-    if (tokenizer?.encoding) {
-      setEncoding(get_encoding(tokenizer.encoding));
-    }
+    // use an event loop macro task to avoid blocking rendering
+    const timeoutId = setTimeout(() => {
+      if (tokenizer?.encoding) {
+        setEncoding(get_encoding(tokenizer.encoding));
+      }
+    }, 0);
+
+    return () => clearTimeout(timeoutId);
   }, [tokenizer]);
 
   useEffect(() => {
     return () => encoding?.free();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const getTokensLength = useCallback(
