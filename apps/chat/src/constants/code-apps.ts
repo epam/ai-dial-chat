b36ca47f3f74af218cc482e7c4552fbeab1587e6
Ilya Bondar <ilya_bondar@epam.com>
2024-12-27 17:35:26 +0100

    add SSL_CERT_FILE and remove DIAL_URL

--- apps/chat/src/constants/code-apps.ts
+++ apps/chat/src/constants/code-apps.ts
@@ -284,7 +284,7 @@ def get_last_attachment_url(messages: List[Message]) -> str:
       '/openai/deployments/simple-rag/chat/completions',
   },
   variables: {
-    DIAL_URL: () => DefaultsService.get('dialApiHost', ''),
+    SSL_CERT_FILE: () => "/etc/ssl/certs/ca-certificates.crt",
   },
 };
 
