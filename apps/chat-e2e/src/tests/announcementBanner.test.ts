9629ad17bc2d63f78f6306f00d01053b359d4d4c
Ilya Bondar <ilya_bondar@epam.com>
2025-01-08 17:17:16 +0100

    fix(chat): revert login/logout changes (#2912)

--- apps/chat-e2e/src/tests/announcementBanner.test.ts
+++ apps/chat-e2e/src/tests/announcementBanner.test.ts
@@ -7,28 +7,24 @@ dialTest(
   'Banner is shown.\n' +
     'Banner text contains html link.\n' +
     "Banner doesn't appear if to close it",
-  async (
-    {
-      dialHomePage,
-      conversationData,
-      dataInjector,
-      chatBar,
-      promptBar,
-      conversations,
-      banner,
-      header,
-      appContainer,
-      accountSettings,
-      accountDropdownMenu,
-      confirmationDialog,
-      providerLogin,
-      setTestIds,
-    },
-    testInfo,
-  ) => {
+  async ({
+    dialHomePage,
+    conversationData,
+    dataInjector,
+    chatBar,
+    promptBar,
+    conversations,
+    banner,
+    header,
+    appContainer,
+    chatMessages,
+    accountSettings,
+    accountDropdownMenu,
+    confirmationDialog,
+    providerLogin,
+    setTestIds,
+  }) => {
     setTestIds('EPMRTC-1576', 'EPMRTC-1580', 'EPMRTC-1577');
-    const username =
-      process.env.E2E_USERNAME!.split(',')[+process.env.TEST_PARALLEL_INDEX!];
     let conversation: Conversation;
     let chatBarBounding;
     let promptBarBounding;
@@ -149,12 +145,7 @@ dialTest(
         await accountDropdownMenu.selectMenuOption(AccountMenuOptions.logout);
         await confirmationDialog.confirm();
         await providerLogin.navigateToCredentialsPage();
-        await providerLogin.authProviderLogin(
-          testInfo,
-          username,
-          process.env.E2E_PASSWORD!,
-          false,
-        );
+        await chatMessages.waitForState({ state: 'attached' });
         await expect
           .soft(banner.getElementLocator(), ExpectedMessages.bannerIsClosed)
           .toBeHidden();
