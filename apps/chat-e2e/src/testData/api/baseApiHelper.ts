81aba2ca7fee3d88189c508612d046fe64d676ee
Irina_Kartun <irina.kartun@gmail.com>
2025-01-10 15:48:37 +0100

    feat/overlay-settings-tests: fixed api host retrieval

--- apps/chat-e2e/src/testData/api/baseApiHelper.ts
+++ apps/chat-e2e/src/testData/api/baseApiHelper.ts
@@ -1,4 +1,4 @@
-import config from '@/config/chat.playwright.config';
+import config, { overlayHost } from '@/config/chat.playwright.config';
 import { APIRequestContext } from '@playwright/test';
 
 export class BaseApiHelper {
@@ -11,19 +11,8 @@ export class BaseApiHelper {
   //function to override the API host if overlay sandbox is running
   public getHost(endpoint: string) {
     const baseUrl = config.use!.baseURL;
-    //overlay sandbox host includes 'overlay' prefix on CI env
-    if (process.env.E2E_HOST) {
-      if (baseUrl?.includes('overlay')) {
-        endpoint = process.env.NEXT_PUBLIC_OVERLAY_HOST + endpoint;
-      }
-    } else {
-      //overlay sandbox has different port on local env
-      if (
-        process.env.NEXT_PUBLIC_OVERLAY_HOST &&
-        baseUrl !== process.env.NEXT_PUBLIC_OVERLAY_HOST
-      ) {
-        endpoint = process.env.NEXT_PUBLIC_OVERLAY_HOST + endpoint;
-      }
+    if (baseUrl === overlayHost) {
+      endpoint = process.env.NEXT_PUBLIC_OVERLAY_HOST + endpoint;
     }
     return endpoint;
   }
