3d2869717378d6624e987f3b03929ba40990135f
Irina_Kartun <irina.kartun@gmail.com>
2025-01-21 10:39:46 +0100

    feat/flaky-model-image-test: fixed import

--- apps/chat-e2e/src/assertions/sendMessageAssertion.ts
+++ apps/chat-e2e/src/assertions/sendMessageAssertion.ts
@@ -1,4 +1,4 @@
-import { BaseAssertion } from '@/src/assertions/base/baseAssertion';
+import { BaseAssertion } from '@/src/assertions/baseAssertion';
 import {
   ElementActionabilityState,
   ElementState,
