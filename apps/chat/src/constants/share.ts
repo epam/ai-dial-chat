eb0416532e20ba70a79498c20977aba6f51bb1f0
Derikyan <armen.derikyan@gmail.com>
2025-01-21 16:48:00 +0400

    resolve conflicts

--- apps/chat/src/constants/share.ts
+++ apps/chat/src/constants/share.ts
@@ -1,1 +1,9 @@
+import { SourceType } from '../types/common';
+
 export const SHARE_QUERY_PARAM = 'share';
+
+export const SOURCE_TYPES = [
+  SourceType.Public,
+  SourceType.SharedWithMe,
+  SourceType.CreatedByMe,
+];
