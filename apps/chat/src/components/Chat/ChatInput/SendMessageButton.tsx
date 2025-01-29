96508c72916a0504e26a696dedd565640fa5fdbe
Yauheniya Taleika <yauheniya_taleika@epam.com>
2025-01-29 20:39:14 +0100

    correct types for aria-label attribute

--- apps/chat/src/components/Chat/ChatInput/SendMessageButton.tsx
+++ apps/chat/src/components/Chat/ChatInput/SendMessageButton.tsx
@@ -58,7 +58,7 @@ export const SendMessageButton = Inversify.register(
             isLastMessageError && 'text-error',
             isOverlay ? 'right-3' : 'right-4',
           )}
-          aria-label={t('Send a message')}
+          aria-label={`${t('Send a message')}`}
           onClick={onSend}
           data-qa="regenerate"
         >
@@ -80,7 +80,7 @@ export const SendMessageButton = Inversify.register(
           'absolute top-[calc(50%_-_12px)] rounded hover:text-accent-primary disabled:cursor-not-allowed disabled:text-secondary',
           isOverlay ? 'right-3' : 'right-4',
         )}
-        aria-label={t('Send a message')}
+        aria-label={`${t('Send a message')}`}
         onClick={onSend}
         disabled={disabled}
         data-qa={dataQa}
