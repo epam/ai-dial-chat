d927e3bd2b06e520c8ba142bedcb0a8f5a79d3ef
Ilya Bondar <ilya_bondar@epam.com>
2025-01-30 01:34:17 +0100

    remove ignore lint comment

--- apps/chat/src/hooks/useTranslation.ts
+++ apps/chat/src/hooks/useTranslation.ts
@@ -6,7 +6,7 @@ import { Translation, TranslationOptions } from '../types/translation';
 
 export const useTranslation = (translationNamespace: Translation) => {
   const { t } = useNextTranslation(translationNamespace);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+
   const translate = useCallback(
     (key: string, options?: TranslationOptions) =>
       ((options ? t(key, options) : t(key)) as unknown as string) ?? key ?? '',
