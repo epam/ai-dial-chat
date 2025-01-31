a81217d34eb0573882c071ad23e9c7e586a177e8
Aliaksandr Kezik <a.kezzk@gmail.com>
2025-01-31 13:15:56 +0100

    fix(chat): fix replay as is start screen for not available modelId (Issue #2847)

--- apps/chat/src/utils/server/get-common-page-props.ts
+++ apps/chat/src/utils/server/get-common-page-props.ts
@@ -1,4 +1,5 @@
 import { GetServerSideProps } from 'next';
+import { getToken } from 'next-auth/jwt';
 import { getServerSession } from 'next-auth/next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
