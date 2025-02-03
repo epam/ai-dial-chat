a2cb220385252cf91b42275509d53057e6a3e7fd
Aliaksandr Kezik <a.kezzk@gmail.com>
2025-02-03 13:10:01 +0100

    remoce logs

--- apps/chat/src/store/prompts/prompts.epics.ts
+++ apps/chat/src/store/prompts/prompts.epics.ts
@@ -222,12 +222,6 @@ const moveOrUpdatePromptEpic: AppEpic = (action$) =>
   action$.pipe(
     filter(PromptsActions.moveOrUpdatePrompt.match),
     switchMap(({ payload }) => {
-      console.log(
-        typeof payload.newValues.name === 'string' &&
-          payload.newValues.name !== payload.prompt.name,
-      );
-      console.log(payload.prompt);
-      console.log(payload.newValues);
       if (
         typeof payload.newValues.name === 'string' &&
         payload.newValues.name !== payload.prompt.name
