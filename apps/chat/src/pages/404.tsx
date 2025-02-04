bcb084478218db06b3718dec4a061bda4bb8ce96
Valery <valery_dluski@epam.com>
2025-02-04 10:27:31 +0100

    fix(404): update translation hook import path

--- apps/chat/src/pages/404.tsx
+++ apps/chat/src/pages/404.tsx
@@ -1,6 +1,6 @@
 import { IconPlus } from '@tabler/icons-react';
 
-import { useTranslation } from 'next-i18next';
+import { useTranslation } from '../hooks/useTranslation';
 
 import { Translation } from '../types/translation';
 
