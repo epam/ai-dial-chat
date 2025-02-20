5edd0d4d49018c5a542cd131592c61ef85c6efe8
Ilya Bondar <ilya_bondar@epam.com>
2025-02-20 21:42:48 +0100

    import

--- apps/chat/src/components/Marketplace/TopicsList.tsx
+++ apps/chat/src/components/Marketplace/TopicsList.tsx
@@ -1,9 +1,8 @@
-import React, { memo, useEffect, useRef, useState } from 'react';
-
-import { stopBubbling } from '@/src/constants/chat';
-
 import Tooltip from '../Common/Tooltip';
 import { ApplicationTopic } from './ApplicationTopic';
+
+import { stopBubbling } from '@/src/constants/chat';
+import React, { memo, useCallback, useEffect, useRef, useState } from 'react';
 
 interface AllTopicsProps {
   topics: string[];
