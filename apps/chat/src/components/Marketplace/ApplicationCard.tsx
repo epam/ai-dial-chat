e2a00234fc63557707f7d6ef432bbbb4d3f246cc
Aliaksandr Kezik <a.kezzk@gmail.com>
2025-03-03 14:25:34 +0100

    wrapp ApplicationCard with memo to optimize render

--- apps/chat/src/components/Marketplace/ApplicationCard.tsx
+++ apps/chat/src/components/Marketplace/ApplicationCard.tsx
@@ -6,7 +6,7 @@ import {
   IconUserShare,
   IconWorldShare,
 } from '@tabler/icons-react';
-import React, { useCallback, useMemo } from 'react';
+import React, { memo, useCallback, useMemo } from 'react';
 
 import classNames from 'classnames';
 
@@ -97,293 +97,300 @@ export interface ApplicationCardProps {
   isPreview?: boolean;
 }
 
-export const ApplicationCard = ({
-  entity,
-  onClick,
-  onDelete,
-  onEdit,
-  onBookmarkClick,
-  onPublish,
-  onLogsClick,
-  isPreview = false,
-}: ApplicationCardProps) => {
-  const { t } = useTranslation(Translation.Marketplace);
-
-  const dispatch = useAppDispatch();
-  const screenState = useScreenState();
-
-  const isCodeAppsEnabled = useAppSelector((state) =>
-    SettingsSelectors.isFeatureEnabled(state, Feature.CodeApps),
-  );
-  const isAdmin = useAppSelector(AuthSelectors.selectIsAdmin);
-
-  const isMyApp = isMyApplication(entity);
-  const isPublicApp = isApplicationPublic(entity);
-
-  const canWrite = canWriteSharedWithMe(entity);
-
-  const isModifyDisabled = isApplicationStatusUpdating(entity);
-  const playerStatus = getApplicationSimpleStatus(entity);
-  const isExecutable =
-    isExecutableApp(entity) && (isMyApp || isAdmin || canWrite);
-
-  const { iconSize, shareIconSize } = CardIconSizes[screenState];
-
-  const PlayerContextIcon = PlayerContextIcons[playerStatus];
-
-  const handleUpdateFunctionStatus = useCallback(
-    (e: React.MouseEvent) => {
-      e.stopPropagation();
-      dispatch(
-        ApplicationActions.startUpdatingFunctionStatus({
-          id: entity.id,
-          status: getApplicationNextStatus(entity),
-        }),
-      );
-    },
-    [dispatch, entity],
-  );
-
-  const handleOpenApplicationLogs = useCallback(
-    (e: React.MouseEvent) => {
-      e.preventDefault();
-      e.stopPropagation();
-      onLogsClick?.(entity);
-    },
-    [entity, onLogsClick],
-  );
-
-  const handleOpenSharing = useCallback(
-    (e: React.MouseEvent) => {
-      e.stopPropagation();
-      dispatch(
-        ShareActions.share({
-          featureType: FeatureType.Application,
-          resourceId: entity.id,
-        }),
-      );
-    },
-    [dispatch, entity.id],
-  );
-
-  const handleOpenUnshare = useCallback(
-    (e: React.MouseEvent) => {
-      e.stopPropagation();
-      dispatch(ShareActions.setUnshareEntity(entity));
-    },
-    [dispatch, entity],
-  );
-
-  const isApplicationsSharingEnabled = useAppSelector((state) =>
-    SettingsSelectors.isFeatureEnabled(state, Feature.ApplicationsSharing),
-  );
-
-  const handleCopy = useCallback(
-    (e: React.MouseEvent) => {
-      e.preventDefault();
-      e.stopPropagation();
-      if (!navigator.clipboard) return;
-      const link = getApplicationLink(entity);
-      navigator.clipboard.writeText(link);
-      dispatch(UIActions.showSuccessToast(t('Link copied!')));
-    },
-    [dispatch, entity, t],
-  );
-
-  const handleEdit = useMenuItemHandler(onEdit, entity);
-  const handleDelete = useMenuItemHandler(onDelete, entity);
-  const handlePublish = useMenuItemHandlerWithTwoArgs(
-    onPublish,
+export const ApplicationCard = memo(
+  ({
     entity,
-    PublishActions.ADD,
-  );
-  const handleUnpublish = useMenuItemHandlerWithTwoArgs(
+    onClick,
+    onDelete,
+    onEdit,
+    onBookmarkClick,
     onPublish,
-    entity,
-    PublishActions.DELETE,
-  );
+    onLogsClick,
+    isPreview = false,
+  }: ApplicationCardProps) => {
+    const { t } = useTranslation(Translation.Marketplace);
 
-  const menuItems: DisplayMenuItemProps[] = useMemo(
-    () => [
-      {
-        name: t('Copy link'),
-        dataQa: 'application-copy-link',
-        display: isPublicApp,
-        Icon: IconLink,
-        onClick: handleCopy,
+    const dispatch = useAppDispatch();
+    const screenState = useScreenState();
+
+    const isCodeAppsEnabled = useAppSelector((state) =>
+      SettingsSelectors.isFeatureEnabled(state, Feature.CodeApps),
+    );
+    const isAdmin = useAppSelector(AuthSelectors.selectIsAdmin);
+
+    const isMyApp = isMyApplication(entity);
+    const isPublicApp = isApplicationPublic(entity);
+
+    const canWrite = canWriteSharedWithMe(entity);
+
+    const isModifyDisabled = isApplicationStatusUpdating(entity);
+    const playerStatus = getApplicationSimpleStatus(entity);
+    const isExecutable =
+      isExecutableApp(entity) && (isMyApp || isAdmin || canWrite);
+
+    const { iconSize, shareIconSize } = CardIconSizes[screenState];
+
+    const PlayerContextIcon = PlayerContextIcons[playerStatus];
+
+    const handleUpdateFunctionStatus = useCallback(
+      (e: React.MouseEvent) => {
+        e.stopPropagation();
+        dispatch(
+          ApplicationActions.startUpdatingFunctionStatus({
+            id: entity.id,
+            status: getApplicationNextStatus(entity),
+          }),
+        );
       },
-      {
-        name: t(getPlayerCaption(entity)),
-        dataQa: 'status-change',
-        disabled: playerStatus === SimpleApplicationStatus.UPDATING,
-        display:
-          (isAdmin || isMyApp || canWrite) &&
-          !!entity.functionStatus &&
-          isCodeAppsEnabled,
-        Icon: PlayerContextIcon,
-        iconClassName: PlayerContextIconClasses[playerStatus],
-        onClick: handleUpdateFunctionStatus,
+      [dispatch, entity],
+    );
+
+    const handleOpenApplicationLogs = useCallback(
+      (e: React.MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        onLogsClick?.(entity);
       },
-      {
-        name: t('Edit'),
-        dataQa: 'edit',
-        display: ((isMyApp || canWrite) && !!onEdit) || isPreview,
-        Icon: IconPencilMinus,
-        onClick: handleEdit,
+      [entity, onLogsClick],
+    );
+
+    const handleOpenSharing = useCallback(
+      (e: React.MouseEvent) => {
+        e.stopPropagation();
+        dispatch(
+          ShareActions.share({
+            featureType: FeatureType.Application,
+            resourceId: entity.id,
+          }),
+        );
       },
-      {
-        name: t('Share'),
-        dataQa: 'share',
-        display: isMyApp && isApplicationsSharingEnabled,
-        Icon: IconUserShare,
-        onClick: handleOpenSharing,
+      [dispatch, entity.id],
+    );
+
+    const handleOpenUnshare = useCallback(
+      (e: React.MouseEvent) => {
+        e.stopPropagation();
+        dispatch(ShareActions.setUnshareEntity(entity));
       },
-      {
-        name: t('Unshare'),
-        dataQa: 'unshare',
-        display: !!entity.sharedWithMe && isApplicationsSharingEnabled,
-        Icon: IconUserUnshare,
-        onClick: handleOpenUnshare,
+      [dispatch, entity],
+    );
+
+    const isApplicationsSharingEnabled = useAppSelector((state) =>
+      SettingsSelectors.isFeatureEnabled(state, Feature.ApplicationsSharing),
+    );
+
+    const handleCopy = useCallback(
+      (e: React.MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        if (!navigator.clipboard) return;
+        const link = getApplicationLink(entity);
+        navigator.clipboard.writeText(link);
+        dispatch(UIActions.showSuccessToast(t('Link copied!')));
       },
-      {
-        name: t('Publish'),
-        dataQa: 'publish',
-        display: (isMyApp && !!onPublish) || isPreview,
-        Icon: IconWorldShare,
-        onClick: handlePublish,
-      },
-      {
-        name: t('Unpublish'),
-        dataQa: 'unpublish',
-        display: isEntityIdPublic(entity) && !!onPublish,
-        Icon: UnpublishIcon,
-        onClick: handleUnpublish,
-      },
-      {
-        name: t('Logs'),
-        dataQa: 'app-logs',
-        display:
-          !!isExecutable && playerStatus === SimpleApplicationStatus.UNDEPLOY,
-        Icon: IconFileDescription,
-        onClick: handleOpenApplicationLogs,
-      },
-      {
-        name: t('Delete'),
-        dataQa: 'delete',
-        display: (isMyApp && !!onDelete) || isPreview,
-        disabled: isModifyDisabled,
-        Icon: IconTrashX,
-        iconClassName: 'stroke-error',
-        onClick: handleDelete,
-      },
-    ],
-    [
-      t,
-      isPublicApp,
-      handleCopy,
-      entity,
-      playerStatus,
-      isAdmin,
-      isMyApp,
-      isCodeAppsEnabled,
-      PlayerContextIcon,
-      handleUpdateFunctionStatus,
-      canWrite,
-      onEdit,
-      handleEdit,
-      isApplicationsSharingEnabled,
-      handleOpenSharing,
-      handleOpenUnshare,
+      [dispatch, entity, t],
+    );
+
+    const handleEdit = useMenuItemHandler(onEdit, entity);
+    const handleDelete = useMenuItemHandler(onDelete, entity);
+    const handlePublish = useMenuItemHandlerWithTwoArgs(
       onPublish,
-      handlePublish,
-      handleUnpublish,
-      isExecutable,
-      handleOpenApplicationLogs,
-      onDelete,
-      isModifyDisabled,
-      handleDelete,
-      isPreview,
-    ],
-  );
+      entity,
+      PublishActions.ADD,
+    );
+    const handleUnpublish = useMenuItemHandlerWithTwoArgs(
+      onPublish,
+      entity,
+      PublishActions.DELETE,
+    );
 
-  return (
-    <div
-      onClick={() => onClick(entity)}
-      className={classNames(
-        'group relative h-[98px] rounded-md bg-layer-2 p-3 shadow-card hover:bg-layer-3 md:h-[162px] md:p-4 xl:h-[164px] xl:p-5',
-        !isPreview && 'cursor-pointer',
-      )}
-      data-qa="agent"
-    >
-      <div>
-        <div className="absolute right-4 top-4 flex gap-1 xl:right-5 xl:top-5">
-          {!isPreview && (
-            <>
-              <ContextMenu
-                menuItems={menuItems}
-                featureType={FeatureType.Application}
-                triggerIconHighlight
-                triggerIconSize={18}
-                className="m-0 xl:invisible group-hover:xl:visible"
-              />
+    const menuItems: DisplayMenuItemProps[] = useMemo(
+      () => [
+        {
+          name: t('Copy link'),
+          dataQa: 'application-copy-link',
+          display: isPublicApp,
+          Icon: IconLink,
+          onClick: handleCopy,
+        },
+        {
+          name: t(getPlayerCaption(entity)),
+          dataQa: 'status-change',
+          disabled: playerStatus === SimpleApplicationStatus.UPDATING,
+          display:
+            (isAdmin || isMyApp || canWrite) &&
+            !!entity.functionStatus &&
+            isCodeAppsEnabled,
+          Icon: PlayerContextIcon,
+          iconClassName: PlayerContextIconClasses[playerStatus],
+          onClick: handleUpdateFunctionStatus,
+        },
+        {
+          name: t('Edit'),
+          dataQa: 'edit',
+          display: ((isMyApp || canWrite) && !!onEdit) || isPreview,
+          Icon: IconPencilMinus,
+          onClick: handleEdit,
+        },
+        {
+          name: t('Share'),
+          dataQa: 'share',
+          display: isMyApp && isApplicationsSharingEnabled,
+          Icon: IconUserShare,
+          onClick: handleOpenSharing,
+        },
+        {
+          name: t('Unshare'),
+          dataQa: 'unshare',
+          display: !!entity.sharedWithMe && isApplicationsSharingEnabled,
+          Icon: IconUserUnshare,
+          onClick: handleOpenUnshare,
+        },
+        {
+          name: t('Publish'),
+          dataQa: 'publish',
+          display: (isMyApp && !!onPublish) || isPreview,
+          Icon: IconWorldShare,
+          onClick: handlePublish,
+        },
+        {
+          name: t('Unpublish'),
+          dataQa: 'unpublish',
+          display: isEntityIdPublic(entity) && !!onPublish,
+          Icon: UnpublishIcon,
+          onClick: handleUnpublish,
+        },
+        {
+          name: t('Logs'),
+          dataQa: 'app-logs',
+          display:
+            !!isExecutable && playerStatus === SimpleApplicationStatus.UNDEPLOY,
+          Icon: IconFileDescription,
+          onClick: handleOpenApplicationLogs,
+        },
+        {
+          name: t('Delete'),
+          dataQa: 'delete',
+          display: (isMyApp && !!onDelete) || isPreview,
+          disabled: isModifyDisabled,
+          Icon: IconTrashX,
+          iconClassName: 'stroke-error',
+          onClick: handleDelete,
+        },
+      ],
+      [
+        t,
+        isPublicApp,
+        handleCopy,
+        entity,
+        playerStatus,
+        isAdmin,
+        isMyApp,
+        isCodeAppsEnabled,
+        PlayerContextIcon,
+        handleUpdateFunctionStatus,
+        canWrite,
+        onEdit,
+        handleEdit,
+        isApplicationsSharingEnabled,
+        handleOpenSharing,
+        handleOpenUnshare,
+        onPublish,
+        handlePublish,
+        handleUnpublish,
+        isExecutable,
+        handleOpenApplicationLogs,
+        onDelete,
+        isModifyDisabled,
+        handleDelete,
+        isPreview,
+      ],
+    );
 
-              <AgentBookmark
-                onBookmarkClick={onBookmarkClick}
-                entity={entity}
-              />
-            </>
-          )}
-        </div>
-        <div className="flex items-center gap-4 overflow-hidden">
-          <div className="flex shrink-0 items-center justify-center xl:my-[3px]">
-            <ShareIcon
-              {...entity}
-              isHighlighted={false}
-              size={shareIconSize}
-              featureType={FeatureType.Application}
-              iconClassName="bg-layer-2 !stroke-[0.6] group-hover:bg-transparent !rounded-[4px]"
-              iconWrapperClassName="!rounded-[4px]"
-            >
-              <ModelIcon entityId={entity.id} entity={entity} size={iconSize} />
-            </ShareIcon>
-          </div>
-          <div className="flex grow flex-col justify-center gap-2 overflow-hidden">
-            {entity.version && (
-              <div
-                className={classNames(
-                  'mr-6 flex gap-1 text-xs leading-[14px] text-secondary',
-                  !isMyApp && '!mr-12',
-                )}
-              >
-                {t('Version: ')}
-                <span
-                  className="max-w-full overflow-hidden truncate whitespace-nowrap"
-                  data-qa="version"
-                >
-                  {entity.version}
-                </span>
-              </div>
+    return (
+      <div
+        onClick={() => onClick(entity)}
+        className={classNames(
+          'group relative h-[98px] rounded-md bg-layer-2 p-3 shadow-card hover:bg-layer-3 md:h-[162px] md:p-4 xl:h-[164px] xl:p-5',
+          !isPreview && 'cursor-pointer',
+        )}
+        data-qa="agent"
+      >
+        <div>
+          <div className="absolute right-4 top-4 flex gap-1 xl:right-5 xl:top-5">
+            {!isPreview && (
+              <>
+                <ContextMenu
+                  menuItems={menuItems}
+                  featureType={FeatureType.Application}
+                  triggerIconHighlight
+                  triggerIconSize={18}
+                  className="m-0 xl:invisible group-hover:xl:visible"
+                />
+
+                <AgentBookmark
+                  onBookmarkClick={onBookmarkClick}
+                  entity={entity}
+                />
+              </>
             )}
-            <div className="flex whitespace-nowrap">
-              <div
-                className={classNames(
-                  'mr-6 flex shrink truncate text-base font-semibold leading-[20px] text-primary',
-                  !isMyApp && !entity.version && '!mr-12',
-                )}
+          </div>
+          <div className="flex items-center gap-4 overflow-hidden">
+            <div className="flex shrink-0 items-center justify-center xl:my-[3px]">
+              <ShareIcon
+                {...entity}
+                isHighlighted={false}
+                size={shareIconSize}
+                featureType={FeatureType.Application}
+                iconClassName="bg-layer-2 !stroke-[0.6] group-hover:bg-transparent !rounded-[4px]"
+                iconWrapperClassName="!rounded-[4px]"
               >
-                <span className="truncate" data-qa="agent-name">
-                  {entity.name}
-                </span>
-                <FunctionStatusIndicator entity={entity} />
-              </div>
+                <ModelIcon
+                  entityId={entity.id}
+                  entity={entity}
+                  size={iconSize}
+                />
+              </ShareIcon>
             </div>
-            <EntityMarkdownDescription className="hidden text-ellipsis text-sm leading-[18px] text-secondary xl:!line-clamp-2">
-              {getModelShortDescription(entity)}
-            </EntityMarkdownDescription>
+            <div className="flex grow flex-col justify-center gap-2 overflow-hidden">
+              {entity.version && (
+                <div
+                  className={classNames(
+                    'mr-6 flex gap-1 text-xs leading-[14px] text-secondary',
+                    !isMyApp && '!mr-12',
+                  )}
+                >
+                  {t('Version: ')}
+                  <span
+                    className="max-w-full overflow-hidden truncate whitespace-nowrap"
+                    data-qa="version"
+                  >
+                    {entity.version}
+                  </span>
+                </div>
+              )}
+              <div className="flex whitespace-nowrap">
+                <div
+                  className={classNames(
+                    'mr-6 flex shrink truncate text-base font-semibold leading-[20px] text-primary',
+                    !isMyApp && !entity.version && '!mr-12',
+                  )}
+                >
+                  <span className="truncate" data-qa="agent-name">
+                    {entity.name}
+                  </span>
+                  <FunctionStatusIndicator entity={entity} />
+                </div>
+              </div>
+              <EntityMarkdownDescription className="hidden text-ellipsis text-sm leading-[18px] text-secondary xl:!line-clamp-2">
+                {getModelShortDescription(entity)}
+              </EntityMarkdownDescription>
+            </div>
           </div>
         </div>
+        <CardFooter entity={entity} />
       </div>
-      <CardFooter entity={entity} />
-    </div>
-  );
-};
+    );
+  },
+);
+ApplicationCard.displayName = 'ApplicationCard';
