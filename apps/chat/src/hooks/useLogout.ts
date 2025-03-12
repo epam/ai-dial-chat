f9c97e33eb22696653eb268be1d0f752cabf07a8
Valery <valery_dluski@epam.com>
2025-03-12 12:55:37 +0100

    fix(chat): simplify logout logic by removing unnecessary parameters

--- apps/chat/src/hooks/useLogout.ts
+++ apps/chat/src/hooks/useLogout.ts
@@ -1,16 +1,12 @@
 import { signIn, useSession } from 'next-auth/react';
 import { useCallback } from 'react';
 
-import { customSignOut } from '@/src/utils/auth/signOut';
-
-import { Routes } from '@/src/constants/routes';
+import { customSignOut } from '../utils/auth/signOut';
 
 export const useLogout = () => {
   const { data: session } = useSession();
   const handleLogout = useCallback(() => {
-    session
-      ? customSignOut({ redirect: true, callbackUrl: Routes.Chat })
-      : signIn('azure-ad', { redirect: true });
+    session ? customSignOut() : signIn('azure-ad', { redirect: true });
   }, [session]);
   return {
     session,
