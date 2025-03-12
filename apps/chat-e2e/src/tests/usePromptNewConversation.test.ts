aea89a1daba904e057e759a736c32208622723f9
Maksim Nartov <nartovm1994@gmail.com>
2025-03-12 19:05:32 +0100

    EPMRTC-5487

--- apps/chat-e2e/src/tests/usePromptNewConversation.test.ts
+++ apps/chat-e2e/src/tests/usePromptNewConversation.test.ts
@@ -1,9 +1,14 @@
 import dialTest from '@/src/core/dialFixtures';
-import { ExpectedMessages, MenuOptions } from '@/src/testData';
+import {
+  ExpectedMessages,
+  MenuOptions,
+  MockedChatApiResponseBodies,
+} from '@/src/testData';
 import { GeneratorUtil } from '@/src/utils';
 
 dialTest.only(
-  'Use own prompt for new conversation',
+  'Use own prompt for new conversation\n' +
+    'Use own prompt for chat with history',
   async ({
     dialHomePage,
     header,
@@ -14,18 +19,20 @@ dialTest.only(
     promptData,
     dataInjector,
     sendMessage,
+    chat,
+    localStorageManager,
   }) => {
-    setTestIds('EPMRTC-5486');
-    // const promptContent = GeneratorUtil.randomString(20);
+    setTestIds('EPMRTC-5486', 'EPMRTC-5487');
     const prompt = promptData.prepareDefaultPrompt();
     await dataInjector.createPrompts([prompt]);
+    const initialMessage = GeneratorUtil.randomString(10);
+    const message = GeneratorUtil.randomString(10);
+    await localStorageManager.setShowSideBarPanels();
 
     await dialTest.step('Open Dial and create a new conversation', async () => {
       await dialHomePage.openHomePage();
       await dialHomePage.waitForPageLoaded();
     });
-
-    const initialMessage = GeneratorUtil.randomString(10);
 
     await dialTest.step('Type initial message', async () => {
       await sendMessage.messageInput.fillInInput(initialMessage);
@@ -41,5 +48,22 @@ dialTest.only(
         `${initialMessage} ${prompt.content}`,
       );
     });
+
+    await dialTest.step(
+      'Type message, get response, type data, and use prompt from context menu',
+      async () => {
+        await sendMessage.clearMessageInput();
+        await dialHomePage.mockChatTextResponse(
+          MockedChatApiResponseBodies.simpleTextBody,
+        );
+        await chat.sendRequestWithButton(message);
+        await sendMessage.messageInput.fillInInput(initialMessage);
+        await prompts.openEntityDropdownMenu(prompt.name);
+        await promptDropdownMenu.selectMenuOption(MenuOptions.use);
+        await sendMessageAssertion.assertMessageValue(
+          `${initialMessage} ${prompt.content}`,
+        );
+      },
+    );
   },
 );
