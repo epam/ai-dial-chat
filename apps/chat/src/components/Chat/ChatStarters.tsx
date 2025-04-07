07e61d50cefea39b4ae9236ed389decc1a31584e
Aliaksandr Kezik <a.kezzk@gmail.com>
2025-04-07 12:37:10 +0200

    fix(chat): fix starters shrinking

--- apps/chat/src/components/Chat/ChatStarters.tsx
+++ apps/chat/src/components/Chat/ChatStarters.tsx
@@ -59,7 +59,7 @@ const ChatStartersView = ({ schema }: ChatStartersViewProps) => {
       buttonClassName="shrink-0"
       wrapperClassName="lg:items-center"
       propertyWrapperClassName={
-        isChatFullWidth ? 'mx-4 md:mx-20' : 'max-w-screen-md'
+        isChatFullWidth ? 'mx-4 md:mx-20 shrink-0' : 'max-w-screen-md'
       }
     />
   );
