4e8aa9fc526280042dada74fbe83f2f505076c7c
Irina_Kartun <irina.kartun@gmail.com>
2025-04-22 17:06:15 +0200

    feat/bookmark-agents-tests: implemented tests for agents bookmarks

--- apps/chat-e2e/src/assertions/confirmationDialogAssertion.ts
+++ apps/chat-e2e/src/assertions/confirmationDialogAssertion.ts
@@ -1,29 +1,28 @@
+import { BaseAssertion } from '@/src/assertions/base/baseAssertion';
 import { ExpectedMessages } from '@/src/testData';
 import { ConfirmationDialog } from '@/src/ui/webElements';
-import { expect } from '@playwright/test';
 
-export class ConfirmationDialogAssertion {
+export class ConfirmationDialogAssertion extends BaseAssertion {
   readonly confirmationDialog: ConfirmationDialog;
 
   constructor(confirmationDialog: ConfirmationDialog) {
+    super();
     this.confirmationDialog = confirmationDialog;
   }
 
   public async assertConfirmationDialogTitle(expectedTitle: string) {
-    expect
-      .soft(
-        await this.confirmationDialog.entityName.getElementInnerContent(),
-        ExpectedMessages.modalDialogTitleIsValid,
-      )
-      .toBe(expectedTitle);
+    await this.assertElementText(
+      this.confirmationDialog.entityName,
+      expectedTitle,
+      ExpectedMessages.modalDialogTitleIsValid,
+    );
   }
 
   public async assertConfirmationMessage(expectedMessage: string) {
-    expect
-      .soft(
-        await this.confirmationDialog.getConfirmationMessage(),
-        ExpectedMessages.confirmationMessageIsValid,
-      )
-      .toBe(expectedMessage);
+    await this.assertElementText(
+      this.confirmationDialog.confirmMessage,
+      expectedMessage,
+      ExpectedMessages.confirmationMessageIsValid,
+    );
   }
 }
