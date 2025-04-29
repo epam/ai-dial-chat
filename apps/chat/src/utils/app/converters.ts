91d02fe26df81bb581e12def057967b5e6d002bf
Aliaksandr Kezik <a.kezzk@gmail.com>
2025-04-29 14:12:59 +0200

    not handle begin-end cases

--- apps/chat/src/utils/app/converters.ts
+++ apps/chat/src/utils/app/converters.ts
@@ -1,18 +1,7 @@
 export const convertLaTeXToMarkdownMath = (content: string) => {
-  let transformedContent = content.replace(
+  const transformedContent = content.replace(
     /\\\[([\s\S]*?)\\\]/g,
     (_, mathContent: string) => `$$\n${mathContent.trim()}\n$$`,
-  );
-
-  transformedContent = transformedContent.replace(
-    /\\begin{(equation\*?|align\*?|gather\*?|multline\*?|displaymath)}([\s\S]*?)\\end{\1}/g,
-    (_, envName: string, inner: string) => {
-      if (/[\^_=+\-*/\\]/.test(inner)) {
-        return `$$\n${inner.trim()}\n$$`;
-      }
-
-      return `\\begin{${envName}}${inner}\\end{${envName}}`;
-    },
   );
 
   return transformedContent.replace(
