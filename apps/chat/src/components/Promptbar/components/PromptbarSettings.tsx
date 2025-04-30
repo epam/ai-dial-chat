6820773a90d668425e93c85726fc02c7587af380
Derikyan <armen.derikyan@gmail.com>
2025-04-30 14:31:27 +0400

    fix

--- apps/chat/src/components/Promptbar/components/PromptbarSettings.tsx
+++ apps/chat/src/components/Promptbar/components/PromptbarSettings.tsx
@@ -137,7 +137,6 @@ export function PromptbarSettings() {
 
       <ConfirmDialog
         isOpen={isClearModalOpen}
-        hasHeadingTooltip
         heading={t(`Confirm deleting ${deleteTerm} prompts`)}
         description={t(
           `Are you sure that you want to delete ${deleteTerm} prompts?`,
