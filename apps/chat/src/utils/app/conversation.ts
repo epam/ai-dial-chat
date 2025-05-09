c40154b0eef4fdeab4d27134a888a71896d704b9
Magomed-Elbi Dzhukalaev <magomed-elbi_dzhukalaev@epam.com>
2025-05-09 19:03:16 +0300

    fix(chat): update quick attachments save path to fit yyyy-m format

--- apps/chat/src/utils/app/conversation.ts
+++ apps/chat/src/utils/app/conversation.ts
@@ -361,5 +361,7 @@ export const isReplayAsIsConversation = (conversation: ConversationInfo) =>
   (conversation as Conversation).replay?.replayAsIs ?? false;
 
 export const getQuickAttachmentsSavingPath = () => {
-  return `${getFileRootId()}/temp`;
+  const date = new Date();
+
+  return `${getFileRootId()}/${date.getFullYear()}-${date.getMonth() + 1}`;
 };
