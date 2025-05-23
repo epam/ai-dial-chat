1e8965b8a941e563d626c1076c7ac4e6653d3d6c
Mikita Butsko <mikita_butsko@epam.com>
2025-05-23 14:16:23 +0200

    fix: return back env bucket

--- apps/overlay-sandbox/app/cases/overlay/new-conversations-folder-id-set-sandbox/page.tsx
+++ apps/overlay-sandbox/app/cases/overlay/new-conversations-folder-id-set-sandbox/page.tsx
@@ -8,8 +8,7 @@ import {
 import { Feature } from '@epam/ai-dial-shared';
 
 // Use your bucket for testing
-const bucket =
-  '3s5mL6jaqKuWBohoam9sRw7jeqixBDW5QSQezNrvhvu36fKFb8FkRVrpyqx8sQzaSj';
+const bucket = process.env.NEXT_PUBLIC_OVERLAY_USER_BUCKET;
 const overlayOptions = {
   ...commonOverlayProps,
   newConversationsFolderId: `conversations/${bucket}/test-folder`,
