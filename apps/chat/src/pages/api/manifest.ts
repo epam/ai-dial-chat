af432ce23c91e2ee47432085dc8c260283cb941d
Ilya Bondar <ilya_bondar@epam.com>
2025-05-29 17:09:08 +0200

    Dial -> DIAL

--- apps/chat/src/pages/api/manifest.ts
+++ apps/chat/src/pages/api/manifest.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { getThemeIconUrl } from '@/src/utils/app/themes';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const appName = process.env.NEXT_PUBLIC_APP_NAME ?? 'AI Dial';
+  const appName = process.env.NEXT_PUBLIC_APP_NAME ?? 'AI DIAL';
   const iconUrl = getThemeIconUrl('favicon');
   const logoUrl = getThemeIconUrl('dark-logo');
   res.setHeader('Content-Type', 'application/json');
