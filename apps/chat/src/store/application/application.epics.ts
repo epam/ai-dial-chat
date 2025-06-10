bccf31d78458217286351162c9cce8d2791b3040
Magomed-Elbi Dzhukalaev <magomed-elbi_dzhukalaev@epam.com>
2025-06-10 16:12:00 +0300

    fix(chat): fix id regenerate

--- apps/chat/src/store/application/application.epics.ts
+++ apps/chat/src/store/application/application.epics.ts
@@ -34,11 +34,7 @@ import { cleanSchemaId } from '@/src/utils/app/application-type-schema';
 import { ApplicationService } from '@/src/utils/app/data/application-service';
 import { DataService } from '@/src/utils/app/data/data-service';
 import { BrowserStorage } from '@/src/utils/app/data/storages/browser-storage';
-import {
-  getEntityBucket,
-  isEntityIdExternal,
-  isEntityIdLocal,
-} from '@/src/utils/app/id';
+import { isEntityIdExternal, isEntityIdLocal } from '@/src/utils/app/id';
 import { translate } from '@/src/utils/app/translation';
 import { parseApplicationApiKey } from '@/src/utils/server/api';
 
@@ -214,9 +210,6 @@ const updateApplicationEpic: AppEpic = (action$, state$) =>
 
       const updatedCustomApplication = regenerateApplicationId(
         payload.applicationData,
-        payload.isApplicationOnReview
-          ? getEntityBucket(payload.applicationData)
-          : undefined,
       ) as CustomApplicationModel;
 
       const isMoved = payload.oldApplication.id !== updatedCustomApplication.id;
