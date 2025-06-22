4c0fd22f58ec453601cb1f62a5de3c2c2473d386
Magomed-Elbi Dzhukalaev <magomed-elbi_dzhukalaev@epam.com>
2025-06-22 15:51:51 +0300

    fix(chat): hide edit button for unpublishing conversations

--- apps/chat/src/components/Chat/Publish/PublicationControls/ChatPublicationControls.tsx
+++ apps/chat/src/components/Chat/Publish/PublicationControls/ChatPublicationControls.tsx
@@ -1,11 +1,15 @@
 import { IconPencilMinus } from '@tabler/icons-react';
+import { useMemo } from 'react';
+
+import { useAppSelector } from '@/src/store/hooks';
+import { PublicationSelectors } from '@/src/store/publication/publication.selectors';
 
 import { IconButton } from '@/src/components/Common/IconButton';
 import { ScrollDownButton } from '@/src/components/Common/ScrollDownButton';
 
 import { PublicationControls } from './PublicationControls';
 
-import { ConversationInfo } from '@epam/ai-dial-shared';
+import { ConversationInfo, PublishActions } from '@epam/ai-dial-shared';
 
 interface Props {
   entity: ConversationInfo;
@@ -20,17 +24,37 @@ export const ChatPublicationControls = ({
   onScrollDownClick,
   onToggleInput,
 }: Props) => {
+  const resourceToReview = useAppSelector((state) =>
+    PublicationSelectors.selectResourceToReviewByReviewUrl(state, entity.id),
+  );
+  const publication = useAppSelector((state) =>
+    PublicationSelectors.selectPublicationByUrl(
+      state,
+      resourceToReview?.publicationUrl ?? '',
+    ),
+  );
+
+  const isUnpublishing = useMemo(() => {
+    const action = publication?.resources?.find(
+      ({ reviewUrl }) => resourceToReview?.reviewUrl === reviewUrl,
+    )?.action;
+
+    return action === PublishActions.DELETE;
+  }, [publication, resourceToReview]);
+
   return (
     <PublicationControls
       controlsClassNames="mx-2 mb-2 mt-5 flex-row md:mx-4 md:mb-0 md:last:mb-6 lg:mx-auto lg:w-[768px] lg:max-w-3xl"
       entity={entity}
     >
-      <IconButton
-        Icon={IconPencilMinus}
-        name="Edit"
-        dataQa="edit-chat"
-        onClick={onToggleInput}
-      />
+      {!isUnpublishing && (
+        <IconButton
+          Icon={IconPencilMinus}
+          name="Edit"
+          dataQa="edit-chat"
+          onClick={onToggleInput}
+        />
+      )}
       {showScrollDownButton && (
         <ScrollDownButton
           className="-top-16 right-0 md:-top-20"
