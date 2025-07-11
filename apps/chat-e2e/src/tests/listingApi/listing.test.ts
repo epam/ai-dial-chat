c23fc120943cfcdcee892ab2dec9363c14812e2e
Maksim Nartov <81012703+nartovm@users.noreply.github.com>
2025-07-11 12:10:31 +0200

    format

--- apps/chat-e2e/src/tests/listingApi/listing.test.ts
+++ apps/chat-e2e/src/tests/listingApi/listing.test.ts
@@ -21,7 +21,10 @@ test('Models API listing', async () => {
   expectedModels.forEach((model) => {
     const actualModel = ModelsUtil.getModel(model.entityId);
     expect
-      .soft(actualModel, `${model.entityId}: ${ExpectedMessages.modelIsAvailable}`)
+      .soft(
+        actualModel,
+        `${model.entityId}: ${ExpectedMessages.modelIsAvailable}`,
+      )
       .toBeDefined();
   });
 });
