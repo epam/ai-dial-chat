7ed621061972188b282b2a520452e056c77695f8
Magomed-Elbi Dzhukalaev <magomed-elbi_dzhukalaev@epam.com>
2025-07-22 22:06:40 +0300

    feat(chat): add hidden folders toggler to change path dialog

--- apps/chat/src/components/Common/SelectFolder/SelectFolderFooter.tsx
+++ apps/chat/src/components/Common/SelectFolder/SelectFolderFooter.tsx
@@ -1,12 +1,18 @@
+import { IconEye, IconEyeOff } from '@tabler/icons-react';
+
 import { useTranslation } from '@/src/hooks/useTranslation';
 
 import { Translation } from '@/src/types/translation';
+
+import { Tooltip } from '@/src/components/Common/Tooltip';
 
 import FolderPlus from '@/public/images/icons/folder-plus.svg';
 
 interface Props {
   onCreateNewFolder: () => void;
   onSelectFolderClick: () => void;
+  onToggleHiddenFolders?: () => void;
+  areHiddenFoldersVisible?: boolean;
   disableSelect?: boolean;
   selectBtnText?: string;
 }
@@ -14,6 +20,8 @@ interface Props {
 export const SelectFolderFooter = ({
   onCreateNewFolder,
   onSelectFolderClick,
+  onToggleHiddenFolders,
+  areHiddenFoldersVisible = false,
   disableSelect,
   selectBtnText = 'Select folder',
 }: Props) => {
@@ -29,6 +37,29 @@ export const SelectFolderFooter = ({
         >
           <FolderPlus height={24} width={24} />
         </button>
+
+        {!!onToggleHiddenFolders && (
+          <button
+            onClick={onToggleHiddenFolders}
+            className="flex size-[34px] items-center justify-center rounded text-secondary hover:bg-accent-primary-alpha hover:text-accent-primary"
+            data-qa="new-folder"
+          >
+            <Tooltip
+              tooltip={
+                areHiddenFoldersVisible
+                  ? t('Hide technical items')
+                  : t('Show technical items')
+              }
+              isTriggerClickable
+            >
+              {areHiddenFoldersVisible ? (
+                <IconEyeOff height={24} width={24} />
+              ) : (
+                <IconEye height={24} width={24} />
+              )}
+            </Tooltip>
+          </button>
+        )}
       </div>
       <div>
         <button
