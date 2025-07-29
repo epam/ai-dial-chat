6d4d7f61a3a412bcc5b0edf84efde17a0618c42c
Irina_Kartun <irina.kartun@gmail.com>
2025-07-29 10:34:10 +0200

    feat/publish-custom-app: updated allure dependency

--- apps/chat-e2e/src/assertions/publishing/publishingApprovalModalAssertion.ts
+++ apps/chat-e2e/src/assertions/publishing/publishingApprovalModalAssertion.ts
@@ -121,10 +121,15 @@ export class PublishingApprovalModalAssertion extends BaseAssertion {
         this.publishingApprovalModal.authorLabel,
         fieldsToVerify.authorLabel,
       );
-      await this.assertElementText(
-        this.publishingApprovalModal.authorLabel,
-        new RegExp(`^${ExpectedConstants.authorLabel}(: )?$`),
-      );
+      fieldsToVerify.authorLabel === 'visible'
+        ? await this.assertElementText(
+            this.publishingApprovalModal.authorLabel,
+            ExpectedConstants.authorLabel,
+          )
+        : await this.assertElementState(
+            this.publishingApprovalModal.author,
+            'hidden',
+          );
     }
     if (fieldsToVerify.author) {
       await this.assertElementText(
@@ -137,10 +142,15 @@ export class PublishingApprovalModalAssertion extends BaseAssertion {
         this.publishingApprovalModal.publicAuthorLabel,
         fieldsToVerify.publicAuthorLabel,
       );
-      await this.assertElementText(
-        this.publishingApprovalModal.publicAuthorLabel,
-        ExpectedConstants.publicAuthorLabel,
-      );
+      fieldsToVerify.publicAuthorLabel === 'visible'
+        ? await this.assertElementText(
+            this.publishingApprovalModal.publicAuthorLabel,
+            ExpectedConstants.publicAuthorLabel,
+          )
+        : await this.assertElementState(
+            this.publishingApprovalModal.publicAuthorLabel,
+            'hidden',
+          );
     }
     if (fieldsToVerify.publicAuthor) {
       await this.assertElementText(
@@ -148,7 +158,6 @@ export class PublishingApprovalModalAssertion extends BaseAssertion {
         fieldsToVerify.publicAuthor,
       );
     }
-
     if (fieldsToVerify.requestCreatedLabel) {
       await this.assertElementState(
         this.publishingApprovalModal.requestCreatedLabel,
