33192186bdfaec5ac96e48548678f8953e40e12f
Irina_Kartun <irina.kartun@gmail.com>
2025-08-12 13:42:02 +0200

    feat/unpublish-custom-app: intro var for timeout

--- apps/chat-e2e/src/ui/webElements/marketplace/marketplaceAgentsSection.ts
+++ apps/chat-e2e/src/ui/webElements/marketplace/marketplaceAgentsSection.ts
@@ -10,6 +10,8 @@ import {
 } from '@/src/ui/webElements';
 import { Locator, Page } from '@playwright/test';
 
+export const marketplaceContentDisplayTimeout = 200;
+
 export class MarketplaceAgentsSection extends BaseElement {
   constructor(page: Page, parentLocator: Locator) {
     super(page, MarketplaceSelectors.marketplaceAgentSection, parentLocator);
@@ -160,7 +162,7 @@ export class MarketplaceAgentsSection extends BaseElement {
     }
     //wait for available cards are displayed
     // eslint-disable-next-line playwright/no-wait-for-timeout
-    await this.page.waitForTimeout(200);
+    await this.page.waitForTimeout(marketplaceContentDisplayTimeout);
     await this.moveToAgentsSection();
     let scrollPosition: { scrollTop: number; clientHeight: number } = {
       scrollTop: 0,
@@ -264,7 +266,7 @@ export class MarketplaceAgentsSection extends BaseElement {
     const rowHeight = lastRowBounding!.height;
     //need to wait the scrolling is finished
     // eslint-disable-next-line playwright/no-wait-for-timeout
-    await this.page.waitForTimeout(200);
+    await this.page.waitForTimeout(marketplaceContentDisplayTimeout);
     return rowHeight;
   }
 
