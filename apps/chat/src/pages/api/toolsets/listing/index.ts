2e710812f6286c992a911bab1a79b8de55c08c21
Magomed-Elbi_Dzhukalaev <mukhammad.amaev@mail.ru>
2025-08-13 16:40:36 +0300

    fix(chat): refactor api handlers

--- apps/chat/src/pages/api/toolsets/listing/index.ts
+++ apps/chat/src/pages/api/toolsets/listing/index.ts
@@ -1,67 +1,8 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { getServerSession } from 'next-auth';
-import { getToken } from 'next-auth/jwt';
+import { createApiHandler } from '@/src/utils/server/api-handler';
 
-import { constructPath } from '@/src/utils/app/file';
-import { validateServerSession } from '@/src/utils/auth/session';
-import { getApiHeaders } from '@/src/utils/server/get-headers';
-import { logger } from '@/src/utils/server/logger';
+import { HTTPMethod } from '@/src/types/http';
 
-import { DialAIError } from '@/src/types/error';
-
-import { errorsMessages } from '@/src/constants/errors';
-
-import { authOptions } from '@/src/pages/api/auth/[...nextauth]';
-
-import { sanitizeUri } from 'micromark-util-sanitize-uri';
-import fetch from 'node-fetch';
-
-const getUrl = (dialApiHost: string): string => {
-  return constructPath(dialApiHost, 'openai', 'toolsets');
-};
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getServerSession(req, res, authOptions);
-  const isSessionValid = validateServerSession(session, req, res);
-
-  if (!isSessionValid) {
-    return;
-  }
-
-  const token = await getToken({ req });
-
-  const url = getUrl(process.env.DIAL_API_HOST);
-
-  try {
-    const proxyRes = await fetch(sanitizeUri(url), {
-      headers: getApiHeaders({ jwt: token?.access_token as string }),
-    });
-
-    let json: unknown;
-    if (!proxyRes.ok) {
-      try {
-        json = await proxyRes.json();
-      } catch (err) {
-        json = undefined;
-      }
-
-      throw new DialAIError(
-        (typeof json === 'string' && json) || proxyRes.statusText,
-        proxyRes.status,
-        req,
-      );
-    }
-    json = await proxyRes.json();
-    return res.status(200).send(json);
-  } catch (error: unknown) {
-    logger.error(error);
-    if (error instanceof DialAIError) {
-      return res
-        .status(parseInt(error.code, 10) || 500)
-        .send(error.message || errorsMessages.generalServer);
-    }
-    return res.status(500).send(errorsMessages.generalServer);
-  }
-};
-
-export default handler;
+export default createApiHandler({
+  endpoint: '/openai/toolsets',
+  method: HTTPMethod.GET,
+});
