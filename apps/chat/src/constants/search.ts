bbf6b4deab4594c92ff7dbeb38cb7a02420165dc
Denys Kolomiitsev <denys_kolomiitsev@epam.com>
2025-08-21 13:09:00 +0200

    fix: resolve comments

--- apps/chat/src/constants/search.ts
+++ apps/chat/src/constants/search.ts
@@ -1,28 +1,9 @@
+import { getEntitySearchOptions } from '@/src/utils/app/search';
+
 import { DialAIEntityModel } from '@/src/types/models';
 import { ToolsetModel } from '@/src/types/toolsets';
 
-import { IFuseOptions } from 'fuse.js';
+export const MODELS_SEARCH_OPTIONS =
+  getEntitySearchOptions<DialAIEntityModel>();
 
-export const MODELS_SEARCH_OPTIONS: IFuseOptions<DialAIEntityModel> = {
-  keys: ['name', 'version'],
-  threshold: 0.2,
-  distance: 100,
-  minMatchCharLength: 1,
-  ignoreLocation: true,
-  useExtendedSearch: false,
-  findAllMatches: false,
-  isCaseSensitive: false,
-  includeScore: false,
-};
-
-export const TOOLSETS_SEARCH_OPTIONS: IFuseOptions<ToolsetModel> = {
-  keys: ['name', 'version'],
-  threshold: 0.2,
-  distance: 100,
-  minMatchCharLength: 1,
-  ignoreLocation: true,
-  useExtendedSearch: false,
-  findAllMatches: false,
-  isCaseSensitive: false,
-  includeScore: false,
-};
+export const TOOLSETS_SEARCH_OPTIONS = getEntitySearchOptions<ToolsetModel>();
--- apps/chat/src/store/toolset/toolset.types.ts
+++ apps/chat/src/store/toolset/toolset.types.ts
@@ -11,5 +11,5 @@ export interface ToolsetState {
   toolsetDetailsStatus: UploadStatus;
 
   bookmarkedToolsets: string[];
-  isBookmarkedToolsetInitialized: boolean;
+  isBookmarkedToolsetsInitialized: boolean;
 }
