48e14c4d0bd3f2aeeaf58b98fc28e078d813f79e
irinakartun <irina.kartun@gmail.com>
2025-09-02 19:31:44 +0200

    feat(chat-e2e): conversation publishing rules tests (#4494)
    Co-authored-by: Ilya Bondar <ilya_bondar@epam.com>

---
+++ apps/chat-e2e/src/assertions/publishing/publishingRulesAssertion.ts
@@ -0,0 +1,168 @@
+import { PublicationFunctions } from '@/chat/types/publication';
+import { BaseAssertion } from '@/src/assertions';
+import {
+  BooleanOperator,
+  ElementState,
+  ExpectedConstants,
+  PublishingRulesFilterTarget,
+} from '@/src/testData';
+import { PublishingRules } from '@/src/ui/webElements';
+
+export class PublishingRulesAssertion extends BaseAssertion {
+  readonly publishingRules: PublishingRules;
+
+  constructor(publishingRules: PublishingRules) {
+    super();
+    this.publishingRules = publishingRules;
+  }
+
+  public async assertLabels(labelsToVerify: {
+    publishPath?: string;
+    allowAccessLabel?: ElementState;
+    availabilityLabel?: ElementState;
+    noChangesLabel?: ElementState;
+    seeChangesButton?: ElementState;
+  }) {
+    if (labelsToVerify.publishPath) {
+      await this.assertElementText(
+        this.publishingRules.publishingPath,
+        labelsToVerify.publishPath,
+      );
+    }
+    if (labelsToVerify.allowAccessLabel) {
+      await this.assertElementState(
+        this.publishingRules.allowAccessLabel,
+        labelsToVerify.allowAccessLabel,
+      );
+      if (labelsToVerify.allowAccessLabel === 'visible') {
+        await this.assertElementText(
+          this.publishingRules.allowAccessLabel,
+          ExpectedConstants.allowAccessLabel,
+        );
+      }
+    }
+    if (labelsToVerify.availabilityLabel) {
+      await this.assertElementState(
+        this.publishingRules.availabilityLabel,
+        labelsToVerify.availabilityLabel,
+      );
+      if (labelsToVerify.availabilityLabel === 'visible') {
+        await this.assertElementText(
+          this.publishingRules.availabilityLabel,
+          ExpectedConstants.availabilityLabel,
+        );
+      }
+    }
+    if (labelsToVerify.seeChangesButton) {
+      await this.assertElementState(
+        this.publishingRules.seeChangesButton,
+        labelsToVerify.seeChangesButton,
+      );
+      if (labelsToVerify.seeChangesButton === 'visible') {
+        await this.assertElementText(
+          this.publishingRules.seeChangesButton,
+          ExpectedConstants.seeChangesLabel,
+        );
+      }
+    }
+    if (labelsToVerify.noChangesLabel) {
+      await this.assertElementState(
+        this.publishingRules.noChangesLabel,
+        labelsToVerify.noChangesLabel,
+      );
+      if (labelsToVerify.noChangesLabel === 'visible') {
+        await this.assertElementText(
+          this.publishingRules.noChangesLabel,
+          ExpectedConstants.noChangesLabel,
+        );
+      }
+    }
+  }
+
+  public async assertFilterFields(fieldsToVerify: {
+    filterTargetState?: ElementState;
+    filterTargetValue?: PublishingRulesFilterTarget | string;
+    filterFunctionState?: ElementState;
+    filterFunctionValue?: PublicationFunctions | string;
+    filterValues?: string[];
+    saveButtonState?: ElementState;
+    cancelButtonState?: ElementState;
+  }) {
+    const publishingFilter = this.publishingRules.gePublishingFilter();
+    if (fieldsToVerify.filterTargetState) {
+      await this.assertElementState(
+        publishingFilter.filterTarget,
+        fieldsToVerify.filterTargetState,
+      );
+    }
+    if (fieldsToVerify.filterTargetValue) {
+      await this.assertElementText(
+        publishingFilter.filterTarget,
+        fieldsToVerify.filterTargetValue,
+      );
+    }
+    if (fieldsToVerify.filterFunctionState) {
+      await this.assertElementState(
+        publishingFilter.filterFunction,
+        fieldsToVerify.filterFunctionState,
+      );
+    }
+    if (fieldsToVerify.filterFunctionValue) {
+      await this.assertElementText(
+        publishingFilter.filterFunction,
+        fieldsToVerify.filterFunctionValue,
+      );
+    }
+    if (fieldsToVerify.filterValues) {
+      await this.assertElementInnerText(
+        publishingFilter.filterPills,
+        fieldsToVerify.filterValues,
+      );
+    }
+    if (fieldsToVerify.saveButtonState) {
+      await this.assertElementState(
+        publishingFilter.saveFilterButton,
+        fieldsToVerify.saveButtonState,
+      );
+    }
+    if (fieldsToVerify.cancelButtonState) {
+      await this.assertElementState(
+        publishingFilter.cancelFilterButton,
+        fieldsToVerify.cancelButtonState,
+      );
+    }
+  }
+
+  public async assertRule(
+    rule: {
+      target: PublishingRulesFilterTarget;
+      fnc: PublicationFunctions;
+      values: string[];
+    },
+    expectedState: ElementState,
+    expectedCancelButtonState: ElementState,
+    expectedOperator?: BooleanOperator,
+  ) {
+    await this.assertElementState(
+      this.publishingRules.rule(rule),
+      expectedState,
+    );
+    if (expectedOperator) {
+      await this.assertElementText(
+        this.publishingRules.ruleOperator(rule),
+        expectedOperator,
+      );
+    } else {
+      await this.assertElementState(
+        this.publishingRules.ruleOperator(rule),
+        'hidden',
+      );
+    }
+    if (expectedCancelButtonState) {
+      await this.assertElementState(
+        this.publishingRules.cancelRuleButton(rule),
+        expectedCancelButtonState,
+      );
+    }
+  }
+}
