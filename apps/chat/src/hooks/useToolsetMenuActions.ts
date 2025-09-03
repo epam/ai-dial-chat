9c039e6d9182e213b96b2cbae4287480b2751bd8
Alexander <98586297+Alexander-Kezik@users.noreply.github.com>
2025-09-03 16:23:19 +0200

    fix(chat): restrict creation of toolset with id that already exists and remove unsupported toolset actions (Issues #4561, #4567) (#4598)

--- apps/chat/src/hooks/useToolsetMenuActions.ts
+++ apps/chat/src/hooks/useToolsetMenuActions.ts
@@ -2,7 +2,7 @@ import { useCallback } from 'react';
 
 import { useRouter } from 'next/router';
 
-import { ToolsetModel } from '@/src/types/toolsets';
+import { ToolsetEditorSteps, ToolsetModel } from '@/src/types/toolsets';
 
 import { MarketplaceActions, ToolsetActions } from '@/src/store/actions';
 import { useAppDispatch } from '@/src/store/hooks';
@@ -49,6 +49,7 @@ export const useToolsetMenuActions = (toolset: ToolsetModel) => {
     (e: React.MouseEvent) => {
       e.preventDefault();
       e.stopPropagation();
+      dispatch(ToolsetActions.setEditorStep(ToolsetEditorSteps.Settings));
       dispatch(ToolsetActions.setToolsetDetails());
       void router.push({
         pathname: Routes.ToolsetEditor,
