314ab0e805a796c04f25a7c4f11131a86a9fea4e
Alexander <98586297+Alexander-Kezik@users.noreply.github.com>
2025-09-08 21:48:12 +0200

    fix(chat): add review QuickApp 2 version window and fix redirect to publication for applications with custom editor (Issues #4608, #4609) (#4623)

--- apps/chat/src/components/AppsEditor/Settings/QuickAppView2/index.tsx
+++ apps/chat/src/components/AppsEditor/Settings/QuickAppView2/index.tsx
@@ -1,8 +1,6 @@
 import { useCallback, useEffect, useMemo, useRef } from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
 
-import { useRouter } from 'next/router';
-
 import { useBeforeRedirect } from '@/src/hooks/useBeforeRedirect';
 import { useTranslation } from '@/src/hooks/useTranslation';
 
@@ -113,8 +111,6 @@ export const QuickAppView2: React.FC<QuickAppView2Props> = ({
     ? CONFIRM_DOCUMENT_VALUES
     : undefined;
 
-  const router = useRouter();
-
   const handleSubmit = useCallback(
     (data: QuickAppFormData2) => {
       const hasChanged = !isEqual(data, lastSubmittedValuesRef.current);
@@ -211,7 +207,6 @@ export const QuickAppView2: React.FC<QuickAppView2Props> = ({
     exitAfterSave,
     isFormValid,
     shouldSaveApplication,
-    router,
     t,
   ]);
 
