1ab9232385ab5a01d2641fabc4ed60654e6f8582
Denys Kolomiitsev <denys_kolomiitsev@epam.com>
2025-09-11 12:45:45 +0200

    fix: add _next/data to the exception

--- apps/chat/src/middleware.ts
+++ apps/chat/src/middleware.ts
@@ -48,17 +48,17 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  // matcher: '/(.*)',
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
      * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
+     * - _next/data (translation files)
      * - favicon.ico (favicon file)
      */
     {
-      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      source: '/((?!api|_next/static|_next/image|_next/data|favicon.ico).*)',
       missing: [
         { type: 'header', key: 'next-router-prefetch' },
         { type: 'header', key: 'purpose', value: 'prefetch' },
