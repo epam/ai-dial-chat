a60d18e20d9eaf0f3bdf2935e8a4a9d4d2a33cf5
Magomed-Elbi_Dzhukalaev <mukhammad.amaev@mail.ru>
2025-09-18 16:59:35 +0300

    fix(chat): hide form on without login, add password input

--- apps/chat/src/components/Common/Forms/Field.tsx
+++ apps/chat/src/components/Common/Forms/Field.tsx
@@ -1,4 +1,11 @@
-import { InputHTMLAttributes, ReactNode, forwardRef } from 'react';
+import { IconEye, IconEyeOff } from '@tabler/icons-react';
+import {
+  InputHTMLAttributes,
+  ReactNode,
+  forwardRef,
+  useCallback,
+  useState,
+} from 'react';
 
 import classNames from 'classnames';
 
@@ -13,19 +20,45 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ error, className, tooltip, dataQa, ...rest }, ref) => {
+  ({ error, className, tooltip, dataQa, type, ...rest }, ref) => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const inputType =
+      type === 'password' ? (isVisible ? 'text' : 'password') : type;
+
+    const handleTogglePassword = useCallback(() => {
+      setIsVisible((prev) => !prev);
+    }, []);
+
     return (
       <Tooltip tooltip={tooltip}>
-        <input
-          {...rest}
-          ref={ref}
-          className={classNames(
-            'input-form input-invalid peer mx-0 disabled:cursor-not-allowed disabled:border-primary',
-            error && 'border-error hover:border-error focus:border-error',
-            className,
+        <div className="relative">
+          <input
+            {...rest}
+            ref={ref}
+            className={classNames(
+              'input-form input-invalid peer mx-0 disabled:cursor-not-allowed disabled:border-primary',
+              error && 'border-error hover:border-error focus:border-error',
+              type === 'password' && 'pr-9',
+              className,
+            )}
+            data-qa={dataQa}
+            type={inputType}
+          />
+
+          {type === 'password' && (
+            <button
+              className="button absolute right-0 top-1/2 -translate-y-1/2"
+              onClick={handleTogglePassword}
+            >
+              {isVisible ? (
+                <IconEye size={18} className="text-secondary" />
+              ) : (
+                <IconEyeOff size={18} className="text-secondary" />
+              )}
+            </button>
           )}
-          data-qa={dataQa}
-        />
+        </div>
       </Tooltip>
     );
   },
