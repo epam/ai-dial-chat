c63ed22cc23c8af4fcdc1b6e85ec41b3c7c2f602
Irina_Kartun <irina.kartun@gmail.com>
2025-09-23 17:38:06 +0200

    feat/conversation-header-dots-menu-tests: implemented more tests

--- apps/chat-e2e/src/ui/webElements/dropdownMenu.ts
+++ apps/chat-e2e/src/ui/webElements/dropdownMenu.ts
@@ -2,11 +2,9 @@ import {
   ShareByLinkResponseModel,
   ShareRequestModel,
 } from '@/chat/types/share';
-import { isApiStorageType } from '@/src/hooks/global-setup';
 import { API, MenuOptions } from '@/src/testData';
 import { Attributes, Tags } from '@/src/ui/domData';
 import { Menu } from '@/src/ui/webElements/menu';
-import { Response } from 'playwright-core';
 
 export class DropdownMenu extends Menu {
   public menuOptions = () =>
@@ -19,33 +17,6 @@ export class DropdownMenu extends Menu {
 
   public getMenuOption(option: string) {
     return this.createElementFromLocator(this.menuOption(option));
-  }
-
-  public async selectMenuOption(
-    option: string,
-    {
-      triggeredHttpMethod = undefined,
-      isHttpMethodTriggered = true,
-      apiHost = undefined,
-    }: {
-      triggeredHttpMethod?: 'PUT' | 'POST' | 'DELETE' | 'GET';
-      isHttpMethodTriggered?: boolean;
-      apiHost?: string;
-    } = {},
-  ) {
-    if (isApiStorageType && isHttpMethodTriggered && triggeredHttpMethod) {
-      const predicate = (resp: Response) =>
-        apiHost !== undefined
-          ? resp.request().method() === triggeredHttpMethod &&
-            resp.status() === 200 &&
-            resp.url().includes(apiHost)
-          : resp.request().method() === triggeredHttpMethod &&
-            resp.status() === 200;
-      const respPromise = this.page.waitForResponse(predicate);
-      await super.selectMenuOption(option);
-      return respPromise;
-    }
-    await super.selectMenuOption(option);
   }
 
   public async selectShareMenuOption() {
