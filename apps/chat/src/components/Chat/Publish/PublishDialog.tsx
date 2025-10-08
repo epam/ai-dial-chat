d283d7c1ce505a07eebc319b85956c890270cc79
Aliaksandr Kezik <a.kezzk@gmail.com>
2025-10-08 13:17:53 +0200

    fix files publish to url

--- apps/chat/src/components/Chat/Publish/PublishDialog.tsx
+++ apps/chat/src/components/Chat/Publish/PublishDialog.tsx
@@ -106,7 +106,10 @@ const PublishDialogContainer = ({
     const fileResources = filteredConversationFiles.map(({ id }) => ({
       action,
       sourceUrl: id,
-      targetUrl: id,
+      targetUrl: constructPath(
+        getFolderIdFromEntityId(entity.id),
+        splitEntityId(id).name,
+      ),
       reviewUrl: id,
     }));
 
