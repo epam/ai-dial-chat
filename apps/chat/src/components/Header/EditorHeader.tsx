a98f6b19994622e6a67d6b5500c7df59fac9e0cc
Mukhammad-Elbi <mukhammad.amaev@mail.ru>
2025-10-09 17:05:42 +0300

    feat(chat): update stepper design (Issue #4800) (#4835)

--- apps/chat/src/components/Header/EditorHeader.tsx
+++ apps/chat/src/components/Header/EditorHeader.tsx
@@ -1,4 +1,11 @@
-import { IconChevronDown, IconLogout } from '@tabler/icons-react';
+import {
+  IconAlertCircle,
+  IconChevronDown,
+  IconCircleCheck,
+  IconCircleDot,
+  IconCircleDotFilled,
+  IconLogout,
+} from '@tabler/icons-react';
 import { useCallback, useState } from 'react';
 
 import classNames from 'classnames';
@@ -13,10 +20,43 @@ import { SettingsSelectors } from '@/src/store/settings/settings.selectors';
 import { UIActions } from '@/src/store/ui/ui.reducers';
 import { UISelectors } from '@/src/store/ui/ui.selectors';
 
-import { Stepper } from '@/src/components/Common/Stepper';
 import { Logo } from '@/src/components/Header/Logo';
 import { User } from '@/src/components/Header/User/User';
 import { SettingDialog } from '@/src/components/Settings/SettingDialog';
+
+const getTabIcon = <T extends string>(
+  tab: T,
+  activeTab: T,
+  isEditing?: boolean,
+  isDisabled?: boolean,
+  isNotValid?: boolean,
+) => {
+  const selected = tab === activeTab;
+
+  const Icon = selected
+    ? IconCircleDotFilled
+    : isNotValid
+      ? IconAlertCircle
+      : isEditing
+        ? IconCircleCheck
+        : IconCircleDot;
+  const color = isDisabled
+    ? 'text-secondary'
+    : isNotValid
+      ? 'text-error'
+      : isEditing || selected
+        ? 'text-accent-primary'
+        : 'text-secondary';
+
+  return (
+    <Icon
+      className={color}
+      data-qa={selected ? 'selected-step-icon' : 'not-selected-step-icon'}
+      width={24}
+      height={24}
+    />
+  );
+};
 
 interface EditorHeaderTab<T extends string> {
   label: string;
@@ -134,17 +174,48 @@ export const EditorHeader = <T extends string>({
             {title}
           </span>
 
-          <Stepper
-            steps={tabs.map((tab) => ({
-              key: tab.key,
-              label: tab.label,
-              disabled: tab.disabled,
-              error: errorTabsSet?.has(tab.key) ?? false,
-              completed: isEditing,
-            }))}
-            active={activeTab}
-            onChange={onTabClick}
-          />
+          <div
+            className="hidden items-center md:flex"
+            data-qa="steps-container"
+          >
+            {tabs.map((tab, index) => {
+              const isDisabled = tab.disabled;
+              const isNotValid = errorTabsSet?.has(tab.key) ?? false;
+              return (
+                <div key={tab.key} className="flex items-center">
+                  <div
+                    tabIndex={isDisabled ? -1 : undefined}
+                    data-qa="single-step-link"
+                    className={classNames(
+                      'flex items-center gap-2 rounded px-2 py-1.5',
+                      isDisabled
+                        ? 'cursor-default text-secondary'
+                        : 'cursor-pointer text-primary hover:bg-accent-primary-alpha',
+                    )}
+                    onClick={() => onTabClick(tab)}
+                  >
+                    {getTabIcon(
+                      tab.key,
+                      activeTab,
+                      isEditing,
+                      isDisabled,
+                      isNotValid,
+                    )}
+
+                    <span className="grow truncate" data-qa="single-step-title">
+                      {tab.label}
+                    </span>
+                  </div>
+                  {index < tabs.length - 1 && (
+                    <div
+                      className="mx-2 h-0.5 w-5"
+                      style={{ backgroundColor: 'var(--text-secondary)' }}
+                    ></div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
         </div>
 
         <div className="hidden h-full xl:flex">
