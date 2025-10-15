c12d5b8a86cb54fdcf26f1283ebc1a011710f0c5
Mukhammad-Elbi <mukhammad.amaev@mail.ru>
2025-10-15 09:19:07 +0300

    feat(chat): add public toolset's user level login (Issue #4727) (#4857)
    Co-authored-by: Magomed-Elbi_Dzhukalaev <magomed-elbi_dzhukalaev@epam.com>

--- apps/chat/src/store/toolset/toolset.reducer.ts
+++ apps/chat/src/store/toolset/toolset.reducer.ts
@@ -109,6 +109,7 @@ export const toolsetSlice = createSlice({
         redirectUrl?: string;
         auth?: {
           apiKey?: string;
+          authLevel?: ToolsetCredentialsLevel;
         };
       }>,
     ) => {
