ab86a5cffcb622a498016a8cdc920d45a79d4add
Ilya Bondar <ilya_bondar@epam.com>
2025-10-25 01:47:23 +0200

    use @epam/ai-dial-chat-visualizer-connector

--- apps/custom-viewer-test/app/page.tsx
+++ apps/custom-viewer-test/app/page.tsx
@@ -1,18 +1,32 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import { ChatVisualizerConnector } from '@epam/ai-dial-chat-visualizer-connector';
+import React, { useEffect, useRef } from 'react';
+
+const setData = () => {
+  return;
+};
 
 export default function Index() {
+  const chatVisualizerConnector = useRef<ChatVisualizerConnector | null>(null);
+
   useEffect(() => {
-    window.parent.postMessage(
-      { type: process.env.NEXT_PUBLIC_APP_NAME + '/READY' },
-      process.env.NEXT_PUBLIC_VIEWER_HOST,
-    );
-    window.parent.postMessage(
-      { type: process.env.NEXT_PUBLIC_APP_NAME + '/READY_TO_INTERACT' },
-      process.env.NEXT_PUBLIC_VIEWER_HOST,
-    );
+    if (!chatVisualizerConnector.current) {
+      chatVisualizerConnector.current = new ChatVisualizerConnector(
+        process.env.NEXT_PUBLIC_VIEWER_HOST,
+        process.env.NEXT_PUBLIC_APP_NAME,
+        setData,
+      );
+      chatVisualizerConnector.current.sendReady();
+      //Make some actions if needed
+      chatVisualizerConnector.current.sendReadyToInteract();
+      return () => {
+        chatVisualizerConnector.current?.destroy();
+        chatVisualizerConnector.current = null;
+      };
+    }
   }, []);
+
   return (
     <>
       <h1>Custom Viewer Test Page</h1>
