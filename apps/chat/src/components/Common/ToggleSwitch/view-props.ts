f875c856b6e7ba698edbfcc4482c9cd46f473a69
Derikyan <armen.derikyan@gmail.com>
2025-11-04 00:33:03 +0400

    fix(chat): fix code interpreter text appericance (Issue #4751)

--- apps/chat/src/components/Common/ToggleSwitch/view-props.ts
+++ apps/chat/src/components/Common/ToggleSwitch/view-props.ts
@@ -3,6 +3,7 @@ export interface ToggleSwitchProps {
   handleSwitch: () => void;
   switchOnText?: string | null;
   switchOFFText?: string | null;
+  additionalText?: string | null;
   className?: string;
   disabled?: boolean;
   tooltip?: string;
