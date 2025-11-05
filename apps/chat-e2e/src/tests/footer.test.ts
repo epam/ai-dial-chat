a643bee6b769edbdb9d4aa6522f62b92829b40d2
Ilya Bondar <ilya_bondar@epam.com>
2025-11-05 14:23:19 +0100

    EPAM DIAL -> DIAL

--- apps/chat-e2e/src/tests/footer.test.ts
+++ apps/chat-e2e/src/tests/footer.test.ts
@@ -3,7 +3,7 @@ import { ExpectedMessages } from '@/src/testData';
 import { expect } from '@playwright/test';
 
 dialTest(
-  'EPAM DIAL leads to kb',
+  'DIAL leads to kb',
   async ({
     dialHomePage,
     footerAssertion,
