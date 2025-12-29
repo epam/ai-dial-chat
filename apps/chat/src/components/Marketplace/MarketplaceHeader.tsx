91eda94333905907584e0ca8044e30943f11cc16
Magomed-Elbi_Dzhukalaev <magomed-elbi_dzhukalaev@epam.com>
2025-12-29 11:28:27 +0300

    fix(chat): rework header layout, make username adaptive

--- apps/chat/src/components/Marketplace/MarketplaceHeader.tsx
+++ apps/chat/src/components/Marketplace/MarketplaceHeader.tsx
@@ -62,7 +62,7 @@ export const MarketplaceHeader = () => {
       RightItems={
         !isUserMenuHidden && (
           <>
-            <div className="w-[48px] md:w-auto">
+            <div className="w-[48px] overflow-hidden md:w-auto md:grow">
               <User />
             </div>
 
