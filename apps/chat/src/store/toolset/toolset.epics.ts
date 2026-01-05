5a2b65b0a651f20e73dbca00cc31431009e10139
Magomed-Elbi_Dzhukalaev <magomed-elbi_dzhukalaev@epam.com>
2026-01-05 14:35:16 +0300

    fix(chat): fix toolset editor infinite loader after failed save

--- apps/chat/src/store/toolset/toolset.epics.ts
+++ apps/chat/src/store/toolset/toolset.epics.ts
@@ -331,6 +331,7 @@ const updateToolsetEpic: AppEpic = (action$) =>
                   }
 
                   const actions: Observable<AppAction>[] = [
+                    of(UIActions.setEditorLoader(false)),
                     of(
                       ToolsetActions.updateToolsetSuccess({
                         oldToolset: payload.oldToolset,
