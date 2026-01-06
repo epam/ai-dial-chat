b1a1614429a801e1664e859722a615ee0fd7b2d3
Ilya Bondar <ilya_bondar@epam.com>
2026-01-06 01:40:04 +0100

    Update ChatMDComponent.tsx

--- apps/chat/src/components/Markdown/ChatMDComponent.tsx
+++ apps/chat/src/components/Markdown/ChatMDComponent.tsx
@@ -138,7 +138,6 @@ const getMDComponents = (
 
       return (
         <details
-          open
           className={classnames(
             'rounded bg-layer-3 [&_details]:bg-layer-1 [&_details_details]:bg-layer-3',
             ' [&>summary]:border-tertiary [&[open]>summary>svg]:rotate-180 [&[open]>summary]:border-b [&_.codeblock>*]:!bg-layer-1 [&_details>summary]:border-secondary [&_details]:border [&_details]:border-secondary [&_details_.codeblock>*]:!bg-layer-3 [&_details_.codeblock>div]:border-tertiary [&_details_.codeblock]:border-0 [&_details_details>summary]:border-tertiary [&_details_details]:border-0 [&_details_details_.codeblock>*]:!bg-layer-1 [&_details_details_.codeblock>div]:border-secondary [&_details_details_.codeblock]:border',
