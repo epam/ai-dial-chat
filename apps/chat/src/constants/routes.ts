95ac6446b823bde80d6b37f9d6efa26a0c857ede
Ilya Bondar <ilya_bondar@epam.com>
2026-01-12 13:44:40 +0100

    chore(chat): rename `Files manager` to `File manager`

--- apps/chat/src/constants/routes.ts
+++ apps/chat/src/constants/routes.ts
@@ -4,7 +4,7 @@ export enum Routes {
   SelectedWidget = '/widgets/[slug]',
   ToolsetEditor = '/toolset-editor',
   AppsEditor = '/apps-editor',
-  FilesManager = '/files-manager',
+  FilesManager = '/file-manager',
   ToolsetSignIn = '/auth/toolset-signin',
   Chat = '/',
   NotFound = '/404',
