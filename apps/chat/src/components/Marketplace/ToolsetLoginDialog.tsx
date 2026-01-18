18ceac9259b1138c4297c43164bb403ec9bdb24e
Magomed-Elbi_Dzhukalaev <magomed-elbi_dzhukalaev@epam.com>
2026-01-18 11:30:54 +0300

    feat(chat): allow to select version in toolset login dialog

--- apps/chat/src/components/Marketplace/ToolsetLoginDialog.tsx
+++ apps/chat/src/components/Marketplace/ToolsetLoginDialog.tsx
@@ -4,6 +4,7 @@ import { FormProvider, useForm } from 'react-hook-form';
 
 import { useTranslation } from 'next-i18next';
 
+import { getGroupMarketplaceEntityKey } from '@/src/utils/app/marketplace';
 import { isEntityIdPublic } from '@/src/utils/app/publications';
 import { isToolsetSignedIn } from '@/src/utils/app/toolsets';
 
@@ -13,8 +14,13 @@ import { Translation } from '@/src/types/translation';
 
 import { MarketplaceActions, ToolsetActions } from '@/src/store/actions';
 import { useAppDispatch, useAppSelector } from '@/src/store/hooks';
-import { AuthSelectors, MarketplaceSelectors } from '@/src/store/selectors';
+import {
+  AuthSelectors,
+  MarketplaceSelectors,
+  ToolsetSelectors,
+} from '@/src/store/selectors';
 
+import { ModelVersionSelect } from '@/src/components/Chat/ModelVersionSelect';
 import { ModelIcon } from '@/src/components/Chatbar/ModelIcon';
 import { ConfirmDialog } from '@/src/components/Common/ConfirmDialog';
 import { Modal } from '@/src/components/Common/Modal';
@@ -55,6 +61,7 @@ export const ToolsetLoginDialogView: FC<ToolsetLoginDialogProps> = ({
   const dispatch = useAppDispatch();
 
   const isAdmin = useAppSelector(AuthSelectors.selectIsAdmin);
+  const allToolsets = useAppSelector(ToolsetSelectors.selectToolsets);
   const authType = entity.authSettings.authenticationType;
   const isPublic = isEntityIdPublic(entity);
 
@@ -112,6 +119,16 @@ export const ToolsetLoginDialogView: FC<ToolsetLoginDialogProps> = ({
     }
   }, [authLevel, entity, t]);
 
+  const allVersions = useMemo(
+    () =>
+      allToolsets.filter(
+        (t) =>
+          getGroupMarketplaceEntityKey(t) ===
+          getGroupMarketplaceEntityKey(entity),
+      ),
+    [allToolsets, entity],
+  );
+
   const handleClose = useCallback(() => {
     dispatch(MarketplaceActions.setLoginEntity());
   }, [dispatch]);
@@ -146,6 +163,13 @@ export const ToolsetLoginDialogView: FC<ToolsetLoginDialogProps> = ({
     handleClose,
     authLevel,
   ]);
+
+  const handleVersionChange = useCallback(
+    (toolset: ToolsetModel) => {
+      dispatch(MarketplaceActions.setLoginEntity(toolset));
+    },
+    [dispatch],
+  );
 
   const handleCredsTabClick = useCallback(
     (value: ToolsetCredentialsLevel) => {
@@ -208,12 +232,18 @@ export const ToolsetLoginDialogView: FC<ToolsetLoginDialogProps> = ({
       <div className="flex gap-2 px-6 py-4">
         <ModelIcon size={40} entityId={entity.id} entity={entity} />
         <div className="flex flex-col justify-between">
-          <h3 className="text-base font-semibold leading-6 text-primary">
-            {entity.name}
-          </h3>
-          <h4 className="text-sm font-normal leading-5 text-primary">
-            {t('Version')}: {entity.version}
-          </h4>
+          <h3 className="text-sm font-semibold text-primary">{entity.name}</h3>
+          <div className="flex items-center gap-1">
+            <span className="text-xs text-primary">{t('Version: ')}</span>
+
+            <ModelVersionSelect
+              entities={allVersions}
+              currentEntity={entity}
+              onSelect={handleVersionChange}
+              className="truncate"
+              triggerClassName="!text-xs bg-layer-4 rounded p-1"
+            />
+          </div>
         </div>
       </div>
 
