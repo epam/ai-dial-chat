aa07b4ea113fe0f0b3beba9dd6602b81dc33411b
Aliaksandr Kezik <a.kezzk@gmail.com>
2026-02-19 14:40:05 +0100

    memo revert

--- apps/chat/src/components/Chat/Publish/PublicationHandler/ReviewRowItems/PublicationItemRow.tsx
+++ apps/chat/src/components/Chat/Publish/PublicationHandler/ReviewRowItems/PublicationItemRow.tsx
@@ -215,14 +215,14 @@ const PublicationVersionInfo: React.FC<PublicationVersionInfoProps> = ({
     [item.id, selectedPublicationItems],
   );
 
-  const overrideTriggerText = (() => {
+  const overrideTriggerText = useMemo(() => {
     if (isDeleteAction && itemVersionsSelected.length > 1) {
       const isAllVersionsSelected =
         versionGroup?.allVersions.length === itemVersionsSelected.length ||
         modelsVersionGroup.length === itemVersionsSelected.length ||
         toolsetVersionGroup.length === itemVersionsSelected.length;
 
-      return isAllVersionsSelected ? t('All') : t('Few');
+      return t(isAllVersionsSelected ? 'All' : 'Few');
     }
 
     if (itemVersionsSelected.length === 1) {
@@ -230,7 +230,14 @@ const PublicationVersionInfo: React.FC<PublicationVersionInfoProps> = ({
     }
 
     return undefined;
-  })();
+  }, [
+    isDeleteAction,
+    itemVersionsSelected,
+    modelsVersionGroup.length,
+    t,
+    toolsetVersionGroup.length,
+    versionGroup?.allVersions.length,
+  ]);
 
   if (isFileId(item.id)) {
     return (
