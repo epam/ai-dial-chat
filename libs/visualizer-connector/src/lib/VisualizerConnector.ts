3f1eabc018cd21cb9a04f8f309bb85922d41400e
Sergey Zinchenko <sergey.zinchenko.rnd@gmail.com>
2026-02-19 20:06:36 +0100

    fix(chat): restrict message processing to specific iframe instance

--- libs/visualizer-connector/src/lib/VisualizerConnector.ts
+++ libs/visualizer-connector/src/lib/VisualizerConnector.ts
@@ -196,6 +196,12 @@ export class VisualizerConnector {
   protected process = (
     event: MessageEvent<VisualizerConnectorRequest>,
   ): void => {
+
+    // Only process messages from this specific iframe instance
+    if (event.source !== this.iframe.contentWindow) {
+      return;
+    }
+
     if (
       event.data.type ===
       `${this.options.visualizerName}/${VisualizerConnectorEvents.ready}`
