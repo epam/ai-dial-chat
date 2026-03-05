2034bc293ca2228eb81ffbb6e0fadc654f6da597
Armen Derikyan <82438895+Derikyan@users.noreply.github.com>
2026-03-05 17:42:04 +0400

    fix(chat): fix tooltips for code app in quick app 2.0 (Issue #5767) (#5945)

--- apps/chat/src/components/Common/AgentAndToolsetSelector/ChipTooltipContent.tsx
+++ apps/chat/src/components/Common/AgentAndToolsetSelector/ChipTooltipContent.tsx
@@ -10,9 +10,6 @@ interface ChipTooltipContentProps {
   item?: MarketplaceEntity;
   name: string;
   version?: string;
-  isInvalid: boolean;
-  isLoggedOut: boolean;
-  isUndeployed: boolean;
   isInSelectionList?: boolean;
   isCustomTool?: boolean;
   readonly?: boolean;
@@ -23,9 +20,6 @@ export const ChipTooltipContent: React.FC<ChipTooltipContentProps> = ({
   item,
   name,
   version,
-  isInvalid,
-  isLoggedOut,
-  isUndeployed,
   isInSelectionList,
   isCustomTool,
   readonly,
@@ -37,9 +31,6 @@ export const ChipTooltipContent: React.FC<ChipTooltipContentProps> = ({
       <StatusMessage
         id={id}
         item={item}
-        isInvalid={isInvalid}
-        isLoggedOut={isLoggedOut}
-        isUndeployed={isUndeployed}
         isInSelectionList={isInSelectionList}
         isCustomTool={isCustomTool}
         readonly={readonly}
