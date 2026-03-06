a51f80c1ea66f8d5357c0062ee2b13f87c47abf1
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-03-06 14:43:24 -0500

    fix: [Regression][Admin view]: incorrect color of counter of requests in Prompt panel

--- apps/chat/src/components/Chat/Publish/ApproveRequiredSection.tsx
+++ apps/chat/src/components/Chat/Publish/ApproveRequiredSection.tsx
@@ -245,7 +245,7 @@ export const ApproveRequiredSection = ({
       isHighlighted={isSectionHighlighted}
       additionalNode={
         !!publicationsToReviewCount && (
-          <span className="absolute right-4 flex h-[14px] min-w-[14px] select-none items-center justify-center rounded bg-accent-secondary px-[2px] text-xxs font-semibold text-layer-3">
+          <span className="absolute right-4 flex h-[14px] min-w-[14px] select-none items-center justify-center rounded bg-accent-tertiary-alpha px-[2px] text-xxs font-semibold text-accent-tertiary">
             {publicationsToReviewCount}
           </span>
         )
