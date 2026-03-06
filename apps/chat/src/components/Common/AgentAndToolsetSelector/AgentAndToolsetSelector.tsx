cd42fbb205df8c9f96d409601f3c5b903892421a
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-03-06 11:07:46 -0500

    fix: [Quick app 2.0][JSON editor]: update tooltip for json toggle for public quick app when admin view

--- apps/chat/src/components/Common/AgentAndToolsetSelector/AgentAndToolsetSelector.tsx
+++ apps/chat/src/components/Common/AgentAndToolsetSelector/AgentAndToolsetSelector.tsx
@@ -108,7 +108,7 @@ export const AgentAndToolsetSelector: React.FC<
                 switchOFFText={t('OFF')}
                 additionalText={t('JSON')}
                 className="flex w-fit items-center gap-2"
-                tooltip={t('This application is public and cannot be edited')}
+                tooltip={t(readonly ? 'Switch to JSON view for Agents and Toolsets' : 'This application is public and cannot be edited')}
               />
             </>
           )}
