c28f570d125ebfde2ff96a692299f46a0ad87e38
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-03-10 17:19:36 +0100

    fix(chat): fix long description for agents in marketplace

--- apps/chat/src/components/Common/MarkdownDescription.tsx
+++ apps/chat/src/components/Common/MarkdownDescription.tsx
@@ -38,7 +38,7 @@ export const EntityMarkdownDescription = ({
     <MemoizedReactMarkdown
       className={classNames(
         className,
-        'prose-sm text-xs prose-a:break-all prose-a:underline break-all',
+        'prose-sm break-all text-xs prose-a:break-all prose-a:underline',
       )}
       remarkPlugins={[remarkGfm]}
       rehypePlugins={[
