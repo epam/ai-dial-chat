b05a1083928246ffea67be7dcea21b73de9f3644
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2026-03-18 11:22:00 +0100

    fix: fix toolset auth title in marketplace (Issue  #4711) (#6106)

--- apps/chat/src/constants/toolsets.ts
+++ apps/chat/src/constants/toolsets.ts
@@ -1,6 +1,17 @@
+import {
+  Icon,
+  IconBrandOauth,
+  IconKey,
+  IconLockOff,
+  IconProps,
+} from '@tabler/icons-react';
+import { ForwardRefExoticComponent, RefAttributes } from 'react';
+
 import { translate } from '@/src/utils/app/translation';
 
 import { ApiKeys } from '@/src/types/common';
+
+import { ToolsetAuthTypes } from '@epam/ai-dial-shared';
 
 export enum ToolsetEditorQuery {
   Id = 'id',
@@ -21,3 +32,24 @@ export enum ToolsetAuthAction {
 export const PUBLIC_TOOLSET_TOOLTIP = translate(
   'This toolset is public and cannot be edited',
 );
+
+export const AUTH_TYPE_OPTIONS: Record<
+  string,
+  {
+    name: string;
+    Icon: ForwardRefExoticComponent<IconProps & RefAttributes<Icon>>;
+  }
+> = {
+  [ToolsetAuthTypes.OAUTH]: {
+    name: 'OAuth',
+    Icon: IconBrandOauth,
+  },
+  [ToolsetAuthTypes.API_KEY]: {
+    name: 'API Key',
+    Icon: IconKey,
+  },
+  [ToolsetAuthTypes.NONE]: {
+    name: 'Without authentication',
+    Icon: IconLockOff,
+  },
+};
