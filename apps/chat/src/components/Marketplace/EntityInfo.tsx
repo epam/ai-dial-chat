770e8d66b28edf8e35fa6a1d093d6ef0a33d2d31
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-03-19 13:06:10 +0100

    fix(chat): fix gaps for mobile application card

--- apps/chat/src/components/Marketplace/EntityInfo.tsx
+++ apps/chat/src/components/Marketplace/EntityInfo.tsx
@@ -43,7 +43,7 @@ export function EntityInfo({ entityInfo }: Props) {
         </button> */}
       </div>
 
-      <div className="flex text-sm gap-3 sm:gap-12">
+      <div className="flex text-sm flex-col gap-6 sm:flex-row sm:gap-12">
         <InfoColumn infoLabel={t('Author')} dataQa="author">
           {entityInfo?.author ?? t('Unknown')}
         </InfoColumn>
