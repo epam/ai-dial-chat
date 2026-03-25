53a336d1a391d9e6519a87f64975e9e72d3ee284
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-03-25 12:56:49 +0100

    fix(chat): hide tooltip for mobile view

--- apps/chat/src/components/Navigation/NavigationButton.tsx
+++ apps/chat/src/components/Navigation/NavigationButton.tsx
@@ -1,31 +1,22 @@
 import { Icon } from '@tabler/icons-react';
 import { ReactNode } from 'react';
 
-
-
 import classNames from 'classnames';
-
-
 
 import { useScreenState } from '@/src/hooks/useScreenState';
 
-
-
 import { ScreenState } from '@/src/types/common';
 
-
-
 import { useAppSelector } from '@/src/store/hooks';
-import { ConversationsSelectors, ModelsSelectors, SettingsSelectors } from '@/src/store/selectors';
-
-
+import {
+  ConversationsSelectors,
+  ModelsSelectors,
+  SettingsSelectors,
+} from '@/src/store/selectors';
 
 import { DEFAULT_ICON_SIZES } from '@/src/constants/icons';
 
-
-
 import { Tooltip } from '@/src/components/Common/Tooltip';
-
 
 interface NavigationButtonProps {
   onClick: () => void;
