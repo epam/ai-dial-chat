3a8f4b2be2d1f2ac936f8ba3646bbdcc2931c922
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-04-02 10:44:22 +0200

    fix(chat): fix Go to workspace button placement and styles

--- apps/chat/src/components/Chat/TalkTo/TalkToModal.tsx
+++ apps/chat/src/components/Chat/TalkTo/TalkToModal.tsx
@@ -1,10 +1,4 @@
-import {
-  useCallback,
-  useLayoutEffect,
-  useMemo,
-  useRef,
-  useState,
-} from 'react';
+import { useCallback, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { useRouter } from 'next/router';
