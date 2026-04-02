065c650b5d7f0150c5e38a1bdb900b78dc593e6a
Magomed-Elbi_Dzhukalaev <magomed-elbi_dzhukalaev@epam.com>
2026-04-02 16:41:21 +0300

    fix(chat-e2e): fix unshareCustomApp expected app name

--- apps/chat-e2e/src/tests/shareCustomApp.test.ts
+++ apps/chat-e2e/src/tests/shareCustomApp.test.ts
@@ -16,6 +16,8 @@ import { Conversation } from '@epam/ai-dial-shared';
 let appData: CustomAppAttributes;
 const getIconName = (iconUrl: string) =>
   iconUrl.substring(iconUrl.lastIndexOf('/') + 1);
+const getAppName = (id: string) =>
+  id.substring(id.lastIndexOf('/') + 1).split('__')[0];
 
 dialSharedWithMeTest(
   'Sharing custom app without editing permissions via context menu in DIAL Marketplace.\n' +
@@ -613,7 +615,7 @@ dialSharedWithMeTest(
           ExpectedConstants.removeAccessTitle,
         );
         await additionalShareUserConfirmationDialogAssertion.assertConfirmationMessage(
-          ExpectedConstants.removeYourAccessMessage(appData.name),
+          ExpectedConstants.removeYourAccessMessage(getAppName(appData.name)),
         );
       },
     );
@@ -804,7 +806,7 @@ dialSharedWithMeTest(
           ExpectedConstants.removeAccessTitle,
         );
         await additionalShareUserConfirmationDialogAssertion.assertConfirmationMessage(
-          ExpectedConstants.removeYourAccessMessage(appData.name),
+          ExpectedConstants.removeYourAccessMessage(getAppName(appData.name)),
         );
       },
     );
@@ -891,7 +893,7 @@ dialSharedWithMeTest(
           ExpectedConstants.removeAccessTitle,
         );
         await additionalShareUserConfirmationDialogAssertion.assertConfirmationMessage(
-          ExpectedConstants.removeYourAccessMessage(appData.name),
+          ExpectedConstants.removeYourAccessMessage(getAppName(appData.name)),
         );
       },
     );
