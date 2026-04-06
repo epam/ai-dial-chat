955d4c88ee08aa35cf3a2c542f7c0d55426ec4af
Ilya Bondar <ilya_bondar@epam.com>
2026-04-07 01:36:18 +0200

    fix(chat): fix displaying cursor during details streaming (Issue #6257)

--- apps/chat/src/components/Markdown/ChatMDComponent.tsx
+++ apps/chat/src/components/Markdown/ChatMDComponent.tsx
@@ -127,8 +127,18 @@ const getMDComponents = (
       // In order to style the contents paddings correctly, we need to wrap them into container
       // Contents of <details> element follow the summary element unwrapped by default,
       // so styling them otherwise would be hacky.
+      let showCursor = false;
+      const childrenArray = Children.toArray(children).map((child) => {
+        if (typeof child === 'string' && child?.length) {
+          if (child.includes(modelCursorSignWithBackquote)) {
+            showCursor = true;
+            return child.replaceAll(modelCursorSignWithBackquote, '');
+          }
+        }
+        return child;
+      });
       const [summary, content] = partition(
-        Children.toArray(children),
+        childrenArray,
         (child: ReactNode) =>
           isObject(child) &&
           'type' in child &&
@@ -144,9 +154,13 @@ const getMDComponents = (
             ' [&>summary]:border-tertiary [&[open]>summary>svg]:rotate-180 [&[open]>summary]:border-b [&_.codeblock>*]:!bg-layer-1 [&_details>summary]:border-secondary [&_details]:border [&_details]:border-secondary [&_details_.codeblock>*]:!bg-layer-3 [&_details_.codeblock>div]:border-tertiary [&_details_.codeblock]:border-0 [&_details_details>summary]:border-tertiary [&_details_details]:border-0 [&_details_details_.codeblock>*]:!bg-layer-1 [&_details_details_.codeblock>div]:border-secondary [&_details_details_.codeblock]:border',
           )}
           {...props}
+          open={showCursor || props.open}
         >
           {summary}
-          <div className="p-3">{content}</div>
+          <div className="p-3">
+            {content}
+            {showCursor && <BlinkingCursor isShowing={isShowResponseLoader} />}
+          </div>
         </details>
       );
     },
