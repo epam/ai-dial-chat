6ae4c59ea11f3460da89daf48e0372332744f82d
macnartov <macnartov@gmail.com>
2026-04-28 11:55:17 +0200

    feat(chat-e2e): File Manager tests (#6371)

---
+++ apps/chat-e2e/src/ui/webElements/common/fileConflictResolutionPopup.ts
@@ -0,0 +1,52 @@
+import { ConfirmationPopup } from './confirmationPopup';
+
+import { FileConflictResolutionSelectors } from '@/src/ui/selectors';
+import { Locator, Page } from '@playwright/test';
+
+export class FileConflictResolutionPopup extends ConfirmationPopup {
+  constructor(page: Page) {
+    super(page, FileConflictResolutionSelectors.confirm);
+  }
+
+  getSingleFileRadio(
+    value:
+      | typeof FileConflictResolutionSelectors.replace
+      | typeof FileConflictResolutionSelectors.duplicate,
+  ): Locator {
+    return this.rootLocator.locator(
+      FileConflictResolutionSelectors.singleFileRadio(value),
+    );
+  }
+
+  getMultipleFilesRadio(
+    value:
+      | typeof FileConflictResolutionSelectors.replaceAll
+      | typeof FileConflictResolutionSelectors.duplicateAll
+      | typeof FileConflictResolutionSelectors.decideForEach,
+  ): Locator {
+    return this.rootLocator.locator(
+      FileConflictResolutionSelectors.multipleFilesRadio(value),
+    );
+  }
+
+  async selectSingleFileAction(
+    value:
+      | typeof FileConflictResolutionSelectors.replace
+      | typeof FileConflictResolutionSelectors.duplicate,
+  ) {
+    await this.rootLocator
+      .locator(FileConflictResolutionSelectors.radioLabel(value))
+      .click();
+  }
+
+  async selectMultipleFilesStrategy(
+    value:
+      | typeof FileConflictResolutionSelectors.replaceAll
+      | typeof FileConflictResolutionSelectors.duplicateAll
+      | typeof FileConflictResolutionSelectors.decideForEach,
+  ) {
+    await this.rootLocator
+      .locator(FileConflictResolutionSelectors.radioLabel(value))
+      .click();
+  }
+}
