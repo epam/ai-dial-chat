17458fd120a08742c7918eb973d498a06fa0a8db
Yauheniya Taleika <yauheniya_taleika@epam.com>
2026-05-06 10:26:29 +0200

    fix: correct choice of logo for dark and light themes

--- apps/chat/src/pages/api/themes/styles.ts
+++ apps/chat/src/pages/api/themes/styles.ts
@@ -145,7 +145,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
           generateColorsCssVariables(theme.authColors),
           generateUrlsCssVariables({
             'app-logo':
-              (theme.displayName == 'Dark'
+              (theme.id == 'dark'
                 ? getImageUrl(json, 'chat-logo-dark')
                 : getImageUrl(json, 'chat-logo-light')) || theme['app-logo'],
           }),
