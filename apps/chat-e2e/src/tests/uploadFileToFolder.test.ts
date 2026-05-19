d45c2e49479d9aa68fe883a6e3bf42f2e1a55271
Derikyan <armen.derikyan@gmail.com>
2026-05-19 06:44:50 +0400

    fix test

--- apps/chat-e2e/src/tests/uploadFileToFolder.test.ts
+++ apps/chat-e2e/src/tests/uploadFileToFolder.test.ts
@@ -33,7 +33,7 @@ dialTest(
           .getNewButtonDropdownMenu()
           .selectItem(MenuOptions.newFolder);
         await fileManagerGridAssertion.assertGridNameCellInputState('visible');
-        await fileManagerGridAssertion.assertGridNameCellInputValue('');
+        await fileManagerGridAssertion.assertGridNameCellInputValue('New folder 1');
       },
     );
 
