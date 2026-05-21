98a350e6e2bd6f2361c6bdd56ab2bd87517e8798
Palina_Kryvasheyeva <Palina_Kryvasheyeva@epam.com>
2026-05-21 18:16:19 +0200

    fix(chat): fix checkbox style

--- apps/chat/src/components/Common/Checkbox.tsx
+++ apps/chat/src/components/Common/Checkbox.tsx
@@ -17,6 +17,7 @@ export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
         <input
           className={classNames(
             'checkbox peer size-[18px] bg-layer-3',
+            isPartialChecked && 'border-accent-primary',
             className,
           )}
           type="checkbox"
