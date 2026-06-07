453e61e9768108538eca4249d44022a07a084878
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-06-07 19:00:33 +0200

    fix test

--- libs/conversation-input/src/utils/deployment.tsx
+++ libs/conversation-input/src/utils/deployment.tsx
@@ -1,5 +1,4 @@
 import type { DeploymentItem } from '@epam/ai-dial-chat-shared';
-import { IconApps } from '@tabler/icons-react';
 import type { ReactNode } from 'react';
 import FallbackEntityIcon from '../assets/fallback-entity-icon.svg?react';
 import { DeploymentIcon } from '../components/Input/Icon/DeploymentIcon.js';
