985371e8435496d84c2b1501320d7e808420227b
Valery Dluski <valerydluski@gmail.com>
2026-06-10 13:54:08 +0200

    feat: enable attachment click handling in UserMessageBubble

--- apps/chat/src/constants/translation-keys.ts
+++ apps/chat/src/constants/translation-keys.ts
@@ -146,3 +146,7 @@ export enum SidebarI18nKeys {
   Empty = 'sidebar.sources.noData',
   AttachmentDownloadLabel = 'sidebar.sources.attachment.downloadLabel',
 }
+
+export enum MessagesI18nKeys {
+  AttachmentDownloadLabel = 'messages.attachment.downloadLabel',
+}
