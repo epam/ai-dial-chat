5339719d3812368b88b7d9fd4dd04b531305ff31
denys-kolomiitsev <denys_kolomiitsev@epam.com>
2026-06-16 19:52:54 +0200

    fix: fix style

--- apps/chat/src/constants/__tests__/default-server-settings.test.ts
+++ apps/chat/src/constants/__tests__/default-server-settings.test.ts
@@ -12,6 +12,7 @@ describe('parseDefaultModel', () => {
   let consoleErrorSpy: ReturnType<typeof vi.spyOn>;
 
   beforeEach(() => {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     consoleErrorSpy = vi.spyOn(console, 'error').mockImplementation(() => {});
   });
 
