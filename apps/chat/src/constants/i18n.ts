f136b50d56d3a3ea91b3331b702b310d70d550cb
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-06-17 14:46:55 +0200

    fix(chat): fix i18n for models

--- apps/chat/src/constants/i18n.ts
+++ apps/chat/src/constants/i18n.ts
@@ -1040,6 +1040,7 @@ export enum MarketplaceI18nKeys {
   SelectDocuments = 'Select documents',
   DocumentsLowercase = 'documents',
   ModelMarketplace = 'Model',
+  ModelEntity = 'model',
   ConfigureToolsets = 'Configure toolsets',
   InstructionsMarketplace = 'Instructions',
   InstructionsPlaceholder = 'Instructions of your application',
