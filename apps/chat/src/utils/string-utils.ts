4fe581d17267c790772d16979d875387ad4f7eef
Nataliia Tolokontseva <nataliia_tolokontseva@epam.com>
2026-06-18 22:07:29 +0300

    fix: Don't allow entering forbidden symbols to chat name

--- apps/chat/src/utils/string-utils.ts
+++ apps/chat/src/utils/string-utils.ts
@@ -1,3 +1,12 @@
+// Characters prohibited in conversation names: tab, ", :, ;, /, \, ,, =, {, }, %, &
+export const PROHIBITED_CONVERSATION_NAME_CHARS_RE = /[\t":;/\\,={}%&]/g;
+
+export const sanitizeConversationName = (name: string): string =>
+  name.replace(PROHIBITED_CONVERSATION_NAME_CHARS_RE, '');
+
+export const stripTrailingDots = (name: string): string =>
+  name.replace(/\.+$/, '');
+
 export const getUtf8ByteLength = (str: string): number =>
   new TextEncoder().encode(str).byteLength;
 
