feff0509ec59c1d7fb6f33fc5b138582ea12b03e
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-06-19 17:49:06 +0200

    chore: update ui kit

--- libs/catalog/src/components/StarToggleButton/StarToggleButton.tsx
+++ libs/catalog/src/components/StarToggleButton/StarToggleButton.tsx
@@ -4,7 +4,7 @@ import {
   ElementSize,
 } from '@epam/ai-dial-ui-kit';
 import { IconStar, IconStarFilled } from '@tabler/icons-react';
-import { FC } from 'react';
+import { FC, MouseEvent } from 'react';
 import styles from './StarToggleButton.module.scss';
 
 /** Props for StarToggleButton. */
@@ -12,7 +12,7 @@ export interface StarToggleButtonProps {
   /** Whether the item is currently starred. */
   isStarred: boolean;
   /** Called when the button is clicked. */
-  onClick: () => void;
+  onClick: (e: MouseEvent<HTMLElement>) => void;
   /** Button size. Defaults to the ui-kit ghost icon button default. */
   size?: ElementSize;
   /** Accessible label for the button. Defaults to `'Toggle favorite'`. */
