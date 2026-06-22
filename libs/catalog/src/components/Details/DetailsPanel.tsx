9e12c4cb6cdf2ab844d43c74e019b63e58e809b5
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-06-22 22:46:28 +0200

    fix import

--- libs/catalog/src/components/Details/DetailsPanel.tsx
+++ libs/catalog/src/components/Details/DetailsPanel.tsx
@@ -4,7 +4,7 @@ import { FC, useCallback, useEffect, useMemo, useState } from 'react';
 import type { DetailsPanelProps } from '../../models/item-details-props';
 import { CatalogDetailsTab } from '../../types/detail-tab';
 import { StarToggleButton } from '../StarToggleButton/StarToggleButton';
-import { ApiDetails } from './CatalogApiDetails';
+import { ApiDetails } from './ApiDetails';
 import styles from './DetailsPanel.module.scss';
 import { Header } from './Header/Header';
 import { Summary } from './Summary/Summary';
