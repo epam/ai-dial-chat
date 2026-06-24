c8e34acd886bc337149ebaf2a6b6bafda8dfd15c
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-06-24 23:32:11 +0200

    fix lint

--- libs/catalog/src/components/ListView/tests/ListView.spec.tsx
+++ libs/catalog/src/components/ListView/tests/ListView.spec.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import React from 'react';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import type { CatalogItem } from '../../../models/catalog-item';
 import { CatalogEntityType } from '../../../types/entity-type';
