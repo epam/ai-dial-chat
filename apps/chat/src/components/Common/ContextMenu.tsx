278dbe5a89d5dc057d9f03ffd6ea53319a3be372
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-07-09 20:49:54 +0200

    fix(chat): fix buttons for chat

--- apps/chat/src/components/Common/ContextMenu.tsx
+++ apps/chat/src/components/Common/ContextMenu.tsx
@@ -8,6 +8,8 @@ import { ContextMenuProps, MenuItemRendererProps } from '@/src/types/menu';
 
 import { Spinner } from '@/src/components/Common/Spinner';
 
+import { DialGhostIconButton, ElementSize } from '@epam/ai-dial-ui-kit';
+
 import { Menu, MenuItem } from './DropdownMenu';
 import { Tooltip } from './Tooltip';
 
@@ -110,11 +112,17 @@ export function ContextMenu({
   const menuContent =
     TriggerCustomRenderer ||
     (!hideTriggerIcon && (
-      <TriggerIcon
-        size={triggerIconSize}
-        width={triggerIconSize}
-        height={triggerIconSize}
-        strokeWidth={1.5}
+      <DialGhostIconButton
+        size={ElementSize.Small}
+        disabled={disabled}
+        icon={
+          <TriggerIcon
+            size={triggerIconSize}
+            width={triggerIconSize}
+            height={triggerIconSize}
+            strokeWidth={1.5}
+          />
+        }
         onClick={(e) => {
           e.stopPropagation();
         }}
