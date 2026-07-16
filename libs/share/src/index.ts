ec03a433f547a6a534bbad9e1f8df1631e14b7dd
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-07-16 20:15:21 +0200

    refactor: review share lib and improve props

--- libs/share/src/index.ts
+++ libs/share/src/index.ts
@@ -2,8 +2,14 @@ export { default as SharePopover } from './components/SharePopover/SharePopover'
 export type {
   SharePopoverLabels,
   SharePopoverProps,
+  SharePopoverStyles,
+  SharePopoverTypography,
 } from './models/share-popover-props';
 export { QrCode } from './components/QrCode/QrCode';
-export type { QrCodeProps } from './components/QrCode/QrCode';
+export type {
+  QrCodeLabels,
+  QrCodeProps,
+  QrCodeStyles,
+} from './components/QrCode/QrCode';
 export type { ShareLinkData } from './models/share-link-data';
 export { ShareLinkAccess, SharePopoverView } from './types/share';
