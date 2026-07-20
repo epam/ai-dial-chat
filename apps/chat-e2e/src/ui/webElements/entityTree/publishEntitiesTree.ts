4906020288edfd017bee37b4dcd8b1eb5c994742
irinakartun <irina.kartun@gmail.com>
2026-07-20 12:42:00 +0200

    test: toolset versions unpublishing (#7821)
    Co-authored-by: macnartov <macnartov@gmail.com>

--- apps/chat-e2e/src/ui/webElements/entityTree/publishEntitiesTree.ts
+++ apps/chat-e2e/src/ui/webElements/entityTree/publishEntitiesTree.ts
@@ -1,8 +1,20 @@
 import { Tags } from '@/src/ui/domData';
 import { EntitySelectors, IconSelectors } from '@/src/ui/selectors';
 import { EntitiesTree } from '@/src/ui/webElements/entityTree/entitiesTree';
+import { PublishVersionChecklistDropdown } from '@/src/ui/webElements/publishVersionChecklistDropdown';
 
 export class PublishEntitiesTree extends EntitiesTree {
+  private versionChecklistDropdown!: PublishVersionChecklistDropdown;
+
+  getVersionChecklistDropdown(): PublishVersionChecklistDropdown {
+    if (!this.versionChecklistDropdown) {
+      this.versionChecklistDropdown = new PublishVersionChecklistDropdown(
+        this.page,
+      );
+    }
+    return this.versionChecklistDropdown;
+  }
+
   public getEntityVersion(entityName: string, entityIndex?: number) {
     return this.getEntityByName(entityName, entityIndex)
       .locator('..')
