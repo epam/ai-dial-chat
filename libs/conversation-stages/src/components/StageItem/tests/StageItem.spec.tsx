33a5cb88df655c4c1ecbf136f4ecb20d7c266b84
PolinaGurinovich97 <palina_kryvasheyeva@epam.com>
2026-07-20 12:34:35 +0200

    fix test

--- libs/conversation-stages/src/components/StageItem/tests/StageItem.spec.tsx
+++ libs/conversation-stages/src/components/StageItem/tests/StageItem.spec.tsx
@@ -1,5 +1,5 @@
 import { StageStatus } from '@epam/ai-dial-chat-shared';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 import { StageItem } from '../StageItem';
 
