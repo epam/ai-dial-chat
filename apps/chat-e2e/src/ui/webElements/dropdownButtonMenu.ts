5f95e7d42944f8a9fdbb5a6e0c42f6e6b2877f35
Irina_Kartun <irina.kartun@gmail.com>
2026-08-25 09:41:59 +0200

    test/feature-flag-e2e-v1: fixed menu locator

--- apps/chat-e2e/src/ui/webElements/dropdownButtonMenu.ts
+++ apps/chat-e2e/src/ui/webElements/dropdownButtonMenu.ts
@@ -4,5 +4,5 @@ import { Menu } from '@/src/ui/webElements/menu';
 export class DropdownButtonMenu extends Menu {
   public menuOptions = () => this.getChildElementBySelector(Tags.button);
   public menuOption = (option: string) =>
-    this.menuOptions().getElementLocatorByText(new RegExp(`^${option}$`));
+    this.menuOptions().getElementLocatorByText(new RegExp(`^${option}\\s*$`));
 }
