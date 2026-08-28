4abcb070a64e8d4043d6df907b0b987429b5059d
Andrii Stropalov <andrii_stropalov@epam.com>
2026-08-28 09:59:29 +0200

    Fix code ql review comment

--- libs/chat-shared/src/utils/string-utils.ts
+++ libs/chat-shared/src/utils/string-utils.ts
@@ -6,8 +6,15 @@ export const sanitizeConversationName = (name: string): string =>
   name.replace(PROHIBITED_CONVERSATION_NAME_CHARS_RE, '');
 
 /** Strips trailing `.` characters from a name. */
-export const stripTrailingDots = (name: string): string =>
-  name.replace(/\.+$/, '');
+export const stripTrailingDots = (name: string): string => {
+  let end = name.length;
+
+  while (end > 0 && name[end - 1] === '.') {
+    end -= 1;
+  }
+
+  return name.slice(0, end);
+};
 
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
